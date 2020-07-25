031d72aa2135b0b10f39c6d4e6a53fff3f3a41ff
chaoyanghe <me@chaoyanghe.com>
2020-07-25 01:01:03 -0700

    change the log format

--- fedml_api/standalone/fedavg/client.py
+++ fedml_api/standalone/fedavg/client.py
@@ -9,11 +9,9 @@ class Client:
     def __init__(self, local_training_data, local_test_data, local_sample_number, args, device):
         self.local_training_data = local_training_data
         self.local_test_data = local_test_data
-
         self.local_sample_number = local_sample_number
 
         self.args = args
-
         self.device = device
 
         self.criterion = nn.CrossEntropyLoss().to(device)
