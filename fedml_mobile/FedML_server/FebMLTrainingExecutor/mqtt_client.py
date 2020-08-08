355f0500b1f28a419d1c8293375716c9533cef6b
zongchang.jie <zongchang.jie@huolala.cn>
2020-08-08 16:44:07 +0800

    Construct a MqttClient

--- fedml_mobile/FedML_server/FebMLTrainingExecutor/mqtt_client.py
+++ fedml_mobile/FedML_server/FebMLTrainingExecutor/mqtt_client.py
@@ -6,49 +6,65 @@ import time
 unacked_sub = []
 
 
-def on_connect(client, userdata, flags, rc):
-    print("Connection returned with result code:" + str(rc))
+class MqttClient(object):
+    def __init__(self, host, port, on_message=None, topic='hello'):
+        self._unacked_sub = list()
+        self._topic = topic
+        # Construct a Client
+        self._client = mqtt.Client()
+        self._client.on_connect = self._on_connect
+        self._client.on_disconnect = self._on_disconnect
+        if on_message is not None:
+            self._client.on_message = on_message
+        else:
+            self._client.on_message = self._on_message
+        self._client.on_subscribe = self._on_subscribe
+        # connect broker,connect() or connect_async()
+        self._client.connect_async(host, port, 60)
+        self._client.loop_start()
+        # self._client.loop_forever()
+
+    def __del__(self):
+        self._client.loop_stop()
+        self._client.disconnect()
+
+    def _on_connect(self, client, userdata, flags, rc):
+        print("Connection returned with result code:" + str(rc))
+        # subscribe one topic
+        result, mid = self._client.subscribe(self._topic, 0)
+        self._unacked_sub.append(mid)
+        print(result)
+        # subscribe topic
+        result, mid = client.subscribe([("temperature", 0), ("humidity", 0)])
+        self._unacked_sub.append(mid)
+        print(result)
+        while len(unacked_sub) != 0:
+            time.sleep(1)
+        print("Finish subscribe!")
+
+    @staticmethod
+    def _on_message(client, userdata, msg):
+        print("Received message, topic:" + msg.topic + "payload:" + str(msg.payload))
+
+    @staticmethod
+    def _on_disconnect(client, userdata, rc):
+        print("Disconnection returned result:" + str(rc))
+
+    @staticmethod
+    def _on_subscribe(client, userdata, mid, granted_qos):
+        print("onSubscribe :" + str(mid))
+        unacked_sub.remove(mid)
+
+    def send(self, topic, msg):
+        print("send(%s, %s)" % (topic, msg))
+        self._client.publish(topic, payload=msg)
 
 
-def on_message(client, userdata, msg):
-    print("Received message, topic:" + msg.topic + "payload:" + str(msg.payload))
-
-
-def on_disconnect(client, userdata, rc):
-    print("Disconnection returned result:" + str(rc))
-
-
-def on_subscribe(client, userdata, mid, granted_qos):
-    unacked_sub.remove(mid)
-
-
-# Construct a Client
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
-client.on_subscribe = on_subscribe
-
-# connect broker,connect() or connect_async()
-client.connect("192.168.1.165", 1883, 60)
-
-client.loop_start()
-
-# subscribe one topic
-result, mid = client.subscribe("hello", 0)
-unacked_sub.append(mid)
-# subscribe topic
-result, mid = client.subscribe([("temperature", 0), ("humidity", 0)])
-unacked_sub.append(mid)
-
-while len(unacked_sub) != 0:
-    time.sleep(1)
-
-client.publish("hello", payload="Hello world!")
-client.publish("temperature", payload="24.0")
-client.publish("humidity", payload="65%")
-
-# disconnect
-time.sleep(5)
-client.loop_stop()
-client.disconnect()
+if __name__ == '__main__':
+    client = MqttClient("127.0.0.1", 1883)
+    time.sleep(5)
+    client.send("hello", "Hello world!")
+    client.send("temperature", "24.0")
+    client.send("humidity", "65%")
+    time.sleep(5)
+    print("client, send Fin...")
