9ebe098fddf57c49f6d6f5fef756f8ba2136790f
chaoyanghe <me@chaoyanghe.com>
2020-08-25 04:42:31 -0700

    shakesphare next char prediction with FedAvg + RNN (2 LSTM + 1FC)

--- fedml_api/model/shallow_neural_networks/rnn.py
+++ fedml_api/model/shallow_neural_networks/rnn.py
@@ -15,17 +15,22 @@ class RNN_OriginalFedAvg(nn.Module):
         An uncompiled `torch.nn.Module`.
       """
 
-    def __init__(self, embedding_dim=8, vocab_size=90):
+    def __init__(self, embedding_dim=8, vocab_size=90, hidden_size=256):
         super(RNN_OriginalFedAvg, self).__init__()
         self.embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim, padding_idx=0)
-        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=256, num_layers=2)
-        self.fc = nn.Linear(256, vocab_size)
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, num_layers=2, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
 
-    def forward(self, input_seq, hidden_state):
+    def forward(self, input_seq):
         embeds = self.embeddings(input_seq)
-        lstm_out, hidden_state = self.lstm(embeds, hidden_state)
-        output = self.fc(lstm_out)
-        return output, (hidden_state[0].detach(), hidden_state[1].detach())
+        # Note that the order of mini-batch is random so there is no hidden relationship among batches.
+        # So we do not input the previous batch's hidden state,
+        # leaving the first hidden state zero `self.lstm(embeds, None)`.
+        lstm_out, _ = self.lstm(embeds)
+        # use the final hidden state as the next character prediction
+        final_hidden_state = lstm_out[:, -1]
+        output = self.fc(final_hidden_state)
+        return output
 
 
 class RNN_StackOverFlow(nn.Module):
@@ -41,13 +46,14 @@ class RNN_StackOverFlow(nn.Module):
       """
 
     def __init__(self, vocab_size=10000,
-                           num_oov_buckets=1,
-                           embedding_size=96,
-                           latent_size=670,
-                           num_layers=1):
+                 num_oov_buckets=1,
+                 embedding_size=96,
+                 latent_size=670,
+                 num_layers=1):
         super(RNN_StackOverFlow, self).__init__()
         extended_vocab_size = vocab_size + 3 + num_oov_buckets  # For pad/bos/eos/oov.
-        self.word_embeddings = nn.Embedding(num_embeddings=extended_vocab_size, embedding_dim=embedding_size, padding_idx=0)
+        self.word_embeddings = nn.Embedding(num_embeddings=extended_vocab_size, embedding_dim=embedding_size,
+                                            padding_idx=0)
         self.lstm = nn.LSTM(input_size=embedding_size, hidden_size=latent_size, num_layers=num_layers)
         self.fc1 = nn.Linear(latent_size, embedding_size)
         self.fc2 = nn.Linear(embedding_size, extended_vocab_size)
--- fedml_api/standalone/fedavg/client.py
+++ fedml_api/standalone/fedavg/client.py
@@ -6,7 +6,8 @@ from torch import nn
 
 class Client:
 
-    def __init__(self, local_training_data, local_test_data, local_sample_number, args, device):
+    def __init__(self, client_idx, local_training_data, local_test_data, local_sample_number, args, device):
+        self.client_idx = client_idx
         self.local_training_data = local_training_data
         self.local_test_data = local_test_data
         self.local_sample_number = local_sample_number
@@ -17,31 +18,41 @@ class Client:
 
         self.criterion = nn.CrossEntropyLoss().to(device)
 
+    def update_local_dataset(self, client_idx, local_training_data, local_test_data, local_sample_number):
+        self.client_idx = client_idx
+        self.local_training_data = local_training_data
+        self.local_test_data = local_test_data
+        self.local_sample_number = local_sample_number
+
     def get_sample_number(self):
         return self.local_sample_number
 
     def train(self, net):
         net.train()
         # train and update
-        # optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=0.5)
-        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=self.args.lr,
-                                          weight_decay=0.0001, amsgrad=True)
+        if self.args.client_optimizer == "sgd":
+            optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr)
+        else:
+            optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=self.args.lr,
+                                              weight_decay=0.0001, amsgrad=True)
 
         epoch_loss = []
         for epoch in range(self.args.epochs):
             batch_loss = []
-            for batch_idx, (images, labels) in enumerate(self.local_training_data):
-                images, labels = images.to(self.device), labels.to(self.device)
+            for batch_idx, (x, labels) in enumerate(self.local_training_data):
+                x, labels = x.to(self.device), labels.to(self.device)
                 net.zero_grad()
-                log_probs = net(images)
+                log_probs = net(x)
                 loss = self.criterion(log_probs, labels)
                 loss.backward()
                 optimizer.step()
-                logging.info('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(images), len(self.local_training_data.dataset),
-                           100. * batch_idx / len(self.local_training_data), loss.item()))
+                # logging.info('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                #     epoch, (batch_idx + 1) * self.args.batch_size, len(self.local_training_data) * self.args.batch_size,
+                #            100. * (batch_idx + 1) / len(self.local_training_data), loss.item()))
                 batch_loss.append(loss.item())
             epoch_loss.append(sum(batch_loss) / len(batch_loss))
+            logging.info('Client Index = {}\tEpoch: {}\tLoss: {:.6f}'.format(
+                self.client_idx, epoch, sum(epoch_loss) / len(epoch_loss)))
         return net.cpu().state_dict(), sum(epoch_loss) / len(epoch_loss)
 
     def local_test(self, model_global, b_use_test_dataset=False):
@@ -56,30 +67,9 @@ class Client:
             for batch_idx, (x, target) in enumerate(test_data):
                 x = x.to(self.device)
                 target = target.to(self.device)
-
                 pred = model_global(x)
                 loss = self.criterion(pred, target)
-                _, predicted = torch.max(pred, 1)
-                correct = predicted.eq(target).sum()
-
-                test_acc += correct.item()
-                test_loss += loss.item() * target.size(0)
-                test_total += target.size(0)
-
-        return test_acc, test_total, test_loss
-
-    def global_test(self, model_global, global_test_data):
-        model_global.eval()
-        model_global.to(self.device)
-        test_loss = test_acc = test_total = 0.
-        with torch.no_grad():
-            for batch_idx, (x, target) in enumerate(global_test_data):
-                x = x.to(self.device)
-                target = target.to(self.device)
-
-                pred = model_global(x)
-                loss = self.criterion(pred, target)
-                _, predicted = torch.max(pred, 1)
+                _, predicted = torch.max(pred, -1)
                 correct = predicted.eq(target).sum()
 
                 test_acc += correct.item()
