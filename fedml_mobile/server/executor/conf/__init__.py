3d987eeea54a13ca6bba00e4492e459bf75b0914
chaoyanghe <me@chaoyanghe.com>
2020-09-01 13:02:10 -0700

    CI errors

--- fedml_mobile/server/executor/conf/__init__.py
+++ fedml_mobile/server/executor/conf/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-n
-__all__ = ['ENV', 'OUT_DIR_PATH', 'MQTT_BROKER_HOST', 'MQTT_BROKER_PORT', 'MODEL_FOLDER_PATH', 'RESOURCE_DIR_PATH']
 import os
-from fedml_mobile.server.executor.conf.conf import *
 from fedml_mobile.server.executor.conf.env import EnvWrapper
 ENV = EnvWrapper(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), True, 'TrainingExecutor')
