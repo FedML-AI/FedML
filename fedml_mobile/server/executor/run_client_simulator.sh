a5702fd64d712e58076d99d57c9091c9cc1b5333
chaoyanghe <me@chaoyanghe.com>
2020-08-31 23:09:40 -0700

    Update run_client_simulator.sh

--- fedml_mobile/server/executor/run_client_simulator.sh
+++ fedml_mobile/server/executor/run_client_simulator.sh
@@ -1,4 +1,2 @@
-CLIENT_NUM=$1
-
-
-nohup python3 ./mobile_clent_simulator.py $INDEX > ./fedavg-lr-mnist.txt 2>&1 &
+nohup python3 ./mobile_clent_simulator.py --client_uuid '0' > ./mobile_client_log_0.txt 2>&1 &
+nohup python3 ./mobile_clent_simulator.py --client_uuid '1' > ./mobile_client_log_1.txt 2>&1 &
