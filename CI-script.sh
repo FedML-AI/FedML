19cb7623bcf256915ad234557b897cad5be66072
chaoyanghe <me@chaoyanghe.com>
2020-09-01 17:26:06 -0700

    CI

--- CI-script.sh
+++ CI-script.sh
@@ -5,10 +5,32 @@ pyflakes .
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 conda activate fedml
 
-# test standalone
-cd ./fedml_experiments/standalone/fedavg
-
-# MNIST
+# MNIST standalone FedAvg
 wandb off
+cd ./fedml_experiments/standalone/fedavg
 sh run_fedavg_standalone_pytorch.sh 2 10 10 mnist ./../../../data/mnist lr hetero 2 2 0.03
+cd ./../../../
+
+# MNIST distributed FedAvg
+cd ./fedml_experiments/distributed/fedavg
+sh run_fedavg_distributed_pytorch.sh 10 10 1 4 lr hetero 2 2 10 0.03 mnist "./../../../data/mnist"
+cd ./../../../
+
+# MNIST mobile FedAvg
+cd ./fedml_mobile/server/executor/
+python3 -m app.py &
+bg_pid_server=$!
+
+python3 ./mobile_clent_simulator.py --client_uuid '0'
+bg_pid_client0=$!
+
+python3 ./mobile_clent_simulator.py --client_uuid '1'
+bg_pid_client1=$!
+
+kill $bg_pid_server
+kill $bg_pid_client0
+kill $bg_pid_client1
+
+exit 0
+
 cd ./../../../
