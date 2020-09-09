d1da2a4a10e4455e26dc5293deb8b78a8f0a2823
hwang595 <hongyiwang@cs.wisc.edu>
2020-09-09 02:40:50 -0500

    adapt latest fedavg implementation to fedavg_robust

--- fedml_experiments/distributed/fedavg_robust/main_fedavg_robust.py
+++ fedml_experiments/distributed/fedavg_robust/main_fedavg_robust.py
@@ -13,14 +13,19 @@ import wandb
 # add the FedML root directory to the python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../")))
 
-from fedml_api.data_preprocessing.cifar100.data_loader import load_partition_data_distributed_cifar100
-from fedml_api.data_preprocessing.cinic10.data_loader import load_partition_data_distributed_cinic10
-from fedml_api.data_preprocessing.cifar10.data_loader import load_partition_data_distributed_cifar10
-
 from fedml_api.distributed.fedavg_robust.FedAvgRobustAPI import FedML_init, FedML_FedAvgRobust_distributed
+
+from fedml_api.data_preprocessing.shakespeare.data_loader import load_partition_data_shakespeare
+from fedml_api.model.shallow_neural_networks.rnn import RNN_OriginalFedAvg
+
+from fedml_api.data_preprocessing.MNIST.data_loader import load_partition_data_mnist
+from fedml_api.model.linear_models.lr import LogisticRegression
+
+from fedml_api.data_preprocessing.cifar10.data_loader import load_partition_data_cifar10
+from fedml_api.data_preprocessing.cifar100.data_loader import load_partition_data_cifar100
+from fedml_api.data_preprocessing.cinic10.data_loader import load_partition_data_cinic10
 from fedml_api.model.deep_neural_networks.mobilenet import mobilenet
 from fedml_api.model.deep_neural_networks.resnet import resnet56
-
 
 
 def add_args(parser):
@@ -41,6 +46,9 @@ def add_args(parser):
     parser.add_argument('--partition_method', type=str, default='hetero', metavar='N',
                         help='how to partition the dataset on local workers')
 
+    parser.add_argument('--partition_alpha', type=float, default=0.5, metavar='PA',
+                        help='partition alpha (default: 0.5)')
+
     parser.add_argument('--defense_type', type=str, default='weak_dp', metavar='N',
                         help='the robust aggregation method to use on the server side')
 
@@ -50,14 +58,17 @@ def add_args(parser):
     parser.add_argument('--stddev', type=str, default=0.025, metavar='N',
                         help='the standard deviation of the Gaussian noise added in weak DP defense.')
 
-    parser.add_argument('--partition_alpha', type=float, default=0.5, metavar='PA',
-                        help='partition alpha (default: 0.5)')
-
-    parser.add_argument('--client_number', type=int, default=16, metavar='NN',
+    parser.add_argument('--client_num_in_total', type=int, default=1000, metavar='NN',
                         help='number of workers in a distributed cluster')
+
+    parser.add_argument('--client_num_per_round', type=int, default=4, metavar='NN',
+                        help='number of workers')
 
     parser.add_argument('--batch_size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
+
+    parser.add_argument('--client_optimizer', type=str, default='adam',
+                        help='SGD with momentum; adam')
 
     parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                         help='learning rate (default: 0.001)')
@@ -67,11 +78,11 @@ def add_args(parser):
     parser.add_argument('--epochs', type=int, default=5, metavar='EP',
                         help='how many epochs will be trained locally')
 
-    parser.add_argument('--local_points', type=int, default=5000, metavar='LP',
-                        help='the approximate fixed number of data points we will have on each local worker')
-
     parser.add_argument('--comm_round', type=int, default=10,
                         help='how many round of communications we shoud use')
+
+    parser.add_argument('--is_mobile', type=int, default=0,
+                        help='whether the program is running on the FedML-Mobile server side')
 
     parser.add_argument('--frequency_of_the_test', type=int, default=1,
                         help='the frequency of the algorithms')
@@ -83,6 +94,59 @@ def add_args(parser):
                         help='gpu_num_per_server')
     args = parser.parse_args()
     return args
+
+
+def load_data(args, dataset_name):
+    if dataset_name == "mnist":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        client_num, train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_mnist(args.batch_size)
+        """
+        For shallow NN or linear models, 
+        we uniformly sample a fraction of clients each round (as the original FedAvg paper)
+        """
+        args.client_num_in_total = client_num
+    elif dataset_name == "shakespeare":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        client_num, train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_shakespeare(args.batch_size)
+        args.client_num_in_total = client_num
+    else:
+        if dataset_name == "cifar10":
+            data_loader = load_partition_data_cifar10
+        elif dataset_name == "cifar100":
+            data_loader = load_partition_data_cifar100
+        elif dataset_name == "cinic10":
+            data_loader = load_partition_data_cinic10
+        else:
+            data_loader = load_partition_data_cifar10
+
+        train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = data_loader(args.dataset, args.data_dir, args.partition_method,
+                                args.partition_alpha, args.client_num_in_total, args.batch_size)
+
+    dataset = [train_data_num, test_data_num, train_data_global, test_data_global,
+               train_data_local_num_dict, train_data_local_dict, test_data_local_dict, class_num]
+    return dataset
+
+
+def create_model(args, model_name, output_dim):
+    logging.info("create_model. model_name = %s, output_dim = %s" % (model_name, output_dim))
+    model = None
+    if model_name == "lr" and args.dataset == "mnist":
+        model = LogisticRegression(28 * 28, output_dim)
+        args.client_optimizer = "sgd"
+    elif model_name == "rnn" and args.dataset == "shakespeare":
+        model = RNN_OriginalFedAvg(28 * 28, output_dim)
+        args.client_optimizer = "sgd"
+    elif model_name == "resnet56":
+        model = resnet56(class_num=output_dim)
+    elif model_name == "mobilenet":
+        model = mobilenet(class_num=output_dim)
+    return model
 
 
 def init_training_device(process_ID, fl_worker_num, gpu_num_per_machine):
@@ -108,6 +172,7 @@ if __name__ == "__main__":
     # parse python script input parameters
     parser = argparse.ArgumentParser()
     args = add_args(parser)
+    logging.info(args)
 
     # customize the process name
     str_process_name = "FedAvg (distributed):" + str(process_id)
@@ -137,9 +202,8 @@ if __name__ == "__main__":
     # Set the random seed. The np.random seed determines the dataset partition.
     # The torch_manual_seed determines the initial weight.
     # We fix these two, so that we can reproduce the result.
-    seed = 0
-    np.random.seed(seed)
-    torch.manual_seed(worker_number)
+    np.random.seed(0)
+    torch.manual_seed(10)
 
     # GPU arrangement: Please customize this function according your own topology.
     # The GPU server list is configured at "mpi_host_file".
@@ -154,28 +218,16 @@ if __name__ == "__main__":
     device = init_training_device(process_id, worker_number-1, args.gpu_num_per_server)
 
     # load data
-    if args.dataset == "cifar10":
-        data_loader = load_partition_data_distributed_cifar10
-    elif args.dataset == "cifar100":
-        data_loader = load_partition_data_distributed_cifar100
-    elif args.dataset == "cinic10":
-        data_loader = load_partition_data_distributed_cinic10
-    else:
-        data_loader = load_partition_data_distributed_cifar10
-    train_data_num, train_data_global, \
-    test_data_global, local_data_num, \
-    train_data_local, test_data_local, class_num = data_loader(process_id, args.dataset, args.data_dir,
-                                                               args.partition_method, args.partition_alpha,
-                                                               args.client_number, args.batch_size)
+    dataset = load_data(args, args.dataset)
+    [train_data_num, test_data_num, train_data_global, test_data_global,
+     train_data_local_num_dict, train_data_local_dict, test_data_local_dict, class_num] = dataset
 
-    # create the model
-    model = None
-    if args.model == "resnet56":
-        model = resnet56(class_num)
-    elif args.model == "mobilenet":
-        model = mobilenet(class_num=class_num)
+    # create model.
+    # Note if the model is DNN (e.g., ResNet), the training will be very slow.
+    # In this case, please use our FedML distributed version (./fedml_experiments/distributed_fedavg)
+    model = create_model(args, model_name=args.model, output_dim=dataset[7])
 
-    # start "federated averaging (FedAvg)"
+    # start "robust federated averaging (FedAvg)"
     FedML_FedAvgRobust_distributed(process_id, worker_number, device, comm,
                  model, train_data_num, train_data_global, test_data_global,
                  local_data_num, train_data_local, test_data_local, args)
