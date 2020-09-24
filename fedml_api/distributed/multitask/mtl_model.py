35e4be3e018112d98a6f126ae676ccf4f7b6dc7e
chaoyanghe <me@chaoyanghe.com>
2020-09-23 23:31:24 -0700

    Update mtl_model.py

--- fedml_api/distributed/multitask/mtl_model.py
+++ fedml_api/distributed/multitask/mtl_model.py
@@ -6,12 +6,11 @@ import torch.nn as nn
 
 class MTLModel(nn.Module):
 
-    def __init__(self, shared_nn, task_specific_nn):
+    def __init__(self, shared_nn, task_specific_nn=None):
         super(MTLModel, self).__init__()
         self.shared_NN = shared_nn
-        self.task_specific_layer = task_specific_nn
+        self.task_specific_layer = shared_nn.fc
 
     def forward(self, x):
         x = self.shared_NN(x)
-        x = self.task_specific_layer(x)
         return x
