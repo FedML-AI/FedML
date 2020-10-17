0670e3ace44452fcb67a3ddf3a9dad28c0f7dd4b
elliebababa <ellie24.huang@gmail.com>
2020-10-16 18:35:39 -0700

    stackoverflow lr

--- fedml_api/data_preprocessing/stackoverflow_lr/data_loader.py
+++ fedml_api/data_preprocessing/stackoverflow_lr/data_loader.py
@@ -7,13 +7,12 @@ import torch
 import torch.utils.data as data
 
 from fedml_api.data_preprocessing.stackoverflow_lr import utils
-
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-client_map_train = None
-client_map_test = None
+client_ids_train = None
+client_ids_test = None
 DEFAULT_TRAIN_CLINETS_NUM = 342477
 DEFAULT_TEST_CLIENTS_NUM = 204088
 DEFAULT_BATCH_SIZE = 100
@@ -28,56 +27,68 @@ _TITLE = 'title'
 _TAGS = 'tags'
 
 
-def get_client_map(client_map, client_id=None, client_num=None):
-    if client_map == None:
-        random.shuffle(client_id)
-        client_map = {
-            k: [client_id[i] for i in range(k, len(client_id), client_num)]
-            for k in range(client_num)
-        }
-    return client_map
-
-
 def get_dataloader(dataset, data_dir, train_bs, test_bs, client_idx=None):
     train_h5 = h5py.File(train_file_path, 'r')
     test_h5 = h5py.File(test_file_path, 'r')
-    if client_idx is None:
-        client_ids_train = list(train_h5[_EXAMPLE].keys())
-        client_ids_test = list(test_h5[_EXAMPLE].keys())
-    else:
-        client_ids_train = get_client_map(client_map_train)[client_idx]
-        client_ids_test = get_client_map(client_map_test)[client_idx]
-
     train_x = []
     train_y = []
     test_x = []
     test_y = []
-    for client_id in client_ids_train:
-        raw_tokens = train_h5[_EXAMPLE][client_id][_TOKENS][()]
+    
+    if client_idx is None:
+        for client_id in client_ids_train:
+            raw_tokens = train_h5[_EXAMPLE][client_id][_TOKENS][()]
+            raw_tokens = [x.decode('utf8') for x in raw_tokens]
+            raw_title = train_h5[_EXAMPLE][client_id][_TITLE][()]
+            raw_title = [x.decode('utf8') for x in raw_title]
+            raw_x = [' '.join(pair) for pair in zip(raw_tokens, raw_title)]
+            raw_y = [
+                x.decode('utf8') for x in train_h5[_EXAMPLE][client_id][_TAGS][()]
+            ]
+            train_x.extend(utils.preprocess_inputs(raw_x))
+            train_y.extend(utils.preprocess_targets(raw_y))
+        for client_id in client_ids_test:
+            raw_tokens_test = test_h5[_EXAMPLE][client_id][_TOKENS][()]
+            raw_tokens_test = [x.decode('utf8') for x in raw_tokens_test]
+            raw_title_test = test_h5[_EXAMPLE][client_id][_TITLE][()]
+            raw_title_test = [x.decode('utf8') for x in raw_title_test]
+            raw_x_test = [
+                ' '.join(pair) for pair in zip(raw_tokens_test, raw_title_test)
+            ]
+            raw_y_test = [
+                x.decode('utf8') for x in test_h5[_EXAMPLE][client_id][_TAGS][()]
+            ]
+            test_x.extend(utils.preprocess_inputs(raw_x_test))
+            test_y.extend(utils.preprocess_targets(raw_y_test))
+    else:
+        client_id_train = client_ids_train[client_idx]
+        raw_tokens = train_h5[_EXAMPLE][client_id_train][_TOKENS][()]
         raw_tokens = [x.decode('utf8') for x in raw_tokens]
-        raw_title = train_h5[_EXAMPLE][client_id][_TITLE][()]
+        raw_title = train_h5[_EXAMPLE][client_id_train][_TITLE][()]
         raw_title = [x.decode('utf8') for x in raw_title]
         raw_x = [' '.join(pair) for pair in zip(raw_tokens, raw_title)]
         raw_y = [
-            x.decode('utf8') for x in train_h5[_EXAMPLE][client_id][_TAGS][()]
+            x.decode('utf8') for x in train_h5[_EXAMPLE][client_id_train][_TAGS][()]
         ]
         train_x.extend(utils.preprocess_inputs(raw_x))
         train_y.extend(utils.preprocess_targets(raw_y))
-    for client_id in client_ids_test:
-        raw_tokens_test = test_h5[_EXAMPLE][client_id][_TOKENS][()]
-        raw_tokens_test = [x.decode('utf8') for x in raw_tokens_test]
-        raw_title_test = test_h5[_EXAMPLE][client_id][_TITLE][()]
-        raw_title_test = [x.decode('utf8') for x in raw_title_test]
-        raw_x_test = [
-            ' '.join(pair) for pair in zip(raw_tokens_test, raw_title_test)
-        ]
-        raw_y_test = [
-            x.decode('utf8') for x in test_h5[_EXAMPLE][client_id][_TAGS][()]
-        ]
-        test_x.extend(utils.preprocess_inputs(raw_x_test))
-        test_y.extend(utils.preprocess_targets(raw_y_test))
-    train_x, train_y = np.asarray(train_x), np.asarray(train_y)
-    test_x, test_y = np.asarray(test_x), np.asarray(test_y)
+
+        if client_idx <= len(client_ids_test) - 1:
+            client_id_test = client_ids_test[client_idx]
+            raw_tokens_test = test_h5[_EXAMPLE][client_id_test][_TOKENS][()]
+            raw_tokens_test = [x.decode('utf8') for x in raw_tokens_test]
+            raw_title_test = test_h5[_EXAMPLE][client_id_test][_TITLE][()]
+            raw_title_test = [x.decode('utf8') for x in raw_title_test]
+            raw_x_test = [
+                ' '.join(pair) for pair in zip(raw_tokens_test, raw_title_test)
+            ]
+            raw_y_test = [
+                x.decode('utf8') for x in test_h5[_EXAMPLE][client_id_test][_TAGS][()]
+            ]
+            test_x.extend(utils.preprocess_inputs(raw_x_test))
+            test_y.extend(utils.preprocess_targets(raw_y_test))
+
+    train_x, train_y = np.asarray(train_x).astype(np.float32), np.asarray(train_y).astype(np.float32)
     train_ds = data.TensorDataset(torch.tensor(train_x[:, :]),
                                   torch.tensor(train_y[:]))
     train_dl = data.DataLoader(dataset=train_ds,
@@ -85,9 +96,10 @@ def get_dataloader(dataset, data_dir, train_bs, test_bs, client_idx=None):
                                shuffle=True,
                                drop_last=False)
 
-    test_ds = data.TensorDataset(torch.tensor(test_x[:, :]),
+    if len(test_x) != 0:
+        test_x, test_y = np.asarray(test_x).astype(np.float32), np.asarray(test_y).astype(np.float32)
+        test_ds = data.TensorDataset(torch.tensor(test_x[:, :]),
                                  torch.tensor(test_y[:]))
-    if len(test_ds) != 0:
         test_dl = data.DataLoader(dataset=test_ds,
                                   batch_size=test_bs,
                                   shuffle=True,
@@ -101,18 +113,23 @@ def get_dataloader(dataset, data_dir, train_bs, test_bs, client_idx=None):
 
 
 def load_partition_data_distributed_federated_stackoverflow_lr(
-        process_id, dataset, data_dir, client_number=None, batch_size=DEFAULT_BATCH_SIZE):
-    client_number_train = client_number_test = client_number
-    if client_number is None:
-        client_number_train = DEFAULT_TRAIN_CLINETS_NUM
-        client_number_test = DEFAULT_TEST_CLIENTS_NUM
+        process_id, dataset, data_dir, batch_size=DEFAULT_BATCH_SIZE):
+
+    #client id list
+    train_h5 = h5py.File(train_file_path, 'r')
+    test_h5 = h5py.File(test_file_path, 'r')
+    global client_ids_train, client_ids_test
+    client_ids_train = list(train_h5[_EXAMPLE].keys())
+    client_ids_test = list(test_h5[_EXAMPLE].keys())
+    train_h5.close()
+    test_h5.close()
 
     # get global dataset
     if process_id == 0:
         train_data_global, test_data_global = get_dataloader(
             dataset, data_dir, batch_size, batch_size, process_id - 1)
-        train_data_num = len(train_data_global)
-        test_data_num = len(test_data_global)
+        train_data_num = len(train_data_global.dataset)
+        test_data_num = len(test_data_global.dataset)
         logging.info("train_dl_global number = " + str(train_data_num))
         logging.info("test_dl_global number = " + str(test_data_num))
         train_data_local = None
@@ -120,17 +137,6 @@ def load_partition_data_distributed_federated_stackoverflow_lr(
         local_data_num = 0
     else:
         # get local dataset
-        train_h5 = h5py.File(train_file_path, 'r')
-        test_h5 = h5py.File(test_file_path, 'r')
-        global client_map_train, client_map_test
-        client_map_train = get_client_map(client_map_train,
-                                          list(train_h5[_EXAMPLE].keys()),
-                                          client_number_train)
-        client_map_test = get_client_map(client_map_test,
-                                         list(test_h5[_EXAMPLE].keys()),
-                                         client_number_test)
-        train_h5.close()
-        test_h5.close()
         train_data_local, test_data_local = get_dataloader(
             dataset, data_dir, batch_size, batch_size, process_id - 1)
         train_data_num = local_data_num = len(train_data_local.dataset)
@@ -138,38 +144,38 @@ def load_partition_data_distributed_federated_stackoverflow_lr(
                      (process_id, local_data_num))
         train_data_global = None
         test_data_global = None
-    return train_data_num, train_data_global, test_data_global, local_data_num, train_data_local, test_data_local
+    output_dim = len(utils.get_tag_dict()) + 1 #oov
+    return train_data_num, train_data_global, test_data_global, local_data_num, train_data_local, test_data_local, output_dim
 
 
-def load_partition_data_federated_stackoverflow_lr(dataset, data_dir, client_number=None, batch_size=DEFAULT_BATCH_SIZE):
-    client_number_train = client_number_test = client_number
-    if client_number is None:
-        client_number = DEFAULT_TRAIN_CLINETS_NUM
-        client_number_train = DEFAULT_TRAIN_CLINETS_NUM
-        client_number_test = DEFAULT_TEST_CLIENTS_NUM
+def load_partition_data_federated_stackoverflow_lr(dataset, data_dir, batch_size=DEFAULT_BATCH_SIZE):
+    logging.info("load_partition_data_federated_stackoverflow_lr START")
+    
+    class ConcatDataset(torch.utils.data.Dataset):
+        def __init__(self, *datasets):
+            self.datasets = datasets
 
-    train_data_global, test_data_global = get_dataloader(
-        dataset, data_dir, batch_size, batch_size)
-    train_data_num = len(train_data_global)
-    test_data_num = len(test_data_global)
+        def __getitem__(self, i):
+            return tuple(d[i] for d in self.datasets)
 
+        def __len__(self):
+            return min(len(d) for d in self.datasets)
+        
+    #client id list
+    train_h5 = h5py.File(train_file_path, 'r')
+    test_h5 = h5py.File(test_file_path, 'r')
+    global client_ids_train, client_ids_test
+    client_ids_train = list(train_h5[_EXAMPLE].keys())
+    client_ids_test = list(test_h5[_EXAMPLE].keys())
+    train_h5.close()
+    test_h5.close()
+    
     # get local dataset
     data_local_num_dict = dict()
     train_data_local_dict = dict()
     test_data_local_dict = dict()
-    train_h5 = h5py.File(train_file_path, 'r')
-    test_h5 = h5py.File(test_file_path, 'r')
-    global client_map_train, client_map_test
-    client_map_train = get_client_map(client_map_train,
-                                      list(train_h5[_EXAMPLE].keys()),
-                                      client_number_train)
-    client_map_test = get_client_map(client_map_test,
-                                     list(test_h5[_EXAMPLE].keys()),
-                                     client_number_test)
-    train_h5.close()
-    test_h5.close()
-
-    for client_idx in range(client_number):
+    
+    for client_idx in range(DEFAULT_TRAIN_CLINETS_NUM):
         train_data_local, test_data_local = get_dataloader(
             dataset, data_dir, batch_size, batch_size, client_idx)
         local_data_num = len(train_data_local.dataset)
@@ -182,13 +188,30 @@ def load_partition_data_federated_stackoverflow_lr(dataset, data_dir, client_num
         train_data_local_dict[client_idx] = train_data_local
         test_data_local_dict[client_idx] = test_data_local
 
-    VOCAB_LEN = len(utils.get_word_dict()) + 1
-    return train_data_num, test_data_num, train_data_global, test_data_global, \
-           data_local_num_dict, train_data_local_dict, test_data_local_dict, VOCAB_LEN
+    train_data_global = torch.utils.data.DataLoader(
+                ConcatDataset(
+                    list(dl.dataset for dl in list(train_data_local_dict.values()))
+                ),
+                batch_size=batch_size, shuffle=True)
+    train_data_num = len(train_data_global.dataset)
+    
+    test_data_global = torch.utils.data.DataLoader(
+            ConcatDataset(
+                list(dl.dataset for dl in list(test_data_local_dict.values()) if dl is not None)
+            ),
+            batch_size=batch_size, shuffle=True)
+    test_data_num = len(test_data_global.dataset)
+
+    output_dim = len(utils.get_tag_dict()) + 1 #oov
+    return DEFAULT_TRAIN_CLINETS_NUM, train_data_num, test_data_num, train_data_global, test_data_global, \
+           data_local_num_dict, train_data_local_dict, test_data_local_dict, output_dim
 
 
 if __name__ == "__main__":
     # load_partition_data_federated_stackoverflow(None, None, 100, 128)
-    train_data_num, train_data_global, test_data_global, local_data_num, train_data_local, test_data_local = load_partition_data_distributed_federated_stackoverflow_lr(
-        2, None, None, 300000, 128)
+    train_data_num, train_data_global, test_data_global, local_data_num, train_data_local, test_data_local, output_dim= load_partition_data_distributed_federated_stackoverflow_lr(
+        2, None, None, 128)
+    DEFAULT_TRAIN_CLINETS_NUM, train_data_num, test_data_num, train_data_global, test_data_global, \
+           data_local_num_dict, train_data_local_dict, test_data_local_dict, output_dim = load_partition_data_federated_stackoverflow_lr(
+        None, None, 128)
     print(train_data_local, test_data_local)
