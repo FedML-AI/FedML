f68ab67d1643f8e33226b723d388f7c26a4b8b0e
chaoyanghe <chaoyanghe.com@gmail.com>
2020-11-05 04:03:37 -0800

    add

--- fedml_iot/raspberry_pi/fedavg/fedavg_rpi_client.py
+++ fedml_iot/raspberry_pi/fedavg/fedavg_rpi_client.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "./../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../")))
 
 
 import time
