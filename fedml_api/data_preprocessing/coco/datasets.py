Fixed Metrics Calculation Mistakes

fedml_api/data_preprocessing/coco/datasets.py
@@ -30,7 +30,9 @@ class CocoDataset(torch.utils.data.Dataset):
                  download_dataset=False,
                  year='2017',
                  split='train',
-                 categories=['person', 'dog', 'cat'],
+                 categories=['airplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'dining table', 'dog', 'horse', 'motorcycle', 'person', 'potted plant', 'sheep', 'couch', 'train', 'tv'],
+                 #categories=['person', 'cat', 'dog'],
+                #  categories=['person', 'cat', 'dog', 'horse', 'microwave', 'bed', 'airplane', 'bus', 'truck', 'elephant', 'sandwich', 'refrigerator', 'dining table', 'couch', 'stop sign'],
                  dataidxs=None):
         self.dataidxs = dataidxs  # Need torch.tensor to use this
         self.annotations_zip_path = Path('{}/annotations_trainval{}.zip'.format(root_dir, year))
@@ -190,3 +192,10 @@ class CocoDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.img_ids)
+
+
+if __name__ == '__main__':
+    root_dir = './data/coco'
+    train_data = CocoDataset(root_dir=root_dir)
+    val_Data = CocoDataset(root_dir=root_dir, split='val')
+

fedml_api/distributed/fedseg/FedSegAggregator.py
@@ -6,7 +6,7 @@ import wandb
 import numpy as np
 from torch import nn
 
-from fedml_api.distributed.fedseg.utils import transform_list_to_tensor, SegmentationLosses, Evaluator
+from fedml_api.distributed.fedseg.utils import transform_list_to_tensor, SegmentationLosses, Evaluator, Saver
 
 
 class FedSegAggregator(object):
@@ -137,10 +137,10 @@ class FedSegAggregator(object):
                     break
 
             # test on training dataset
-            train_acc = sum(train_acc_clients) / sum(train_num_samples_clients)
-            train_acc_class = sum(train_acc_class_clients) / sum(train_num_samples_clients)
-            train_mIoU = sum(train_mIoU_clients) / sum(train_num_samples_clients)
-            train_FWIoU = sum(train_FWIoU_clients) / sum(train_num_samples_clients)
+            train_acc = sum(train_acc_clients) / len(train_acc_clients)
+            train_acc_class = sum(train_acc_class_clients) / len(train_acc_class_clients)
+            train_mIoU = sum(train_mIoU_clients) / len(train_mIoU_clients)
+            train_FWIoU = sum(train_FWIoU_clients) / len(train_FWIoU_clients)
             train_loss = sum(train_losses_clients) / sum(train_num_samples_clients)
             wandb.log({"Train/Acc": train_acc, "round": round_idx})
             wandb.log({"Train/Acc_class": train_acc_class, "round": round_idx})
@@ -155,10 +155,10 @@ class FedSegAggregator(object):
             logging.info(stats)
 
             # test on test dataset
-            test_acc = sum(test_acc_clients) / sum(test_num_samples_clients)
-            test_acc_class = sum(test_acc_class_clients) / sum(test_num_samples_clients)
-            test_mIoU = sum(test_mIoU_clients) / sum(test_num_samples_clients)
-            test_FWIoU = sum(test_FWIoU_clients) / sum(test_num_samples_clients)
+            test_acc = sum(test_acc_clients) / len(test_acc_clients)
+            test_acc_class = sum(test_acc_class_clients) / len(test_acc_class_clients)
+            test_mIoU = sum(test_mIoU_clients) / len(test_mIoU_clients)
+            test_FWIoU = sum(test_FWIoU_clients) / len(test_FWIoU_clients)
             test_loss = sum(test_losses_clients) / sum(test_num_samples_clients)
             wandb.log({"Test/Acc": test_acc, "round": round_idx})
             wandb.log({"Test/Acc_class": test_acc_class, "round": round_idx})
@@ -178,16 +178,16 @@ class FedSegAggregator(object):
         self.evaluator.reset()
 
         test_loss = test_acc = test_total = 0.
-        criterion = SegmentationLosses().build_loss(mode=self.args.loss_type).to(self.device)
+        criterion = SegmentationLosses().build_loss(mode=self.args.loss_type)
         with torch.no_grad():
-            for batch_idx, (x, target) in enumerate(test_data):
-                x = x.to(self.device)
-                target = target.to(self.device)
+            for (batch_idx, batch) in enumerate(test_data):
+                x, target = batch['image'], batch['label']
+                x, target = x.to(self.device), target.to(self.device)
                 output = self.model(x)
-                loss = criterion(output, target)
+                loss = criterion(output, target).to(self.device)
                 test_loss += loss.item()
                 test_total += target.size(0)
-                pred = output.data.cpu().numpy
+                pred = output.cpu().numpy()
                 target = target.cpu().numpy()
                 pred = np.argmax(pred, axis=1)
                 self.evaluator.add_batch(target, pred)

fedml_experiments/distributed/fedseg/main_fedseg.py
@@ -95,7 +95,7 @@ def add_args(parser):
     parser.add_argument('--frequency_of_the_test', type=int, default=1,
                         help='the frequency of the algorithms')
 
-    parser.add_argument('--gpu_server_num', type=int, default=1,
+    parser.add_argument('--gpu_server_num', type=int, default=3,
                         help='gpu_server_num')
 
     parser.add_argument('--gpu_num_per_server', type=int, default=4,
@@ -157,7 +157,7 @@ def init_training_device(process_ID, fl_worker_num, gpu_num_per_machine):
     process_gpu_dict = dict()
     for client_index in range(fl_worker_num):
         gpu_index = client_index % gpu_num_per_machine
-        process_gpu_dict[client_index] = gpu_index
+        process_gpu_dict[client_index] = gpu_index + 1
 
     logging.info(process_gpu_dict)
     device = torch.device("cuda:" + str(process_gpu_dict[process_ID - 1]) if torch.cuda.is_available() else "cpu")
@@ -174,14 +174,18 @@ if __name__ == "__main__":
     args = add_args(parser)
 
     # customize the process name
-    str_process_name = "FedAvg (distributed):" + str(process_id)
+    str_process_name = "FedSeg (distributed):" + str(process_id)
     setproctitle.setproctitle(str_process_name)
 
-    # customize the log format
-    logging.basicConfig(level=logging.INFO,
+    
+
+    #customize the log format
+    logging.basicConfig(filename='info.log',
+                        level=logging.INFO,
                         format=str(
                             process_id) + ' - %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                         datefmt='%a, %d %b %Y %H:%M:%S')
+
     hostname = socket.gethostname()
     logging.info("#############process ID = " + str(process_id) +
                  ", host name = " + hostname + "########" +