e4ff3825b003c14b5bf06091d812906a03c188f1
chaoyanghe <me@chaoyanghe.com>
2020-11-29 14:02:09 -0800

    support as SDK

--- fedml_api/distributed/fedavg/MyModelTrainer.py
+++ fedml_api/distributed/fedavg/MyModelTrainer.py
@@ -3,7 +3,10 @@ import logging
 import torch
 from torch import nn
 
-from fedml_core.trainer.model_trainer import ModelTrainer
+try:
+    from fedml_core.trainer.model_trainer import ModelTrainer
+except ImportError:
+    from FedML.fedml_core.trainer.model_trainer import ModelTrainer
 
 
 class MyModelTrainer(ModelTrainer):
