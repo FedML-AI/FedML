273600dde0821717932fedb965dba13b35306010
wizard1203 <956234606@qq.com>
2020-12-02 22:16:37 +0800

    add single version and fix some landmark loader bugs

---
+++ fedml_experiments/single/run_single.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+CLIENT_NUM=$1
+WORKER_NUM=$2
+SERVER_NUM=$3
+GPU_NUM_PER_SERVER=$4
+MODEL=$5
+DISTRIBUTION=$6
+ROUND=$7
+EPOCH=$8
+BATCH_SIZE=$9
+LR=${10}
+DATASET=${11}
+DATA_DIR=${12}
+CLIENT_OPTIMIZER=${13}
+CI=${14}
+PYTHON=${15}
+GPU=${16}
+
+echo $BATCH_SIZE
+echo $LR
+
+
+
+
+
+$PYTHON ./main_single.py \
+  --gpu_server_num $SERVER_NUM \
+  --gpu_num_per_server $GPU_NUM_PER_SERVER \
+  --model $MODEL \
+  --dataset $DATASET \
+  --data_dir $DATA_DIR \
+  --partition_method $DISTRIBUTION  \
+  --client_num_in_total $CLIENT_NUM \
+  --client_num_per_round $WORKER_NUM \
+  --comm_round $ROUND \
+  --epochs $EPOCH \
+  --client_optimizer $CLIENT_OPTIMIZER \
+  --batch_size $BATCH_SIZE \
+  --lr $LR \
+  --ci $CI \
+  --gpu $GPU
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
