d455ff3fd3e9532f756f7c8704c7101c1b9c9e35
Johann Boschmann <johannboschmann@hotmail.de>
2020-12-04 08:28:45 +0100

    Fixes if/elif statements.

--- fedml_experiments/standalone/fedavg/main_fedavg.py
+++ fedml_experiments/standalone/fedavg/main_fedavg.py
@@ -151,6 +151,38 @@ def load_data(args, dataset_name):
         train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
         class_num = load_partition_data_federated_stackoverflow_nwp(args.dataset, args.data_dir)
         args.client_num_in_total = client_num
+    elif dataset_name == "ILSVRC2012":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_ImageNet(dataset=dataset_name, data_dir=args.data_dir,
+            partition_method=None, partition_alpha=None,
+            client_number=args.client_num_in_total, batch_size=args.batch_size)
+    elif dataset_name == "gld23k":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        args.client_num_in_total = 233
+        fed_train_map_file = os.path.join(args.data_dir, 'mini_gld_train_split.csv')
+        fed_test_map_file = os.path.join(args.data_dir, 'mini_gld_test.csv')
+
+        train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_landmarks(dataset=dataset_name, data_dir=args.data_dir,
+            fed_train_map_file=fed_train_map_file, fed_test_map_file=fed_test_map_file,
+            partition_method=None, partition_alpha=None,
+            client_number=args.client_num_in_total, batch_size=args.batch_size)
+
+    elif dataset_name == "gld160k":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        args.client_num_in_total = 1262
+        fed_train_map_file = os.path.join(args.data_dir, 'federated_train.csv')
+        fed_test_map_file = os.path.join(args.data_dir, 'test.csv')
+
+        train_data_num, test_data_num, train_data_global, test_data_global, \
+        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
+        class_num = load_partition_data_landmarks(dataset=dataset_name, data_dir=args.data_dir,
+            fed_train_map_file=fed_train_map_file, fed_test_map_file=fed_test_map_file,
+            partition_method=None, partition_alpha=None,
+            client_number=args.client_num_in_total, batch_size=args.batch_size)
     else:
         if dataset_name == "cifar10":
             data_loader = load_partition_data_cifar10
@@ -164,41 +196,6 @@ def load_data(args, dataset_name):
         train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
         class_num = data_loader(args.dataset, args.data_dir, args.partition_method,
                                 args.partition_alpha, args.client_num_in_total, args.batch_size)
-
-    elif dataset_name == "ILSVRC2012":
-        logging.info("load_data. dataset_name = %s" % dataset_name)
-        train_data_num, test_data_num, train_data_global, test_data_global, \
-        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
-        class_num = load_partition_data_ImageNet(dataset=dataset_name, data_dir=args.data_dir,
-            partition_method=None, partition_alpha=None, 
-            client_number=args.client_num_in_total, batch_size=args.batch_size)
-
-    elif dataset_name == "gld23k":
-        logging.info("load_data. dataset_name = %s" % dataset_name)
-        args.client_num_in_total = 233
-        fed_train_map_file = os.path.join(args.data_dir, 'mini_gld_train_split.csv')
-        fed_test_map_file = os.path.join(args.data_dir, 'mini_gld_test.csv')
-
-        train_data_num, test_data_num, train_data_global, test_data_global, \
-        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
-        class_num = load_partition_data_landmarks(dataset=dataset_name, data_dir=args.data_dir,
-            fed_train_map_file=fed_train_map_file, fed_test_map_file=fed_test_map_file,
-            partition_method=None, partition_alpha=None, 
-            client_number=args.client_num_in_total, batch_size=args.batch_size)
-
-    elif dataset_name == "gld160k":
-        logging.info("load_data. dataset_name = %s" % dataset_name)
-        args.client_num_in_total = 1262
-        fed_train_map_file = os.path.join(args.data_dir, 'federated_train.csv')
-        fed_test_map_file = os.path.join(args.data_dir, 'test.csv')
-
-        train_data_num, test_data_num, train_data_global, test_data_global, \
-        train_data_local_num_dict, train_data_local_dict, test_data_local_dict, \
-        class_num = load_partition_data_landmarks(dataset=dataset_name, data_dir=args.data_dir,
-            fed_train_map_file=fed_train_map_file, fed_test_map_file=fed_test_map_file,
-            partition_method=None, partition_alpha=None, 
-            client_number=args.client_num_in_total, batch_size=args.batch_size)
-
 
     if centralized:
         train_data_local_num_dict = {0: sum(user_train_data_num for user_train_data_num in train_data_local_num_dict.values())}
