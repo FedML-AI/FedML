115edc7514fae23b1b018f5da0a15a8c8a91c64a
elliebababa <ellie24.huang@gmail.com>
2020-12-11 19:54:22 -0800

    Update download bash

--- data/fed_cifar100/download_fedcifar100.sh
+++ data/fed_cifar100/download_fedcifar100.sh
@@ -2,6 +2,5 @@
 
 mkdir datasets
 cd datasets
-
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oJE5dtifbaDEFGTUjoIwLEtUTfL9XhcU' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1oJE5dtifbaDEFGTUjoIwLEtUTfL9XhcU" -O "fed_cifar100_test.h5" && rm -rf /tmp/cookies.txt 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1B9D5JCWAb2W0BvhLw7mzKgsGnOz7N774' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1B9D5JCWAb2W0BvhLw7mzKgsGnOz7N774" -O "fed_cifar100_train.h5" && rm -rf /tmp/cookies.txt 
+wget --no-check-certificate --no-proxy https://fedml.s3-us-west-1.amazonaws.com/fed_cifar100.tar.bz2
+tar -xvf fed_cifar100.tar.bz2
--- data/fed_shakespeare/dataset.py
+++ data/fed_shakespeare/dataset.py
@@ -1,8 +1,7 @@
 import tensorflow_federated as tff
 
-def download_and_save_federated_cifar100():
-    cifar100_train, cifar100_test = tff.simulation.datasets.cifar100.load_data(cache_dir='./')
-    
+def download_and_save_federated_shakespeare():
+    shakespeare_train, shakespeare_test = tff.simulation.datasets.shakespeare.load_data(cache_dir='./')
     
 """
 #with Tensorflow dependencies, you can run this python script to process the data from Tensorflow Federated locally:
@@ -12,4 +11,4 @@ Before downloading, please install TFF as its official instruction:
 pip install --upgrade tensorflow_federated
 """
 if __name__ == "__main__":
-    download_and_save_federated_cifar100()
+    download_and_save_federated_shakespeare()
