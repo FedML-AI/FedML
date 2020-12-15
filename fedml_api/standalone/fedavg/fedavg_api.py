8348dc7f363f2698faffdb2b5c3ef93d93976185
elliebababa <ellie24.huang@gmail.com>
2020-12-14 22:34:30 -0800

    Update test of fedavg_api

--- fedml_api/standalone/fedavg/fedavg_api.py
+++ fedml_api/standalone/fedavg/fedavg_api.py
@@ -16,6 +16,7 @@ class FedAvgAPI(object):
          train_data_local_num_dict, train_data_local_dict, test_data_local_dict, class_num] = dataset
         self.train_global = train_data_global
         self.test_global = test_data_global
+        self.val_global = None
         self.train_data_num_in_total = train_data_num
         self.test_data_num_in_total = test_data_num
 
@@ -67,9 +68,16 @@ class FedAvgAPI(object):
             w_global = self._aggregate(w_locals)
             self.model_trainer.set_model_params(w_global)
 
-            if round_idx % self.args.frequency_of_the_test == 0 or round_idx == self.args.comm_round - 1:
-
+            # test results
+            # at last round
+            if round_idx == self.args.comm_round - 1:
                 self._local_test_on_all_clients(round_idx)
+            # per {frequency_of_the_test} round
+            elif round_idx % self.args.frequency_of_the_test == 0:
+                if self.args.dataset.startswith("stackoverflow"):
+                    self._local_test_on_validation_set(round_idx)
+                else:
+                    self._local_test_on_all_clients(round_idx)
 
     def _client_sampling(self, round_idx, client_num_in_total, client_num_per_round):
         if client_num_in_total == client_num_per_round:
@@ -80,6 +88,13 @@ class FedAvgAPI(object):
             client_indexes = np.random.choice(range(client_num_in_total), num_clients, replace=False)
         logging.info("client_indexes = %s" % str(client_indexes))
         return client_indexes
+
+    def _generate_validation_set(num_samples=10000):
+        test_data_num  = len(self.test_global.dataset)
+        sample_indices = random.sample(range(test_data_num), min(num_samples, test_data_num))
+        subset = torch.utils.data.Subset(self.test_global.dataset, sample_indices)
+        sample_testset = torch.utils.data.DataLoader(subset, batch_size=self.args.batch_size)
+        self.val_global = sample_testset
 
     def _aggregate(self, w_locals):
         training_num = 0
@@ -100,33 +115,23 @@ class FedAvgAPI(object):
 
     def _local_test_on_all_clients(self, round_idx):
 
-        if self.args.dataset in ["stackoverflow_lr", "stackoverflow_nwp"]:
-            # due to the amount of test set, only abount 10000 samples are tested each round
-            testlist = random.sample(range(0, self.args.client_num_in_total), 100)
-            logging.info("################local_test_round_{}_on_clients : {}".format(round_idx, str(testlist)))
-        else:
-            logging.info("################local_test_on_all_clients : {}".format(round_idx))
-            testlist = list(range(self.args.client_num_in_total))
+        logging.info("################local_test_on_all_clients : {}".format(round_idx))
 
         train_metrics = {
             'num_samples': [],
             'num_correct': [],
-            'precisions': [],
-            'recalls': [],
             'losses': []
         }
 
         test_metrics = {
             'num_samples': [],
             'num_correct': [],
-            'precisions': [],
-            'recalls': [],
             'losses': []
         }
 
         client = self.client_list[0]
 
-        for client_idx in testlist:
+        for client_idx in range(self.args.client_num_in_total):
             """
             Note: for datasets like "fed_CIFAR100" and "fed_shakespheare",
             the training client number is larger than the testing client number
@@ -148,15 +153,6 @@ class FedAvgAPI(object):
             test_metrics['num_correct'].append(copy.deepcopy(test_local_metrics['test_correct']))
             test_metrics['losses'].append(copy.deepcopy(test_local_metrics['test_loss']))
 
-            if self.args.dataset == "stackoverflow_lr":
-                train_metrics['precisions'].append(copy.deepcopy(train_local_metrics['test_precision']))
-                train_metrics['recalls'].append(copy.deepcopy(train_local_metrics['test_recall']))
-                test_metrics['precisions'].append(copy.deepcopy(test_local_metrics['test_precision']))
-                test_metrics['recalls'].append(copy.deepcopy(test_local_metrics['test_recall']))
-                # due to the amount of test set, only abount 10000 samples are tested each round
-                if sum(test_metrics['num_samples']) >= 10000:
-                    break
-
             """
             Note: CI environment is CPU-based computing. 
             The training speed for RNN training is to slow in this setting, so we only test a client to make sure there is no programming error.
@@ -167,39 +163,51 @@ class FedAvgAPI(object):
         # test on training dataset
         train_acc = sum(train_metrics['num_correct']) / sum(train_metrics['num_samples'])
         train_loss = sum(train_metrics['losses']) / sum(train_metrics['num_samples'])
-        train_precision = sum(train_metrics['precisions']) / sum(train_metrics['num_samples'])
-        train_recall = sum(train_metrics['recalls']) / sum(train_metrics['num_samples'])
 
         # test on test dataset
         test_acc = sum(test_metrics['num_correct']) / sum(test_metrics['num_samples'])
         test_loss = sum(test_metrics['losses']) / sum(test_metrics['num_samples'])
-        test_precision = sum(test_metrics['precisions']) / sum(test_metrics['num_samples'])
-        test_recall = sum(test_metrics['recalls']) / sum(test_metrics['num_samples'])
 
-        if self.args.dataset == "stackoverflow_lr":
-            stats = {'training_acc': train_acc, 'training_precision': train_precision, 'training_recall': train_recall,
-                     'training_loss': train_loss}
-            wandb.log({"Train/Acc": train_acc, "round": round_idx})
-            wandb.log({"Train/Pre": train_precision, "round": round_idx})
-            wandb.log({"Train/Rec": train_recall, "round": round_idx})
-            wandb.log({"Train/Loss": train_loss, "round": round_idx})
-            logging.info(stats)
+        stats = {'training_acc': train_acc, 'training_loss': train_loss}
+        wandb.log({"Train/Acc": train_acc, "round": round_idx})
+        wandb.log({"Train/Loss": train_loss, "round": round_idx})
+        logging.info(stats)
 
-            stats = {'test_acc': test_acc, 'test_precision': test_precision, 'test_recall': test_recall,
-                     'test_loss': test_loss}
-            wandb.log({"Test/Acc": test_acc, "round": round_idx})
-            wandb.log({"Test/Pre": test_precision, "round": round_idx})
-            wandb.log({"Test/Rec": test_recall, "round": round_idx})
-            wandb.log({"Test/Loss": test_loss, "round": round_idx})
-            logging.info(stats)
+        stats = {'test_acc': test_acc, 'test_loss': test_loss}
+        wandb.log({"Test/Acc": test_acc, "round": round_idx})
+        wandb.log({"Test/Loss": test_loss, "round": round_idx})
+        logging.info(stats)
 
-        else:
-            stats = {'training_acc': train_acc, 'training_loss': train_loss}
-            wandb.log({"Train/Acc": train_acc, "round": round_idx})
-            wandb.log({"Train/Loss": train_loss, "round": round_idx})
-            logging.info(stats)
 
+    def _local_test_on_validation_set(self, round_idx):
+
+        logging.info("################local_test_on_validation_set : {}".format(round_idx))
+
+        if self.val_global is None:
+            self._generate_validation_set()
+
+        client = self.client_list[0]
+        client.update_local_dataset(0, None, self.val_global, None)
+        # test data
+        test_metrics = client.local_test(True)
+
+        if self.args.dataset == "stackoverflow_nwp":
+            test_acc = test_metrics['test_correct'] / test_metrics['test_total']
+            test_loss = test_metrics['test_loss'] / test_metrics['test_total']
             stats = {'test_acc': test_acc, 'test_loss': test_loss}
             wandb.log({"Test/Acc": test_acc, "round": round_idx})
             wandb.log({"Test/Loss": test_loss, "round": round_idx})
-            logging.info(stats)
+        elif self.args.dataset == "stackoverflow_lr":
+            test_acc = test_metrics['test_correct'] / test_metrics['test_total']
+            test_pre = test_metrics['test_precision'] / test_metrics['test_total']
+            test_rec = test_metrics['test_recall'] / test_metrics['test_total']
+            test_loss = test_metrics['test_loss'] / test_metrics['test_total']
+            stats = {'test_acc': test_acc, 'test_pre': test_pre, 'test_rec': test_rec, 'test_loss': test_loss}
+            wandb.log({"Test/Acc": test_acc, "round": round_idx})
+            wandb.log({"Test/Pre": test_pre, "round": round_idx})
+            wandb.log({"Test/Rec": test_rec, "round": round_idx})
+            wandb.log({"Test/Loss": test_loss, "round": round_idx})
+        else:
+            raise Exception("Unknown format to log metrics for dataset {}!"%self.args.dataset)
+
+        logging.info(stats)
