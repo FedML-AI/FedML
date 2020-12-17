3c41296d6dea0743b6d5d03c0c04d16366ac0815
mitashimpi <mitashimpi@gmail.com>
2020-12-17 15:51:54 -0800

    extracted common dirichlet distribution code into another method

--- fedml_core/non_iid_partition/noniid_partition.py
+++ fedml_core/non_iid_partition/noniid_partition.py
@@ -41,26 +41,32 @@ def non_iid_partition_with_dirichlet_distribution(label_list, client_num, classi
         for k in range(K):
             # get a list of batch indexes which are belong to label k
             idx_k = np.where(label_list == k)[0]
-            np.random.shuffle(idx_k)
-
-            # using dirichlet distribution to determine the unbalanced proportion for each client (n_nets in total)
-            # e.g., when n_nets = 4, proportions = [0.29543505 0.38414498 0.31998781 0.00043216], sum(proportions) = 1
-            proportions = np.random.dirichlet(np.repeat(alpha, client_num))
-
-            # get the index in idx_k according to the dirichlet distribution
-            proportions = np.array([p * (len(idx_j) < N / client_num) for p, idx_j in zip(proportions, idx_batch)])
-            proportions = proportions / proportions.sum()
-            proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
-
-            # generate the batch list for each client
-            idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
-            min_size = min([len(idx_j) for idx_j in idx_batch])
-
+            idx_batch, min_size = partition_class_samples_with_dirichlet_distribution(N, alpha, client_num, idx_batch,
+                                                                                      idx_k)
     for i in range(client_num):
         np.random.shuffle(idx_batch[i])
         net_dataidx_map[i] = idx_batch[i]
 
     return net_dataidx_map
+
+
+def partition_class_samples_with_dirichlet_distribution(N, alpha, client_num, idx_batch, idx_k):
+
+    np.random.shuffle(idx_k)
+    # using dirichlet distribution to determine the unbalanced proportion for each client (n_nets in total)
+    # e.g., when n_nets = 4, proportions = [0.29543505 0.38414498 0.31998781 0.00043216], sum(proportions) = 1
+    proportions = np.random.dirichlet(np.repeat(alpha, client_num))
+
+    # get the index in idx_k according to the dirichlet distribution
+    proportions = np.array([p * (len(idx_j) < N / client_num) for p, idx_j in zip(proportions, idx_batch)])
+    proportions = proportions / proportions.sum()
+    proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
+
+    # generate the batch list for each client
+    idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
+    min_size = min([len(idx_j) for idx_j in idx_batch])
+
+    return idx_batch, min_size
 
 
 def record_data_stats(y_train, net_dataidx_map, is_segmentation=False):
@@ -74,3 +80,5 @@ def record_data_stats(y_train, net_dataidx_map, is_segmentation=False):
         net_cls_counts[net_i] = tmp
     logging.debug('Data statistics: %s' % str(net_cls_counts))
     return net_cls_counts
+
+
