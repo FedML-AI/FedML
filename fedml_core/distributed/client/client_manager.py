4bf606fa1dccc86fa6a6db352a3b9a3ca31d17c1
Chaoyang He <chaoyanghe.com@gmail.com>
2021-10-03 21:15:45 -0700

    merge torch RPC and be ready to test the performance

--- fedml_core/distributed/client/client_manager.py
+++ fedml_core/distributed/client/client_manager.py
@@ -1,6 +1,6 @@
 import logging
 from abc import abstractmethod
-import sys
+
 from mpi4py import MPI
 
 from ..communication.gRPC.grpc_comm_manager import GRPCCommManager
@@ -8,10 +8,10 @@ from ..communication.message import Message
 from ..communication.mpi.com_manager import MpiCommunicationManager
 from ..communication.mqtt.mqtt_comm_manager import MqttCommManager
 from ..communication.observer import Observer
+from ..communication.trpc.trpc_comm_manager import TRPCCommManager
 
 
 class ClientManager(Observer):
-
     def __init__(self, args, comm=None, rank=0, size=0, backend="MPI"):
         self.args = args
         self.size = size
@@ -27,7 +27,11 @@ class ClientManager(Observer):
         elif backend == "GRPC":
             HOST = "0.0.0.0"
             PORT = 50000 + rank
-            self.com_manager = GRPCCommManager(HOST, PORT, ip_config_path=args.grpc_ipconfig_path, client_id=rank, client_num=size - 1)
+            self.com_manager = GRPCCommManager(
+                HOST, PORT, ip_config_path=args.grpc_ipconfig_path, client_id=rank, client_num=size - 1
+            )
+        elif backend == "TRPC":
+            self.com_manager = TRPCCommManager(args.trpc_master_config_path, client_id=rank, client_num=size)
         else:
             self.com_manager = MpiCommunicationManager(comm, rank, size, node_type="client")
         self.com_manager.add_observer(self)
@@ -70,4 +74,6 @@ class ClientManager(Observer):
         elif self.backend == "MQTT":
             self.com_manager.stop_receive_message()
         elif self.backend == "GRPC":
+            self.com_manager.stop_receive_message()
+        elif self.backend == "TRPC":
             self.com_manager.stop_receive_message()
