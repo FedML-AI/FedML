8f19ebeed273a4a031095de9886022efaf96a319
Chaoyang He <chaoyanghe.com@gmail.com>
2021-10-03 23:48:11 -0700

    Update run_fedavg_trpc.sh

--- fedml_experiments/distributed/fedavg/run_fedavg_trpc.sh
+++ fedml_experiments/distributed/fedavg/run_fedavg_trpc.sh
@@ -2,16 +2,16 @@
 set -x
 
 # enable InfiniBand
-export NCCL_SOCKET_IFNAME=ib0
-export GLOO_SOCKET_IFNAME=ib0
-export TP_SOCKET_IFNAME=ib0
-export NCCL_IB_HCA=ib0
+#export NCCL_SOCKET_IFNAME=ib0
+#export GLOO_SOCKET_IFNAME=ib0
+#export TP_SOCKET_IFNAME=ib0
+#export NCCL_IB_HCA=ib0
 
 # disable InfiniBand
-#export NCCL_IB_DISABLE=1
-#export NCCL_SOCKET_IFNAME=eno2
-#export GLOO_SOCKET_IFNAME=eno2
-#export TP_SOCKET_IFNAME=eno2
+export NCCL_IB_DISABLE=1
+export NCCL_SOCKET_IFNAME=eno2
+export GLOO_SOCKET_IFNAME=eno2
+export TP_SOCKET_IFNAME=eno2
 
 export NCCL_DEBUG=INFO
 export NCCL_MIN_NRINGS=1
