7adee345c8b7479b32dd5e7f50aa3c3c815639e4
Chaoyang He <chaoyanghe.com@gmail.com>
2021-10-06 22:48:25 -0700

    add fedml rpc demo

---
+++ fedml_core/distributed/test/test_rpc/client_manager.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../../")))
+
+from fedml_core.distributed.client.client_manager import ClientManager
+from fedml_core.distributed.communication.message import Message
+
+from .message_define import MyMessage
+
+
+class RPCClientManager(ClientManager):
+    def __init__(self, args, comm=None, rank=0, size=0, backend="GRPC"):
+        super().__init__(args, comm, rank, size, backend)
+
+    def run(self):
+        super().run()
+
+    def register_message_receive_handlers(self):
+        self.register_message_receive_handler(
+            MyMessage.MSG_TYPE_S2C_SYNC_MODEL_TO_CLIENT, self.handle_message_receive_model_from_server
+        )
+
+    def handle_message_receive_model_from_server(self, msg_params):
+        logging.info("handle_message_receive_model_from_server. msg_params = {}".format(msg_params))
+        self.finish()
+
---
+++ fedml_core/distributed/test/test_rpc/main.py
@@ -0,0 +1,71 @@
+import argparse
+import logging
+
+from mpi4py import MPI
+
+from .client_manager import RPCClientManager
+from .server_manager import RPCServerManager
+
+
+def add_args(parser):
+
+    parser.add_argument("--backend", type=str, default="GRPC")
+
+    parser.add_argument(
+        "--gpu_mapping_file",
+        type=str,
+        default="gpu_mapping.yaml",
+        help="the gpu utilization file for servers and clients. If there is no \
+                        gpu_util_file, gpu will not be used.",
+    )
+
+    parser.add_argument(
+        "--gpu_mapping_key", type=str, default="mapping_default", help="the key in gpu utilization file"
+    )
+
+    parser.add_argument(
+        "--grpc_ipconfig_path",
+        type=str,
+        default="grpc_ipconfig.csv",
+        help="config table containing ipv4 address of grpc server",
+    )
+
+    parser.add_argument(
+        "--trpc_master_config_path",
+        type=str,
+        default="trpc_master_config.csv",
+        help="config indicating ip address and port of the master (rank 0) node",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def run_worker(args, rank, size):
+    if rank == 0:
+        client_manager = RPCClientManager(args, rank=rank, size=size, backend=args.backend)
+    else:
+        server_manager = RPCServerManager(args, rank=rank, size=size, backend=args.backend)
+        server_manager.send_model_params()
+
+
+if __name__ == "__main__":
+
+    # parse python script input parameters
+    parser = argparse.ArgumentParser()
+    args = add_args(parser)
+
+    # customize the log format
+    # logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format=" - %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+        datefmt="%a, %d %b %Y %H:%M:%S",
+    )
+
+    logging.info(args)
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    world_size = 2
+    run_worker(args, rank, world_size)
