4b803f86a1c0be5ff18b9a2bc64760b56f7147e1
Chaoyang He <chaoyanghe.com@gmail.com>
2021-10-06 23:05:39 -0700

    Update main.py

--- fedml_core/distributed/test/test_rpc/main.py
+++ fedml_core/distributed/test/test_rpc/main.py
@@ -6,7 +6,7 @@ import sys
 from mpi4py import MPI
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../../")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../../FedML")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../../../FedML")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../../FedML")))
 
 from fedml_core.distributed.test.test_rpc.dummy_algorithm.client_manager import RPCClientManager
