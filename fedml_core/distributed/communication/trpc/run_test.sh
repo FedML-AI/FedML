f1d086f34d439324d2280f98e11c0a398f0b59f4
Chaoyang He <chaoyanghe.com@gmail.com>
2021-10-06 20:52:17 -0700

    Update run_test.sh

--- fedml_core/distributed/communication/trpc/run_test.sh
+++ fedml_core/distributed/communication/trpc/run_test.sh
@@ -1,14 +1,14 @@
 # enable InfiniBand
-#export NCCL_SOCKET_IFNAME=ib0
-#export GLOO_SOCKET_IFNAME=ib0
-#export TP_SOCKET_IFNAME=ib0
-#export NCCL_IB_HCA=ib0
+export NCCL_SOCKET_IFNAME=ib0
+export GLOO_SOCKET_IFNAME=ib0
+export TP_SOCKET_IFNAME=ib0
+export NCCL_IB_HCA=ib0
 
 # disable InfiniBand
-export NCCL_IB_DISABLE=1
-export NCCL_SOCKET_IFNAME=eno2
-export GLOO_SOCKET_IFNAME=eno2
-export TP_SOCKET_IFNAME=eno2
+#export NCCL_IB_DISABLE=1
+#export NCCL_SOCKET_IFNAME=eno2
+#export GLOO_SOCKET_IFNAME=eno2
+#export TP_SOCKET_IFNAME=eno2
 
 export NCCL_DEBUG=INFO
 export NCCL_MIN_NRINGS=1
@@ -24,6 +24,6 @@ kill $(ps aux | grep "test_cuda_cpu.py" | grep -v grep | awk '{print $2}')
 kill $(ps aux | grep "test_cuda_gpu.py" | grep -v grep | awk '{print $2}')
 kill $(ps aux | grep "test_cuda_gpu_ib.py" | grep -v grep | awk '{print $2}')
 # mpirun -np 2 -hostfile ./mpi_host_file python3 ./test-p2p.py
-mpirun -np 2 -hostfile ./mpi_host_file python3 ./test_cuda_gpu.py
-# mpirun -np 2 -hostfile ./mpi_host_file python3 ./test_cuda_gpu_ib.py
+# mpirun -np 2 -hostfile ./mpi_host_file python3 ./test_cuda_gpu.py
+mpirun -np 2 -hostfile ./mpi_host_file python3 ./test_cuda_gpu_ib.py
 mpirun -np 2 -hostfile ./mpi_host_file python3 ./test_cuda_cpu.py
