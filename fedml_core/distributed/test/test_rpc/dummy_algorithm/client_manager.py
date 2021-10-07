0223782f4366d4b0dbffd2b3d91fcf57a1cc696a
Chaoyang He <chaoyanghe.com@gmail.com>
2021-10-06 23:30:48 -0700

    Update client_manager.py

--- fedml_core/distributed/test/test_rpc/dummy_algorithm/client_manager.py
+++ fedml_core/distributed/test/test_rpc/dummy_algorithm/client_manager.py
@@ -6,7 +6,7 @@ import torch
 from fedml_core.distributed.client.client_manager import ClientManager
 from .message_define import MyMessage
 
-
+from .utils import transform_list_to_tensor
 class RPCClientManager(ClientManager):
     def __init__(self, args, comm=None, rank=0, size=0, backend="GRPC"):
         super().__init__(args, comm, rank, size, backend)
@@ -21,6 +21,7 @@ class RPCClientManager(ClientManager):
 
     def handle_message_receive_model_from_server(self, msg_params):
         list_model_params = msg_params.get_params()
-        received_model_tensor = torch.from_numpy(np.asarray(list_model_params)).float()
+        # received_model_tensor = torch.from_numpy(np.asarray(list_model_params)).float()
+        received_model_tensor = transform_list_to_tensor(list_model_params)
         logging.info("handle_message_receive_model_from_server. tensor.shape = {}".format(received_model_tensor.shape))
         self.finish()
