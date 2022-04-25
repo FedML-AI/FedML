e4176648f2535cb68d43190938698f3183c36311
chaoyanghe <chaoyanghe.com@gmail.com>
2022-04-25 14:32:36 -0700

    Revert "Revert "update events for different device id.""
    This reverts commit 2a8706fadfa0337e086ddaf1356aaecd0edc3170.

--- fedml_api/distributed/fedavg_cross_silo/FedAvgServerManager.py
+++ fedml_api/distributed/fedavg_cross_silo/FedAvgServerManager.py
@@ -121,7 +121,7 @@ class FedAVGServerManager(ServerManager):
             self.send_init_msg()
 
     def handle_message_receive_model_from_client(self, msg_params):
-        self.event_sdk.log_event_ended("comm_c2s")
+        self.event_sdk.log_event_ended("comm_c2s", event_edge_id=0)
         self.event_sdk.log_event_ended("wait")
 
         sender_id = msg_params.get(MyMessage.MSG_ARG_KEY_SENDER)
