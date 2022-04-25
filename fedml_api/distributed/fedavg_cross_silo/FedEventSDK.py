4a99770ef1efe757d4380b1d1f0f14c147fc25c9
chaoyanghe <chaoyanghe.com@gmail.com>
2022-04-25 14:32:15 -0700

    Revert "update events for different device id."
    This reverts commit 720015c90fcfec88d465a81b1e8fb45676dce9fb.

--- fedml_api/distributed/fedavg_cross_silo/FedEventSDK.py
+++ fedml_api/distributed/fedavg_cross_silo/FedEventSDK.py
@@ -35,36 +35,16 @@ class FedEventSDK:
         self.com_manager = MqttS3StatusManager(
             args.mqtt_config_path, args.s3_config_path, topic=args.run_id)
 
-    def log_event_started(self, event_name, event_value=None, event_edge_id=None):
-        if event_value is None:
-            event_value_passed = ""
-        else:
-            event_value_passed = event_value
-
-        if event_edge_id is not None:
-            edge_id = event_edge_id
-        else:
-            edge_id = self.edge_id
-
-        event_topic, event_msg = self.__build_event_mqtt_msg(self.args.run_id, edge_id,
+    def log_event_started(self, event_name, event_value=None):
+        event_topic, event_msg = self.__build_event_mqtt_msg(self.args.run_id, self.edge_id,
                                                              FedEventSDK.EVENT_TYPE_STARTED,
-                                                             event_name, event_value_passed)
+                                                             event_name, event_value)
         self.com_manager.send_message_json(event_topic, json.dumps(event_msg))
 
-    def log_event_ended(self, event_name, event_value=None, event_edge_id=None):
-        if event_value is None:
-            event_value_passed = ""
-        else:
-            event_value_passed = event_value
-
-        if event_edge_id is not None:
-            edge_id = event_edge_id
-        else:
-            edge_id = self.edge_id
-
-        event_topic, event_msg = self.__build_event_mqtt_msg(self.args.run_id, edge_id,
+    def log_event_ended(self, event_name, event_value=None):
+        event_topic, event_msg = self.__build_event_mqtt_msg(self.args.run_id, self.edge_id,
                                                              FedEventSDK.EVENT_TYPE_ENDED,
-                                                             event_name, event_value_passed)
+                                                             event_name, event_value)
         self.com_manager.send_message_json(event_topic, json.dumps(event_msg))
 
     @staticmethod
