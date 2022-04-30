b27e51e13650c0f97fb10ff47e0544471147b08e
chaoyanghe <chaoyanghe.com@gmail.com>
2022-04-30 00:59:17 -0700

    remove ssh file

--- doc/deploy/README.md
+++
@@ -1,8 +0,0 @@
-# Deploy to Self-hosted Static Server
-We deploy this documentation to doc.fedml.ai
-
-Currently. doc.fedml.ai is pointed to IP `34.209.120.253`
-```
-ssh -i edgeapi.pem centos@34.209.120.253
-
-```
--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="FedML",
-    version="0.7.0",
+    version="0.7.1",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
