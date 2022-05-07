736aab1eb48f35eed36ec77f104a1db02dc6d506
alex.liang <alexliang.kh@gmail.com>
2022-05-07 09:30:52 +0800

    update cross-silo and server-mnn.

--- python/fedml/device/device.py
+++ python/fedml/device/device.py
@@ -26,31 +26,34 @@ def get_device(args):
         )
         return device
     elif args.training_type == "cross_silo":
-        # if args.using_gpu:
-        #     device = torch.device(
-        #         "cuda:" + args.gpu_id if torch.cuda.is_available() else "cpu"
-        #     )
-        # else:
-        #     device = torch.device("cpu")
-        # logging.info("device = {}".format(device))
-        from .gpu_mapping import (
-            mapping_processes_to_gpu_device_from_yaml_file,
-        )
+        if args.scenario == "horizontal":
+            if args.using_gpu:
+                device = torch.device(
+                    "cuda:" + args.gpu_id if torch.cuda.is_available() else "cpu"
+                )
+            else:
+                device = torch.device("cpu")
+            logging.info("device = {}".format(device))
+        elif args.scenario == "hierarchical":
+            from .gpu_mapping import (
+                mapping_processes_to_gpu_device_from_yaml_file,
+            )
 
-        device = mapping_processes_to_gpu_device_from_yaml_file(
-            args.rank_in_node,
-            args.n_proc_per_node,
-            args.gpu_mapping_file if args.using_gpu else None,
-            args.gpu_mapping_key if args.using_gpu else None,
-        )
-        logging.info("device = {}".format(device))
-        return device
-        # if args.using_gpu:
-        #     device = torch.device(
-        #         "cuda:" + args.gpu_id if torch.cuda.is_available() else "cpu"
-        #     )
-        # else:
-        #     device = torch.device("cpu")
+            device = mapping_processes_to_gpu_device_from_yaml_file(
+                args.rank_in_node,
+                args.n_proc_per_node,
+                args.gpu_mapping_file if args.using_gpu else None,
+                args.gpu_mapping_key if args.using_gpu else None,
+            )
+            logging.info("device = {}".format(device))
+            return device
+        else:
+            if args.using_gpu:
+                device = torch.device(
+                    "cuda:" + args.gpu_id if torch.cuda.is_available() else "cpu"
+                )
+            else:
+                device = torch.device("cpu")
     elif args.training_type == "cross_device":
         if args.using_gpu:
             device = torch.device(
