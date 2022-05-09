76c9d1f11eee489e987d4d3ac6d7b105d34d4266
chaoyanghe <chaoyanghe.com@gmail.com>
2022-05-09 00:56:09 -0700

    windows build wheels

--- python/build_tools/github/build_wheels.sh
+++ python/build_tools/github/build_wheels.sh
@@ -3,5 +3,9 @@
 set -e
 set -x
 
+if [[ "$RUNNER_OS" == "windows-*" ]]; then
+    conda install -c conda-forge mpi4py openmpi
+fi
+
 python -m pip install cibuildwheel==2.5.0
 python -m cibuildwheel --output-dir wheelhouse
