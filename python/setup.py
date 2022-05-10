24df3713ce0f8abc810ae4e8f17b62708f16da31
chaoyanghe <chaoyanghe.com@gmail.com>
2022-05-10 12:05:39 -0700

    upload docker image fedml/fedml:cuda-11.4.0-devel-ubuntu20.04

--- python/setup.py
+++ python/setup.py
@@ -43,7 +43,7 @@ if platform.machine() == "x86_64":
 
 setup(
     name="fedml",
-    version="0.7.27",
+    version="0.7.28",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
