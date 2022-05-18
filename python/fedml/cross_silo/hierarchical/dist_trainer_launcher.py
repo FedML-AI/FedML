f256f1a289c4772992ddbe9e5737db7fb4b73363
amir-zsh <amir.ziashahabi@gmail.com>
2022-05-18 06:13:53 -0700

    pdsh added for nodes excution

--- python/fedml/cross_silo/hierarchical/dist_trainer_launcher.py
+++ python/fedml/cross_silo/hierarchical/dist_trainer_launcher.py
@@ -3,21 +3,25 @@
 import sys
 from fedml.arguments import load_arguments
 import subprocess
-
+import os
 def launch_dist_trainers():
     inputs = sys.argv[1:]
     args = load_arguments()
-    torchrun_arguments = [
+    os.environ['PDSH_RCMD_TYPE'] = 'ssh'
+    node_addresses = ",".join(args.node_addresses)
+    pdsh_cmd_aruments = ['pdsh', '-w', node_addresses]
+    torchrun_cmd_arguments = [
+        "torchrun",
         f"--nnodes={args.n_node_in_silo}",
         f"--nproc_per_node={args.n_proc_per_node}",
         f"--rdzv_endpoint={args.pg_master_address}:{args.pg_master_port}",
-        f"--node_rank={args.node_rank}",
+        "--node_rank=%n",
         "--rdzv_id=hi_fl",
         "torch_client.py",
     ] + inputs
 
-    print(" ".join(torchrun_arguments))
+    # print(" ".join(torchrun_arguments))
 
-    subprocess.run(["torchrun"] + torchrun_arguments)
+    subprocess.run(pdsh_cmd_aruments + torchrun_cmd_arguments)
 
 
