86c5efdb3704b181e2888974f96136214c064157
amir-zsh <amir.ziashahabi@gmail.com>
2022-05-18 06:13:53 -0700

    pdsh added for nodes excution

--- python/examples/cross_silo/mqtt_s3_fedavg_hierarchical_mnist_lr_example/one_line/run_client.sh
+++ python/examples/cross_silo/mqtt_s3_fedavg_hierarchical_mnist_lr_example/one_line/run_client.sh
@@ -4,7 +4,5 @@ export NCCL_SOCKET_IFNAME=$NETWORK_INTERFACE
 export GLOO_SOCKET_IFNAME=$NETWORK_INTERFACE
 
 RANK=$1
-NODE_RANK=$2
-PROCESS_NUM=2
 
-python3 client_dist_launcher.py --cf config/fedml_config.yaml --rank $RANK --node_rank $NODE_RANK
+python3 client_dist_launcher.py --cf config/fedml_config.yaml --rank $RANK
