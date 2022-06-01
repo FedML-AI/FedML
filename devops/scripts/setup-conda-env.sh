103d4f7a4359f0623f1ed99e3e42d98e642dff28
alex.liang <alexliang.kh@gmail.com>
2022-06-01 23:38:19 +0800

    update conda env.

--- devops/scripts/setup-conda-env.sh
+++ devops/scripts/setup-conda-env.sh
@@ -4,19 +4,7 @@ is_building_gpu_image=$1
 
 echo "conda set ssl_verify"
 
-conda clean -i
-
-conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
-
-conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
-
-conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-
-conda config --set show_channel_urls yes
-
-conda config --set ssl_verify false
-
-conda update conda
+conda config --set ssl_verify no
 
 conda_base_dir=`conda info |grep  'base environment' |awk -F':' '{print $2}' |awk -F'(' '{print $1}' |awk -F' ' '{print $1}'`
 conda_env_init="${conda_base_dir}/etc/profile.d/conda.sh"
