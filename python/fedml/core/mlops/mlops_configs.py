116399afad12956964e9614612abe1213ff1d454
Alex <alex.gpt.llm@gmail.com>
2022-06-03 00:42:54 +0800

    using server login mode as the original server agent.

--- python/fedml/core/mlops/mlops_configs.py
+++ python/fedml/core/mlops/mlops_configs.py
@@ -26,12 +26,12 @@ class MLOpsConfigs(Singleton):
 
         return MLOpsConfigs._config_instance
 
-    def fetch_configs(self):
+    def get_request_params(self):
         url = "https://open.fedml.ai/fedmlOpsServer/configs/fetch"
         config_version = "release"
         if (
-            hasattr(self.args, "config_version")
-            and self.args.config_version is not None
+                hasattr(self.args, "config_version")
+                and self.args.config_version is not None
         ):
             # Setup config url based on selected version.
             config_version = self.args.config_version
@@ -44,13 +44,20 @@ class MLOpsConfigs(Singleton):
             elif self.args.config_version == "local":
                 url = "http://localhost:9000/fedmlOpsServer/configs/fetch"
 
-        logging.info(url)
-        json_params = {"config_name": ["mqtt_config", "s3_config"]}
+        cert_path = None
         if str(url).startswith("https://"):
             cur_source_dir = os.path.dirname(__file__)
             cert_path = os.path.join(
                 cur_source_dir, "ssl", "open-" + config_version + ".fedml.ai_bundle.crt"
             )
+
+        return url, cert_path
+
+    def fetch_configs(self):
+        url, cert_path = self.get_request_params()
+        logging.info(url)
+        json_params = {"config_name": ["mqtt_config", "s3_config"]}
+        if cert_path is not None:
             requests.session().verify = cert_path
             response = requests.post(
                 url, json=json_params, verify=True, headers={"Connection": "close"}
@@ -66,6 +73,30 @@ class MLOpsConfigs(Singleton):
         else:
             raise Exception("failed to fetch device configurations!")
         return mqtt_config, s3_config
+
+    def fetch_all_configs(self):
+        url, cert_path = self.get_request_params()
+        logging.info(url)
+        json_params = {"config_name": ["mqtt_config", "s3_config", "ml_ops_config", "docker_config"]}
+        if cert_path is not None:
+            requests.session().verify = cert_path
+            response = requests.post(
+                url, json=json_params, verify=True, headers={"Connection": "close"}
+            )
+        else:
+            response = requests.post(
+                url, json=json_params, headers={"Connection": "close"}
+            )
+        status_code = response.json().get("code")
+        if status_code == "SUCCESS":
+            mqtt_config = response.json().get("data").get("mqtt_config")
+            s3_config = response.json().get("data").get("s3_config")
+            mlops_config = response.json().get("data").get("ml_ops_config")
+            docker_config = response.json().get("data").get("docker_config")
+        else:
+            raise Exception("failed to fetch device configurations!")
+
+        return mqtt_config, s3_config, mlops_config, docker_config
 
 
 if __name__ == "__main__":
