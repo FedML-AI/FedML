4dea91a2da36c8d49a11e6f402fbd33e8653a625
Chaoyang He <chaoyanghe.com@gmail.com>
2022-06-05 19:52:29 -0700

    Update client_login.py

--- python/fedml/cli/edge_deployment/client_login.py
+++ python/fedml/cli/edge_deployment/client_login.py
@@ -481,7 +481,8 @@ class FedMLClientRunner:
                 pos1 = uuid.find("\\n") + 2
                 uuid = uuid[pos1:-15]
                 return str(uuid)
-            device_id = hex(GetUUID())
+
+            device_id = str(GetUUID())
             click.echo(device_id)
         elif "posix" in os.name:
             device_id = hex(uuid.getnode())
@@ -489,7 +490,7 @@ class FedMLClientRunner:
             device_id = subprocess.Popen(
                 "hal-get-property --udi /org/freedesktop/Hal/devices/computer --key system.hardware.uuid".split()
             )
-            device_id = hex(device_id)
+            # device_id = hex(device_id)
 
         return device_id
 
