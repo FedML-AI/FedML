c8d5dcb14c723e73b0660c7a6d875a4faf8d3671
beiyuouo <beiyu233@gmail.com>
2022-06-07 17:20:42 +0800

    build(fedcv): image classification

---
+++ app/fedcv/image_classification/data/imagenet/dataset.py
@@ -0,0 +1,201 @@
+import os
+import os.path
+
+import torch.utils.data as data
+from PIL import Image
+
+
+def has_file_allowed_extension(filename, extensions):
+    """Checks if a file is an allowed extension.
+
+    Args:
+        filename (string): path to a file
+
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    filename_lower = filename.lower()
+    return any(filename_lower.endswith(ext) for ext in extensions)
+
+
+def find_classes(dir):
+    classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+    classes.sort()
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+    return classes, class_to_idx
+
+
+def make_dataset(dir, class_to_idx, extensions):
+    images = []
+
+    data_local_num_dict = dict()
+    net_dataidx_map = dict()
+    sum_temp = 0
+    dir = os.path.expanduser(dir)
+    for target in sorted(os.listdir(dir)):
+        d = os.path.join(dir, target)
+        if not os.path.isdir(d):
+            continue
+
+        target_num = 0
+        for root, _, fnames in sorted(os.walk(d)):
+            for fname in sorted(fnames):
+                if has_file_allowed_extension(fname, extensions):
+                    path = os.path.join(root, fname)
+                    item = (path, class_to_idx[target])
+                    images.append(item)
+                    target_num += 1
+
+        net_dataidx_map[class_to_idx[target]] = (sum_temp, sum_temp + target_num)
+        data_local_num_dict[class_to_idx[target]] = target_num
+        sum_temp += target_num
+
+    assert len(images) == sum_temp
+    return images, data_local_num_dict, net_dataidx_map
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        pass
+
+
+def default_loader(path):
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
+
+
+class ImageNet(data.Dataset):
+
+    def __init__(self, data_dir, dataidxs=None, train=True, transform=None, target_transform=None, download=False):
+        """
+            Generating this class too many times will be time-consuming.
+            So it will be better calling this once and put it into ImageNet_truncated.
+        """
+        self.dataidxs = dataidxs
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+        self.download = download
+        self.loader = default_loader
+        if self.train:
+            self.data_dir = os.path.join(data_dir, 'train')
+        else:
+            self.data_dir = os.path.join(data_dir, 'val')
+
+        self.all_data, self.data_local_num_dict, self.net_dataidx_map = self.__getdatasets__()
+        if dataidxs == None:
+            self.local_data = self.all_data
+        elif type(dataidxs) == int:
+            (begin, end) = self.net_dataidx_map[dataidxs]
+            self.local_data = self.all_data[begin: end]
+        else:
+            self.local_data = []
+            for idxs in dataidxs:
+                (begin, end) = self.net_dataidx_map[idxs]
+                self.local_data += self.all_data[begin: end]
+
+    def get_local_data(self):
+        return self.local_data
+
+    def get_net_dataidx_map(self):
+        return self.net_dataidx_map
+
+    def get_data_local_num_dict(self):
+        return self.data_local_num_dict
+
+    def __getdatasets__(self):
+        # all_data = datasets.ImageFolder(data_dir, self.transform, self.target_transform)
+
+        classes, class_to_idx = find_classes(self.data_dir)
+        IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif']
+        all_data, data_local_num_dict, net_dataidx_map = make_dataset(self.data_dir, class_to_idx, IMG_EXTENSIONS)
+        if len(all_data) == 0:
+            raise (RuntimeError("Found 0 files in subfolders of: " + self.data_dir + "\n"
+                                                                                     "Supported extensions are: " + ",".join(
+                extensions)))
+        return all_data, data_local_num_dict, net_dataidx_map
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        # img, target = self.data[index], self.target[index]
+
+        path, target = self.local_data[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.local_data)
+
+
+class ImageNet_truncated(data.Dataset):
+
+    def __init__(self, imagenet_dataset: ImageNet, dataidxs, net_dataidx_map, train=True, transform=None,
+                 target_transform=None, download=False):
+
+        self.dataidxs = dataidxs
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+        self.download = download
+        self.net_dataidx_map = net_dataidx_map
+        self.loader = default_loader
+        self.all_data = imagenet_dataset.get_local_data()
+        if dataidxs == None:
+            self.local_data = self.all_data
+        elif type(dataidxs) == int:
+            (begin, end) = self.net_dataidx_map[dataidxs]
+            self.local_data = self.all_data[begin: end]
+        else:
+            self.local_data = []
+            for idxs in dataidxs:
+                (begin, end) = self.net_dataidx_map[idxs]
+                self.local_data += self.all_data[begin: end]
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        # img, target = self.data[index], self.target[index]
+
+        path, target = self.local_data[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.local_data)
---
+++ app/fedcv/image_classification/model/__init__.py
@@ -0,0 +1,3 @@
+from .efficientnet.efficientnet import EfficientNet
+from .densenet import DenseNet
+from .mobilenet_v3 import MobileNetV3
