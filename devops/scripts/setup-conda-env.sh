437e8b34df913b4c2b55c76d3d269f2d299eccf9
alex.liang <alexliang.kh@gmail.com>
2022-06-09 05:23:12 +0800

    update jenkinsfile.

--- devops/scripts/setup-conda-env.sh
+++ devops/scripts/setup-conda-env.sh
@@ -2,17 +2,11 @@
 
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-source /opt/conda/etc/profile.d/conda.sh
-conda info
-conda config --set ssl_verify false
-conda update --all
-conda env create -f ./fedml/environment.yml
+echo "conda create -n fedml python=3.7.4"
+conda create -y -n fedml-pip python=3.7.4
 
-#echo "conda create -n fedml python=3.7.4"
-#conda create -y -n fedml-pip python=3.7.4
-#
-#echo "conda activate fedml"
-#conda activate fedml-pip
+echo "conda activate fedml"
+conda activate fedml-pip
 
 # Install MPI
 conda install -y -c anaconda mpi4py
