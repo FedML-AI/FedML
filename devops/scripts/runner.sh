0258a0fd021e6cb1de6cb937474605f9f34f5ef6
alex.liang <alexliang.kh@gmail.com>
2022-06-09 03:06:50 +0800

    update jenkinsfile.

--- devops/scripts/runner.sh
+++ devops/scripts/runner.sh
@@ -1,4 +1,24 @@
-#!/bin/bash
+#!/bin/bash --login
+# The --login ensures the bash configuration is loaded,
+# enabling Conda.
+
+ACCOUNT_ID=$1
+FEDML_VERSION=$2
+FEDML_RUNNER_CMD=$3
+
+# Enable strict mode.
+set -euo pipefail
+# ... Run whatever commands ...
+
+# Temporarily disable strict mode and activate conda:
+set +euo pipefail
+conda activate fedml_env
+
+# Re-enable strict mode:
+set -euo pipefail
+
+# exec the final command:
+exec fedml login ${ACCOUNT_ID} -v ${FEDML_VERSION} -s -r cloud_server -rc ${FEDML_RUNNER_CMD}
 
 cur_loop=1
 while [ $cur_loop -eq 1 ]
