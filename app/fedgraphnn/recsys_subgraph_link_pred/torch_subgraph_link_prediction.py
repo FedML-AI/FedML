e98d1884a642013dd977e2c1c60f70f2b7d3ba32
emirceyani <emirceyani@gmail.com>
2022-06-08 03:08:29 +0000

    Upload RecSys SubgraphLP Codes

--- app/fedgraphnn/recsys_subgraph_link_pred/torch_subgraph_link_prediction.py
+++ app/fedgraphnn/recsys_subgraph_link_pred/torch_subgraph_link_prediction.py
@@ -1,6 +1,68 @@
 import fedml
-import fedgraphnn
+from data.data_loader import *
+from model.gcn_link import GCNLinkPred
+from model.gat_link import GATLinkPred
+from model.sage_link import SAGELinkPred
+
+from trainer.fed_subgraph_lp_trainer import FedSubgraphLPTrainer
 from fedml.simulation import SimulatorMPI
+
+
+def load_data(args):
+    if args.dataset not in ["ciao", "epinions"]:
+        raise Exception("no such dataset!")
+
+    args.pred_task = "link_prediction"
+
+    args.metric = "MAE"
+
+    if args.model == "gcn":
+        args.normalize_features = True
+        args.normalize_adjacency = True
+
+    (
+        train_data_num,
+        val_data_num,
+        test_data_num,
+        train_data_global,
+        val_data_global,
+        test_data_global,
+        data_local_num_dict,
+        train_data_local_dict,
+        val_data_local_dict,
+        test_data_local_dict,
+        feature_dim,
+    ) = load_partition_data(args, args.data_dir, args.client_num_in_total)
+
+    dataset = [
+        train_data_num,
+        test_data_num,
+        train_data_global,
+        test_data_global,
+        data_local_num_dict,
+        train_data_local_dict,
+        test_data_local_dict,
+        feature_dim,
+    ]
+
+    return dataset 
+
+
+def create_model(args, model_name, feature_dim):
+    logging.info("create_model. model_name = %s" % (model_name))
+    if model_name == "gcn":
+        model = GCNLinkPred(feature_dim, args.hidden_size, args.node_embedding_dim)
+    elif model_name == "gat":
+        model = GATLinkPred(
+            feature_dim, args.hidden_size, args.node_embedding_dim, args.num_heads
+        )
+    elif model_name == "sage":
+        model = SAGELinkPred(feature_dim, args.hidden_size, args.node_embedding_dim)
+    else:
+        raise Exception("such model does not exist !")
+    trainer = FedSubgraphLPTrainer(model)
+    logging.info("Model and Trainer  - done")
+    return model, 
 
 if __name__ == "__main__":
     # init FedML framework
@@ -10,10 +72,10 @@ if __name__ == "__main__":
     device = fedml.device.get_device(args)
 
     # load data
-    dataset, output_dim = fedgraphnn.data.load(args)
+    dataset, output_dim = load_data(args)
 
     # load model
-    model, trainer = fedgraphnn.model.create(args, output_dim)
+    model, trainer = create_model(args, args.model_name)
 
     # start training
     simulator = SimulatorMPI(args, device, dataset, model, trainer)
