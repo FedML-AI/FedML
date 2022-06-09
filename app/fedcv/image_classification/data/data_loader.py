7a865293739f17d458fb15de439db661592467fd
beiyuouo <beiyu233@gmail.com>
2022-06-09 09:24:58 +0800

    chore: add most dataset to fedcv

--- app/fedcv/image_classification/data/data_loader.py
+++ app/fedcv/image_classification/data/data_loader.py
@@ -2,8 +2,14 @@ import os
 
 import numpy as np
 import torch
-from .imagenet.data_loader import load_partition_data_ImageNet
-from .landmarks.data_loader import load_partition_data_landmarks
+from fedml.data.ImageNet.data_loader import load_partition_data_ImageNet
+from fedml.data.Landmarks.data_loader import load_partition_data_landmarks
+from fedml.data.cifar10.data_loader import load_partition_data_cifar10
+from fedml.data.cifar100.data_loader import load_partition_data_cifar100
+from fedml.data.FederatedEMNIST.data_loader import load_partition_data_federated_emnist
+from fedml.data.MNIST.data_loader import load_partition_data_mnist, download_mnist
+from fedml.data.cinic10.data_loader import load_partition_data_cinic10
+from fedml.data.fed_cifar100.data_loader import load_partition_data_federated_cifar100
 import logging
 
 
@@ -21,7 +27,7 @@ def combine_batches(batches):
 
 
 def load_synthetic_data(args):
-    dataset_name = args.dataset
+    dataset_name = str(args.dataset).lower()
     # check if the centralized training is enabled
     centralized = True if (args.client_num_in_total == 1 and args.training_type != "cross_silo") else False
 
@@ -33,14 +39,67 @@ def load_synthetic_data(args):
     else:
         full_batch = False
 
-    if dataset_name.lower() == "imagenet":
-        # load imagenet dataset
+    if dataset_name == "mnist":
+        download_mnist(args.data_cache_dir)
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        (
+            client_num,
+            train_data_num,
+            test_data_num,
+            train_data_global,
+            test_data_global,
+            train_data_local_num_dict,
+            train_data_local_dict,
+            test_data_local_dict,
+            class_num,
+        ) = load_partition_data_mnist(
+            args.batch_size,
+            train_path=args.data_cache_dir + "/MNIST/train",
+            test_path=args.data_cache_dir + "/MNIST/test",
+        )
+        """
+        For shallow NN or linear models, 
+        we uniformly sample a fraction of clients each round (as the original FedAvg paper)
+        """
+        args.client_num_in_total = client_num
+
+    elif dataset_name == "femnist":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        (
+            client_num,
+            train_data_num,
+            test_data_num,
+            train_data_global,
+            test_data_global,
+            train_data_local_num_dict,
+            train_data_local_dict,
+            test_data_local_dict,
+            class_num,
+        ) = load_partition_data_federated_emnist(args.dataset, args.data_cache_dir)
+        args.client_num_in_total = client_num
+
+    elif dataset_name == "fed_cifar100":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        (
+            client_num,
+            train_data_num,
+            test_data_num,
+            train_data_global,
+            test_data_global,
+            train_data_local_num_dict,
+            train_data_local_dict,
+            test_data_local_dict,
+            class_num,
+        ) = load_partition_data_federated_cifar100(args.dataset, args.data_cache_dir)
+        args.client_num_in_total = client_num
+    elif dataset_name in ["imagenet", "ILSVRC2012".lower()]:
+        logging.info("load_data. dataset_name = %s" % dataset_name)
         (
             train_data_num,
             test_data_num,
             train_data_global,
             test_data_global,
-            data_local_num_dict,
+            train_data_local_num_dict,
             train_data_local_dict,
             test_data_local_dict,
             class_num,
@@ -52,29 +111,85 @@ def load_synthetic_data(args):
             client_number=args.client_num_in_total,
             batch_size=args.batch_size,
         )
-    elif dataset_name.lower() == "landmark":
-        # load landmark dataset
+
+    elif dataset_name in ["gld23k", "landmarks"]:
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        args.client_num_in_total = 233
+        fed_train_map_file = os.path.join(args.data_cache_dir, "mini_gld_train_split.csv")
+        fed_test_map_file = os.path.join(args.data_cache_dir, "mini_gld_test.csv")
+
         (
             train_data_num,
             test_data_num,
             train_data_global,
             test_data_global,
-            data_local_num_dict,
+            train_data_local_num_dict,
             train_data_local_dict,
             test_data_local_dict,
             class_num,
         ) = load_partition_data_landmarks(
-            dataset=args.dataset,
-            data_dir=args.data_dir,
-            fed_train_map_file=args.fed_train_map_file,
-            fed_test_map_file=args.fed_test_map_file,
-            partition_method=args.partition_method,
-            partition_alpha=args.partition_alpha,
+            dataset=dataset_name,
+            data_dir=args.data_cache_dir,
+            fed_train_map_file=fed_train_map_file,
+            fed_test_map_file=fed_test_map_file,
+            partition_method=None,
+            partition_alpha=None,
             client_number=args.client_num_in_total,
             batch_size=args.batch_size,
         )
+
+    elif dataset_name == "gld160k":
+        logging.info("load_data. dataset_name = %s" % dataset_name)
+        args.client_num_in_total = 1262
+        fed_train_map_file = os.path.join(args.data_cache_dir, "federated_train.csv")
+        fed_test_map_file = os.path.join(args.data_cache_dir, "test.csv")
+
+        (
+            train_data_num,
+            test_data_num,
+            train_data_global,
+            test_data_global,
+            train_data_local_num_dict,
+            train_data_local_dict,
+            test_data_local_dict,
+            class_num,
+        ) = load_partition_data_landmarks(
+            dataset=dataset_name,
+            data_dir=args.data_cache_dir,
+            fed_train_map_file=fed_train_map_file,
+            fed_test_map_file=fed_test_map_file,
+            partition_method=None,
+            partition_alpha=None,
+            client_number=args.client_num_in_total,
+            batch_size=args.batch_size,
+        )
+
     else:
-        return None
+        if dataset_name == "cifar10":
+            data_loader = load_partition_data_cifar10
+        elif dataset_name == "cifar100":
+            data_loader = load_partition_data_cifar100
+        elif dataset_name == "cinic10":
+            data_loader = load_partition_data_cinic10
+        else:
+            raise ValueError("dataset_name %s not supported" % dataset_name)
+        (
+            train_data_num,
+            test_data_num,
+            train_data_global,
+            test_data_global,
+            train_data_local_num_dict,
+            train_data_local_dict,
+            test_data_local_dict,
+            class_num,
+        ) = data_loader(
+            args.dataset,
+            args.data_cache_dir,
+            args.partition_method,
+            args.partition_alpha,
+            args.client_num_in_total,
+            args.batch_size,
+        )
 
     if centralized:
         train_data_local_num_dict = {
