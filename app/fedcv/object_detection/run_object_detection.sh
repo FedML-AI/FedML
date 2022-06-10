9572d3ef8f92a3d54929f7fc150e385de5fe403f
beiyuouo <beiyu233@gmail.com>
2022-06-10 15:48:19 +0800

    build(fedcv): update yolov5 model, add detection trainer

--- app/fedcv/object_detection/run_object_detection.sh
+++ app/fedcv/object_detection/run_object_detection.sh
@@ -9,4 +9,4 @@ hostname > mpi_host_file
 
 $(which mpirun) -np $PROCESS_NUM \
 -hostfile mpi_host_file \
-python torch_image_segmentation.py --cf config/fedml_config.yaml
+python torch_object_detection.py --cf config/fedml_config.yaml
--- app/fedcv/object_detection/trainer/detection_trainer.py
+++ app/fedcv/object_detection/trainer/detection_trainer.py
@@ -1,19 +1,21 @@
+from ast import Pass
 import logging, time
 import sys, os
 
 import numpy as np
 import torch
 import wandb
+from torch.optim import Adam, lr_scheduler
 
 # add the FedML root directory to the python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../")))
 from fedml.core.alg_frame.client_trainer import ClientTrainer
-from fedml.simulation.mpi.fedseg.utils import SegmentationLosses, Evaluator, LR_Scheduler, EvaluationMetricsKeeper
+from ..model.yolo.utils.loss import ComputeLoss
 
 
-class SegmentationTrainer(ClientTrainer):
+class DetectionTrainer(ClientTrainer):
     def __init__(self, model, args=None):
-        super(SegmentationTrainer, self).__init__(model, args)
+        super(DetectionTrainer, self).__init__(model, args)
 
     def get_model_params(self):
         if self.args.backbone_freezed:
@@ -36,8 +38,7 @@ class SegmentationTrainer(ClientTrainer):
         args = self.args
         model.to(device)
         model.train()
-        criterion = SegmentationLosses().build_loss(mode=args.loss_type)
-        scheduler = LR_Scheduler(args.lr_scheduler, args.lr, args.epochs, len(train_data))
+        criterion = ComputeLoss(model)
 
         if args.client_optimizer == "sgd":
 
@@ -66,6 +67,8 @@ class SegmentationTrainer(ClientTrainer):
                 amsgrad=True,
             )
 
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: 1.0 - epoch / args.epochs)
+
         epoch_loss = []
 
         for epoch in range(args.epochs):
@@ -74,9 +77,8 @@ class SegmentationTrainer(ClientTrainer):
             logging.info("Trainer_ID: {0}, Epoch: {1}".format(self.id, epoch))
 
             for (batch_idx, batch) in enumerate(train_data):
-                x, labels = batch["image"], batch["label"]
+                x, labels, paths, _ = batch
                 x, labels = x.to(device), labels.to(device)
-                scheduler(optimizer, batch_idx, epoch)
                 optimizer.zero_grad()
                 log_probs = model(x)
                 loss = criterion(log_probs, labels).to(device)
@@ -90,6 +92,8 @@ class SegmentationTrainer(ClientTrainer):
                         )
                     )
 
+            scheduler.step()
+
             if len(batch_loss) > 0:
                 epoch_loss.append(sum(batch_loss) / len(batch_loss))
                 logging.info(
@@ -102,54 +106,13 @@ class SegmentationTrainer(ClientTrainer):
         logging.info("Evaluating on Trainer ID: {}".format(self.id))
         model = self.model
         args = self.args
-        evaluator = Evaluator(model.n_classes)
 
         model.eval()
         model.to(device)
 
         t = time.time()
-        evaluator.reset()
-        test_acc = test_acc_class = test_mIoU = test_FWIoU = test_loss = test_total = 0.0
-        criterion = SegmentationLosses().build_loss(mode=args.loss_type)
 
-        with torch.no_grad():
-            for (batch_idx, batch) in enumerate(test_data):
-                x, target = batch["image"], batch["label"]
-                x, target = x.to(device), target.to(device)
-                output = model(x)
-                loss = criterion(output, target).to(device)
-                test_loss += loss.item()
-                test_total += target.size(0)
-                pred = output.cpu().numpy()
-                target = target.cpu().numpy()
-                pred = np.argmax(pred, axis=1)
-                evaluator.add_batch(target, pred)
-                if batch_idx % 100 == 0:
-                    logging.info(
-                        "Trainer_ID: {0} Iteration: {1}, Loss: {2}, Time Elapsed: {3}".format(
-                            self.id, batch_idx, loss, (time.time() - t) / 60
-                        )
-                    )
-
-                # time_end_test_per_batch = time.time()
-                # logging.info("time per batch = " + str(time_end_test_per_batch - time_start_test_per_batch))
-                # logging.info("Client = {0} Batch = {1}".format(self.client_index, batch_idx)
-
-        # Evaluation Metrics (Averaged over number of samples)
-        test_acc = evaluator.Pixel_Accuracy()
-        test_acc_class = evaluator.Pixel_Accuracy_Class()
-        test_mIoU = evaluator.Mean_Intersection_over_Union()
-        test_FWIoU = evaluator.Frequency_Weighted_Intersection_over_Union()
-        test_loss = test_loss / test_total
-
-        logging.info(
-            "Trainer_ID={0}, test_acc={1}, test_acc_class={2}, test_mIoU={3}, test_FWIoU={4}, test_loss={5}".format(
-                self.id, test_acc, test_acc_class, test_mIoU, test_FWIoU, test_loss
-            )
-        )
-
-        eval_metrics = EvaluationMetricsKeeper(test_acc, test_acc_class, test_mIoU, test_FWIoU, test_loss)
-        return eval_metrics
+        pass
 
     def test_on_the_server(self, train_data_local_dict, test_data_local_dict, device, args=None):
         pass
