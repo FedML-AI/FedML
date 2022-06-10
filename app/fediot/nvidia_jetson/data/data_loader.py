f3861e24e6297dca1d0a6bef46f46d70c7ede4c5
leigao97 <leig@usc.edu>
2022-06-10 15:05:01 -0700

    fix bug

--- app/fediot/nvidia_jetson/data/data_loader.py
+++ app/fediot/nvidia_jetson/data/data_loader.py
@@ -24,11 +24,11 @@ def load_data(args):
     train_data_local_num_dict = dict()
     train_data_num = 0
     test_data_num = 0
-    min_dataset = np.loadtxt("min_dataset.txt")
-    max_dataset = np.loadtxt("max_dataset.txt")
+    min_dataset = np.loadtxt(os.path.join(args.data_cache_dir, "min_dataset.txt"))
+    max_dataset = np.loadtxt(os.path.join(args.data_cache_dir, "max_dataset.txt"))
     for i, device in enumerate(device_list):
         benign_data = pd.read_csv(
-            os.path.join(args.data_dir, device, "benign_traffic.csv")
+            os.path.join(args.data_cache_dir, device, "benign_traffic.csv")
         )
         benign_data = benign_data[:5000]
         benign_data = np.array(benign_data)
@@ -36,16 +36,16 @@ def load_data(args):
         benign_data = (benign_data - min_dataset) / (max_dataset - min_dataset)
 
         g_attack_data_list = [
-            os.path.join(args.data_dir, device, "gafgyt_attacks", f)
-            for f in os.listdir(os.path.join(args.data_dir, device, "gafgyt_attacks"))
+            os.path.join(args.data_cache_dir, device, "gafgyt_attacks", f)
+            for f in os.listdir(os.path.join(args.data_cache_dir, device, "gafgyt_attacks"))
         ]
         if device == "Ennio_Doorbell" or device == "Samsung_SNH_1011_N_Webcam":
             attack_data_list = g_attack_data_list
         else:
             m_attack_data_list = [
-                os.path.join(args.data_dir, device, "mirai_attacks", f)
+                os.path.join(args.data_cache_dir, device, "mirai_attacks", f)
                 for f in os.listdir(
-                    os.path.join(args.data_dir, device, "mirai_attacks")
+                    os.path.join(args.data_cache_dir, device, "mirai_attacks")
                 )
             ]
             attack_data_list = g_attack_data_list + m_attack_data_list
