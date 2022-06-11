e3939a4d6ef076fc30ea01d9f0e0372e09a256ef
leigao97 <leig@usc.edu>
2022-06-10 17:51:37 -0700

    fix bug

--- app/fediot/nvidia_jetson/model/autoencoder.py
+++ app/fediot/nvidia_jetson/model/autoencoder.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self, output_dim):
+    def __init__(self, output_dim=115):
         super(AutoEncoder, self).__init__()
         self.enc = nn.Sequential(
             nn.Linear(output_dim, round(output_dim * 0.75)),
