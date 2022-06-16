536dda1346b94111ae626ea1e9ca90985505c384
zuluzazu <putugudu@gmail.com>
2022-06-16 08:52:26 +0000

    gpu debug seq2seq

--- app/fednlp/seq2seq/run_step_by_step_example.sh
+++ app/fednlp/seq2seq/run_step_by_step_example.sh
@@ -8,4 +8,4 @@ hostname > mpi_host_file
 
 $(which mpirun) -np $PROCESS_NUM \
 -hostfile mpi_host_file \
-/home/ubuntu/fednlp_migration/bin/python3.8 -m fednlp.seq2seq.torch_fedavg_20news_bert_step_by_step_example --cf fednlp/seq2seq/config/fedml_config.yaml
+/home/ubuntu/fedml/bin/python3.8 -m fednlp.seq2seq.torch_fedavg_20news_bert_step_by_step_example --cf fednlp/seq2seq/config/fedml_config.yaml
