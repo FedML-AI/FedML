cd37eba04944c46e184e0318c7a17edab644712b
Chaoyang He <chaoyanghe.com@gmail.com>
2022-06-19 22:27:03 -0700

    support run_id for github actions

--- python/tests/smoke_test/cross_silo/run_client.sh
+++ python/tests/smoke_test/cross_silo/run_client.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 RANK=$1
-python3 client/torch_client.py --cf config/fedml_config.yaml --rank $RANK
+RUN_ID=$2
+python3 client/torch_client.py --cf config/fedml_config.yaml --rank $RANK --run_id $RUN_ID
