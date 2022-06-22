3531038a71433457384ef2f5e19fdcfcd4994447
Alex <alex.gpt.llm@gmail.com>
2022-06-22 19:16:20 +0800

    add status manager and refactor mqtt manager.

---
+++ python/fedml/core/distributed/communication/constants.py
@@ -0,0 +1,10 @@
+
+
+class CommunicationConstants:
+    MSG_TYPE_CONNECTION_IS_READY = 0
+    MSG_CLIENT_STATUS_OFFLINE = "OFFLINE"
+    MSG_CLIENT_STATUS_IDLE = "IDLE"
+    CLIENT_TOP_LAST_WILL_MSG = "/flclient/last_will_msg"
+    CLIENT_TOP_ACTIVE_MSG = "/flclient/active"
+    SERVER_TOP_LAST_WILL_MSG = "/flserver/last_will_msg"
+    SERVER_TOP_ACTIVE_MSG = "/flserver/active"
