112e1b9ac0863f7d474260c154d6f02b55db8ac4
Tuo Zhang <tuozhang@usc.edu>
2022-06-22 12:20:00 -0700

    centralized version

---
+++ app/fediot/anomaly_detection_for_cybersecurity/torch_centralized_simulation.py
@@ -0,0 +1,26 @@
+import fedml
+from fedml.simulation import SimulatorMPI
+from data.data_loader import load_data
+from model.autoencoder import AutoEncoder
+from trainer.fed_detect_trainer import MyModelTrainer
+
+
+if __name__ == "__main__":
+    # init FedML framework
+    args = fedml.init()
+
+    # init device
+    device = fedml.device.get_device(args)
+
+    # load data
+    dataset, output_dim = load_data(args)
+
+    # load model
+    model = AutoEncoder(output_dim)
+
+    # create trainer
+    trainer = MyModelTrainer(model)
+
+    # start training
+    client = SimulatorMPI(args, device, dataset, model, trainer)
+    client.run()
