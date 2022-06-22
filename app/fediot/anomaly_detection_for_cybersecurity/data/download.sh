ba0a0e4bf569203be7b9761816367efafbe27564
Lei Gao <leig@usc.edu>
2022-06-22 12:10:48 -0700

    Delete download.sh

--- app/fediot/anomaly_detection_for_cybersecurity/data/download.sh
+++
@@ -1,5 +0,0 @@
-wget -r -np -R "index.html*" https://archive.ics.uci.edu/ml/machine-learning-databases/00442/
-rsync -a archive.ics.uci.edu/ml/machine-learning-databases/00442/* ./N-BaIoT
-rm -r archive.ics.uci.edu
-find ./N-BaIoT -name '*.rar' -execdir unar {} \; -exec rm {} \;
-python dataset.py
