2583f2b778f3a9ba12409104af6753e33f544c0b
alex.liang <alexliang.kh@gmail.com>
2022-06-28 19:03:44 +0800

    update cli.

--- python/fedml/cli/server_deployment/server_runner.py
+++ python/fedml/cli/server_deployment/server_runner.py
@@ -62,9 +62,9 @@ class FedMLServerRunner:
         self.client_mqtt_lock = None
         self.unique_device_id = None
         self.edge_id = 0
-        self.cloud_agent_id = None
+        self.server_id = 0
         if request_json is not None:
-            self.cloud_agent_id = request_json.get("cloud_agent_id", 0)
+            self.server_id = request_json.get("serverId", 0)
         self.process = None
         self.args = args
         self.request_json = copy.deepcopy(request_json)
@@ -251,7 +251,6 @@ class FedMLServerRunner:
         fedml_conf_object["train_args"]["client_id_list"] = package_dynamic_args["client_id_list"]
         fedml_conf_object["train_args"]["client_num_in_total"] = int(package_dynamic_args["client_num_in_total"])
         fedml_conf_object["train_args"]["client_num_per_round"] = int(package_dynamic_args["client_num_in_total"])
-        fedml_conf_object["train_args"]["cloud_agent_id"] = self.cloud_agent_id
         fedml_conf_object["device_args"]["worker_num"] = int(package_dynamic_args["client_num_in_total"])
         fedml_conf_object["data_args"]["data_cache_dir"] = package_dynamic_args["data_cache_dir"]
         fedml_conf_object["tracking_args"]["log_file_dir"] = package_dynamic_args["log_file_dir"]
@@ -463,7 +462,6 @@ class FedMLServerRunner:
         self.running_request_json[str(run_id)] = request_json
 
         if self.run_as_edge_server_and_agent:
-            request_json["cloud_agent_id"] = self.edge_id
             server_runner = FedMLServerRunner(self.args, run_id=run_id,
                                               request_json=request_json,
                                               agent_config=self.agent_config)
@@ -472,7 +470,6 @@ class FedMLServerRunner:
             server_process.start()
             FedMLServerRunner.save_run_process(server_process.pid)
         elif self.run_as_cloud_agent:
-            request_json["cloud_agent_id"] = self.edge_id
             server_runner = FedMLServerRunner(self.args, run_id=run_id,
                                               request_json=request_json,
                                               agent_config=self.agent_config)
@@ -481,7 +478,7 @@ class FedMLServerRunner:
             server_process.start()
             FedMLServerRunner.save_run_process(server_process.pid)
         elif self.run_as_cloud_server:
-            self.cloud_agent_id = request_json.get("cloud_agent_id", 0)
+            self.server_id = request_json.get("serverId", 0)
             self.run()
 
     def start_cloud_server_process(self):
@@ -613,7 +610,7 @@ class FedMLServerRunner:
             self.mlops_metrics.set_messenger(self.client_mqtt_mgr)
             self.mlops_metrics.run_id = self.run_id
             self.mlops_metrics.edge_id = self.edge_id
-            self.mlops_metrics.cloud_agent_id = self.cloud_agent_id
+            self.mlops_metrics.server_id = self.server_id
 
         logging.info("on_client_mqtt_connected: {}.".format(self.client_mqtt_is_connected))
 
@@ -1004,7 +1001,6 @@ class FedMLServerRunner:
             self.mlops_metrics.set_messenger(self.mqtt_mgr)
             self.mlops_metrics.run_id = self.run_id
             self.mlops_metrics.edge_id = self.edge_id
-            self.mlops_metrics.cloud_agent_id = self.edge_id
             self.mlops_metrics.report_server_training_status(self.edge_id, ServerConstants.MSG_MLOPS_SERVER_STATUS_IDLE)
 
         # Setup MQTT message listener for starting training
