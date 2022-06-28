e09523f9f6348e00642bd0be942e35833a67a101
Alex <alex.gpt.llm@gmail.com>
2022-06-28 20:17:11 +0800

    update cli.

--- python/fedml/core/mlops/mlops_runtime_log.py
+++ python/fedml/core/mlops/mlops_runtime_log.py
@@ -46,8 +46,8 @@ class MLOpsRuntimeLog:
         self.log_file = None
         self.run_id = args.run_id
         if args.rank == 0:
-            if hasattr(args, "server_id"):
-                self.edge_id = args.server_id
+            if hasattr(args, "server_agent_id"):
+                self.edge_id = args.server_agent_id
             else:
                 self.edge_id = 0
         else:
@@ -117,8 +117,8 @@ class MLOpsRuntimeLog:
     def build_log_file_path(args):
         if args.rank == 0:
             edge_id = 0
-            if hasattr(args, "server_id"):
-                edge_id = args.server_id
+            if hasattr(args, "server_agent_id"):
+                edge_id = args.server_agent_id
             program_prefix = "FedML-Server({}) @device-id-{}".format(args.rank, edge_id)
         else:
             edge_id = json.loads(args.client_id_list)[0]
