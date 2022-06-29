706ba359f637864a60c987456b9ec35cdcd23377
emirceyani <emirceyani@gmail.com>
2022-06-29 22:44:27 +0000

    Update relation prediction config

--- app/fedgraphnn/subgraph_relation_pred/data/data_loader.py
+++ app/fedgraphnn/subgraph_relation_pred/data/data_loader.py
@@ -11,29 +11,22 @@ from .utils import DefaultCollator, WalkForestCollator
 from fedml.core import partition_class_samples_with_dirichlet_distribution
 
 def get_data_community(path, data, pred_task, algo):
-    assert pred_task in ["relation", "link"]
+    assert pred_task in ["relation"]
     assert data in ["wn18rr", "FB15k-237", "YAGO3-10"]
 
-    if pred_task == "relation" and algo == "Louvain":
-        # for relation type prediction
-        subdir = "subgraphs_byLouvain"
-        if data == "wn18rr":
-            num_of_classes = 11
-        if data == "FB15k-237":
-            num_of_classes = 237
-        if data == "YAGO3-10":
-            num_of_classes = 37
+    if data == "wn18rr":
+        num_of_classes = 11
+    if data == "FB15k-237":
+        num_of_classes = 237
+    if data == "YAGO3-10":
+        num_of_classes = 37
 
-    if pred_task == "link":
-        # for link prediction with communities grouped by the relation type
-        subdir = "subgraphs_byRelType"
-        num_of_classes = 2
 
     graphs_train = pickle.load(
-        open(os.path.join(path, data, subdir, "train.pkl"), "rb")
+        open(os.path.join(path, data, "train.pkl"), "rb")
     )
-    graphs_val = pickle.load(open(os.path.join(path, data, subdir, "valid.pkl"), "rb"))
-    graphs_test = pickle.load(open(os.path.join(path, data, subdir, "test.pkl"), "rb"))
+    graphs_val = pickle.load(open(os.path.join(path, data, "valid.pkl"), "rb"))
+    graphs_test = pickle.load(open(os.path.join(path, data, "test.pkl"), "rb"))
 
     # number of graphs == number of relation type
     return graphs_train, graphs_val, graphs_test, num_of_classes
