9bd349aff0ac9d2f1fc82f143a80cd4ea463c09d
emirceyani <emirceyani@gmail.com>
2022-06-29 23:06:31 +0000

    Remove subgraph relation and link prediction

--- app/fedgraphnn/subgraph_link_pred/data/FB15k-237/download_and_unzip_FB15k-237.sh
+++
@@ -1,5 +0,0 @@
-wget --no-check-certificate --no-proxy https://fedmol.s3.us-west-1.amazonaws.com/datasets/subgraph_level/FB15k-237/FB15k-237.zip && \
-unzip FB15k-237.zip && rm FB15k-237.zip && \
-mv ./FB15k-237/* ./ && \
-rm -rf __MACOSX && \
-rm -rf FB15k-237
