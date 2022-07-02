1f0cd930c5e323242436e70fec05450e051af5d4
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-02 17:26:17 -0400

    Update git_commit_all_submodules.sh

--- android/git_commit_all_submodules.sh
+++ android/git_commit_all_submodules.sh
@@ -1,35 +1,44 @@
 # commit MNN
 cd fedmlsdk/MobileNN/MNN
 git add -A
-git commit -m "update MNN"
+git commit -m "commit changes in MNN"
 git push
 cd ../../../
 
+
 # commit MNN submodule changes in MobileNN
 cd fedmlsdk/MobileNN
+git submodule update --remote
 git add MNN
-git commit -m "updated MNN submodule"
+git commit -m "updated MNN submodule in MobileNN"
 git push
 cd ../../
 
 # commit MobileNN
 cd fedmlsdk/MobileNN
 git add -A
-git commit -m "submit Mobile MNN changes"
+git commit -m "commit changes in MobileNN"
 git push
 cd ../../
 
 # commit MobileNN submodule changes in fedmlsdk
 cd fedmlsdk
+git submodule update --remote
 git add MobileNN
-git commit -m "updated MobileNN submodule"
+git commit -m "commit MobileNN submodule changes in fedmlsdk"
 git push
 cd ..
 
 # commit fedmlsdk
-git pull
-git add fedmlsdk
-git commit -m "updated fedmlsdk submodule"
+cd fedmlsdk
+git add ./
+git commit -m "commit fedmlsdk"
 git push
+cd ..
 
 
+# commit fedmlsdk submodule changes in android
+git submodule update --remote
+git add fedmlsdk
+git commit -m "commit fedmlsdk submodule changes in android"
+git push
