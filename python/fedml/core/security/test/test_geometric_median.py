53809b89038b25c08668d2585b0de930d98945eb
Shanshan <sshan0731@hotmail.com>
2022-07-04 01:46:21 -0700

    attack & defense: geometric_median - update + reformat

--- python/fedml/core/security/test/test_geometric_median.py
+++ python/fedml/core/security/test/test_geometric_median.py
@@ -1,4 +1,10 @@
-from fedml.core.security.defense.geometric_median import GeometricMedian
+import torch
+
+from fedml.core.security.defense.geometric_median import (
+    GeometricMedian,
+    _compute_middle_point,
+    _compute_geometric_median,
+)
 from fedml.core.security.test.utils import create_fake_model_list
 
 
@@ -10,9 +16,29 @@ def test_defense():
     gm = _get_geometric_median_obj()
     model_list = create_fake_model_list(gm.client_num_per_round)
     val = gm.defend(model_list)
-    print(val)
+    print(f"val={val}")
 
 
+def test__compute_middle_point():
+    alphas = [0.5, 0.5]
+    batch_w = [
+        torch.FloatTensor([[1, 0, 1], [2, 2, 2], [1, 1, 1]]),
+        torch.FloatTensor([[1, 1, 1], [0, 0, 0], [1, 1, 1]]),
+    ]
+    print(f"middle_point = {_compute_middle_point(alphas, batch_w)}")
 
-if __name__ == '__main__':
+
+def test__compute_geometric_median():
+    alphas = [1, 1, 1]
+    batch_w = [
+        torch.FloatTensor([[1, 0, 1], [2, 2, 2], [1, 1, 1]]),
+        torch.FloatTensor([[1, 1, 1], [10, 10, 0], [1, 1, 1]]),
+        torch.FloatTensor([[2, 2, 2], [2, 2, 2], [1, 2, 1]]),
+    ]
+    print(f"_compute_geometric_median = {_compute_geometric_median(alphas, batch_w)}")
+
+
+if __name__ == "__main__":
     test_defense()
+    # test__compute_middle_point()
+    # test__compute_geometric_median()
