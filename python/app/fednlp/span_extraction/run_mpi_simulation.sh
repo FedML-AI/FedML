17da6d44600b403e5102a6bb7a0033326e7eb686
zuluzazu <putugudu@gmail.com>
2022-07-06 20:15:19 +0000

    add span extraction mlops

--- python/app/fednlp/span_extraction/run_mpi_simulation.sh
+++ python/app/fednlp/span_extraction/run_mpi_simulation.sh
@@ -8,4 +8,4 @@ hostname > mpi_host_file
 
 $(which mpirun) -np $PROCESS_NUM \
 -hostfile mpi_host_file \
-/home/ubuntu/fednlp_migration/bin/python3.8 -m fednlp.span_extraction.torch_fedavg_20news_bert_step_by_step_example --cf fednlp/span_extraction/config/fedml_config.yaml
+/home/ubuntu/fednlp_migration/bin/python3.8 -m fednlp.span_extraction.torch_mpi_simulation --cf fednlp/span_extraction/config/fedml_config.yaml
