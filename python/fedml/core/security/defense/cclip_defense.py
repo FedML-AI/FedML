501593e4f63e4be24a081de807a32ffdfdafb1a8
Xiaoyang-Wang <xw28@illinois.edu>
2022-07-10 14:24:46 -0500

    add self-centered-clipping (cclip) defense

---
+++ python/fedml/core/security/defense/cclip_defense.py
@@ -0,0 +1,41 @@
+import torch
+
+from .defense_base import BaseDefenseMethod
+from ..common import utils
+
+"""
+defense @ server, added by Xiaoyang, 07/10/2022
+"Byzantine-Robust Learning on Heterogeneous Datasets via Bucketing"
+https://arxiv.org/pdf/2006.09365.pdf
+"""
+
+
+class CClipDefense(BaseDefenseMethod):
+    def __init__(self, tau):
+        self.tau = tau  # clipping raduis
+
+    def defend(self, local_w, refs):
+        num_client = len(local_w)
+        vec_local_w = [(local_w[i][0], utils.vectorize_weight(local_w[i][1])) for i in range(0, num_client)]
+        print(vec_local_w)
+        vec_refs = utils.vectorize_weight(refs)
+        cclip_score = self._compute_cclip_score(vec_local_w, vec_refs)
+
+        _, averaged_params = local_w[0]
+        for k in averaged_params.keys():
+            averaged_params[k] = refs[k]
+            for i in range(0, num_client):
+                _, local_model_params = local_w[i]
+                averaged_params[k] += (local_model_params[k] - refs[k]) * cclip_score[i]
+
+        return averaged_params, cclip_score
+
+    def _compute_cclip_score(self, local_w, refs):
+        cclip_score = []
+        num_client = len(local_w)
+        for i in range(0, num_client):
+            dist = utils.compute_euclidean_distance(local_w[i][1], refs).item() + 1e-8
+            score = min(1, self.tau / dist)
+            cclip_score.append(score)
+
+        return cclip_score
