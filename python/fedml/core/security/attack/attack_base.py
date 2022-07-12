b8ec7248e867e1f90565127c692f7179831e5d34
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-11 21:21:49 -0400

    update base classes for attack&defense

--- python/fedml/core/security/attack/attack_base.py
+++ python/fedml/core/security/attack/attack_base.py
@@ -1,11 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Dict
+
+from torch.utils.data import DataLoader
 
 
 class BaseAttackMethod(ABC):
     @abstractmethod
-    def attack_model(self, local_w, global_w, refs=None):
+    def attack_model(
+        self, local_weights: Dict, global_weights: Dict, refs=None
+    ) -> (Dict, Dict):
         pass
 
     @abstractmethod
-    def attack_data(self, train_data_loader, test_data_loader):
+    def poison_data(
+        self, train_data_loader: DataLoader, test_data_loader: DataLoader
+    ) -> (DataLoader, DataLoader):
         pass
