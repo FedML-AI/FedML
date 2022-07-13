38399988727cd88219507344cbbd732bb3f2d9b1
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-13 03:42:25 -0400

    FedRunner and cod refactoring for FedNLP

--- python/app/fednlp/span_extraction/run_client.sh
+++ python/app/fednlp/span_extraction/run_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 RANK=$1
-/home/ubuntu/fednlp_migration/bin/python3.8 torch_client.py --cf config/fedml_config.yaml --rank $RANK
+/home/ubuntu/fednlp_migration/bin/python torch_main.py --cf config/fedml_config.yaml --rank $RANK --role client
