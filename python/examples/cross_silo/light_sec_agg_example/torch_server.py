a4745a4047bb407d5c0e7d5f17ad5aef03b21e27
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-12 23:01:30 -0400

    FedMLRunner support for LightSecAgg

--- python/examples/cross_silo/light_sec_agg_example/torch_server.py
+++ python/examples/cross_silo/light_sec_agg_example/torch_server.py
@@ -1,7 +1,7 @@
 import torch
 
 import fedml
-from fedml.cross_silo import Server
+from fedml import FedMLRunner
 from fedml.data.MNIST.data_loader import download_mnist, load_partition_data_mnist
 
 
@@ -72,5 +72,5 @@ if __name__ == "__main__":
     model = LogisticRegression(28 * 28, output_dim)
 
     # start training
-    server = Server(args, device, dataset, model)
-    server.run()
+    fedml_runner = FedMLRunner(args, device, dataset, model)
+    fedml_runner.run()
