c31c43920c5d7c94e3e302539d146e41736cd4a6
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-13 03:56:47 -0400

    add FedMLRunner for Object Detection

--- python/app/fedcv/object_detection/run_simulation.sh
+++ python/app/fedcv/object_detection/run_simulation.sh
@@ -4,4 +4,4 @@ PROCESS_NUM=`expr $WORKER_NUM + 1`
 echo $PROCESS_NUM
 hostname > mpi_host_file
 $(which mpirun) -np $PROCESS_NUM \
-python torch_fedml_mpi_object_detection.py --cf config/fedml_object_detection.yaml
+python main_fedml_object_detection.py --cf config/fedml_object_detection.yaml
