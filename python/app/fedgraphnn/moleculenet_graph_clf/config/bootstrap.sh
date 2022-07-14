11f9938fafdcc6c6d3423727e6c3f0aa4d845caf
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-14 19:29:06 -0400

    ~path for fedgraphnn

--- python/app/fedgraphnn/moleculenet_graph_clf/config/bootstrap.sh
+++ python/app/fedgraphnn/moleculenet_graph_clf/config/bootstrap.sh
@@ -1,9 +1,9 @@
-conda install pyg -c pyg
-pip install torch-scatter -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-geometric
+#conda install pyg -c pyg
+#pip install torch-scatter -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+#pip install torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+#pip install torch-geometric
 
-DATA_PATH=~/fedgraphnn
+DATA_PATH=~/fedgraphnn_data/clintox
 wget --no-check-certificate --no-proxy -P $DATA_PATH https://fedmol.s3-us-west-1.amazonaws.com/datasets/clintox/clintox.zip &&
 cd $DATA_PATH &&
 unzip clintox.zip && rm clintox.zip
