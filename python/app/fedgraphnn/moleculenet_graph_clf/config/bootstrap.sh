2f52797942d2392afb054ec0a0831293976a0377
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-15 16:52:57 -0400

    bug fix for fedgraphnn

--- python/app/fedgraphnn/moleculenet_graph_clf/config/bootstrap.sh
+++ python/app/fedgraphnn/moleculenet_graph_clf/config/bootstrap.sh
@@ -1,9 +1,11 @@
 #CPU installation
 
 pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.0+cpu.html
-DATA_PATH= ../data/clintox
-wget --no-check-certificate --no-proxy -P $DATA_PATH https://fedmol.s3-us-west-1.amazonaws.com/datasets/clintox/clintox.zip &&
-cd $DATA_PATH &&
+
+BASE_DATA_PATH=~/fedgraphnn_data
+DATASET=clintox
+wget --no-check-certificate --no-proxy -P $BASE_DATA_PATH/$DATASET https://fedmol.s3-us-west-1.amazonaws.com/datasets/clintox/clintox.zip &&
+cd $BASE_DATA_PATH/$DATASET &&
 unzip clintox.zip && rm clintox.zip
 
 
