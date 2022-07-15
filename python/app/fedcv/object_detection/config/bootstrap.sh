22a11fcd9c70f409281097f586236e78d8e47180
beiyuouo <bj.yan.pa@qq.com>
2022-07-15 11:21:44 +0800

    build(fedcv): update yolov5

--- python/app/fedcv/object_detection/config/bootstrap.sh
+++ python/app/fedcv/object_detection/config/bootstrap.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-pip install fedml
-pip install matplotlib numpy opencv-python Pillow PyYAML scipy torch torchvision tqdm tensorboard seaborn pandas onnx onnxruntime onnx-simplifier thop pycocotools
-PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
-cd $PROJECT_DIR
-bash ./data/coco128/download_coco128_mlops.sh
+pip install opencv-python pandas matplotlib seaborn addict mpi4py
