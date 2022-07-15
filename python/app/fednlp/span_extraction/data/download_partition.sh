871dfd2e50bfff2d33a60fe663f7f02757dc78e6
zuluzazu <putugudu@gmail.com>
2022-07-15 20:33:58 +0000

    [fednlp] correct data paths

--- python/app/fednlp/span_extraction/data/download_partition.sh
+++ python/app/fednlp/span_extraction/data/download_partition.sh
@@ -5,7 +5,7 @@ rm -rf ${DATA_DIR}/partition_files
 #declare -a data_names=("20news" "agnews" "cnn_dailymail" "cornell_movie_dialogue" "semeval_2010_task8" "sentiment140" "squad_1.1" "sst_2" "ploner" "wikiner" "wmt_cs-en" "wmt_de-en" "wmt_ru-en" "wmt_zh-en" "mrqa" "onto" "gigaword")
 
 # declare -a data_names=("mrqa" "20news" "squad_1.1" "onto" "gigaword" "wikiner" "cornell_movie_dialogue")
-declare -a data_names=("20news")
+declare -a data_names=("mrqa")
 
 mkdir ${DATA_DIR}
 mkdir ${DATA_DIR}/partition_files
