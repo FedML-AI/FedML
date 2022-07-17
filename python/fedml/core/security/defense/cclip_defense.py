e778ba1a8432c2e8fedf71d020ac9bce02cf0f2f
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-17 14:37:55 -0400

    refactor cclip defense

--- python/fedml/core/security/defense/cclip_defense.py
+++ python/fedml/core/security/defense/cclip_defense.py
@@ -3,6 +3,7 @@ from typing import Callable, List, Tuple, Dict, Any
 
 from .defense_base import BaseDefenseMethod
 from ..common import utils
+from ..common.bucket import Bucket
 
 """
 defense @ server, added by Xiaoyang, 07/10/2022
@@ -20,8 +21,26 @@ class CClipDefense(BaseDefenseMethod):
 
     def run(self, base_aggregation_func: Callable, raw_client_grad_list: List[Tuple[int, Dict]],
             extra_auxiliary_info: Any = None) -> Dict:
-        avg_params = base_aggregation_func(raw_client_grad_list)
-        initial_guess = self._compute_an_initial_guess(raw_client_grad_list)
+        client_grad_buckets = Bucket.bucketization(raw_client_grad_list, self.bucket_size)
+        self.initial_guess = self._compute_an_initial_guess(client_grad_buckets)
+        bucket_num = len(client_grad_buckets)
+        vec_local_w = [
+            (client_grad_buckets[i][0], utils.vectorize_weight(client_grad_buckets[i][1]))
+            for i in range(bucket_num)
+        ]
+        vec_refs = utils.vectorize_weight(self.initial_guess)
+        cclip_score = self._compute_cclip_score(vec_local_w, vec_refs)
+        new_grad_list = []
+        for i in range(bucket_num):
+            tuple = dict()
+            sample_num, bucket_params = client_grad_buckets[i]
+            for k in bucket_params.keys():
+                tuple[k] = (bucket_params[k] - self.initial_guess[k]) * cclip_score[i]
+            new_grad_list.append((sample_num, tuple))
+        print(f"cclip_score = {cclip_score}")
+
+        avg_params = base_aggregation_func(new_grad_list)
+        initial_guess = self._compute_an_initial_guess(new_grad_list)
         for k in avg_params.keys():
             avg_params[k] = initial_guess[k] + avg_params[k]
         return avg_params
