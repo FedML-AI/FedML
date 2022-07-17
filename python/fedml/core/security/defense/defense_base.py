ab5d68ab8f3f3b7b440ff2397aab2aa2e28ddd26
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-17 14:37:55 -0400

    refactor cclip defense

--- python/fedml/core/security/defense/defense_base.py
+++ python/fedml/core/security/defense/defense_base.py
@@ -21,19 +21,6 @@ class BaseDefenseMethod(ABC):
         """
         pass
 
-
-    @abstractmethod
-    def defend(
-        self, client_grad_list: List[Tuple[int, Dict]], global_w=None
-    ) -> List[Tuple[int, Dict]]:
-        """
-        args:
-            client_grad_list is a list, each item is (sample_num, gradients)
-        return:
-            Note: the data type of the return variable should be the same as the input
-        """
-        pass
-
     def robust_aggregate(
         self, client_grad_list: List[Tuple[int, Dict]], global_w=None
     ) -> Dict:
--- python/fedml/core/security/test/defense/test_cclip.py
+++ python/fedml/core/security/test/defense/test_cclip.py
@@ -1,4 +1,3 @@
-
 from fedml.core.security.defense.cclip_defense import CClipDefense
 from fedml.core.security.test.aggregation.aggregation_functions import AggregationFunction
 from fedml.core.security.test.utils import create_fake_model_list
@@ -7,17 +6,9 @@ from fedml.core.security.test.utils import create_fake_model_list
 def test_defense():
     client_grad_list = create_fake_model_list(20)
     cclip = CClipDefense(tau=10, bucket_size=3)
-    new_grad_list = cclip.defend(client_grad_list, global_w=None)
-    print(f"new_grad_list={new_grad_list}")
-
-
-def test_robustify_global_model():
-    client_grad_list = create_fake_model_list(20)
-    cclip = CClipDefense(tau=10, bucket_size=3)
     result = cclip.run(AggregationFunction.FedAVG, client_grad_list)
     print(f"result = {result}")
 
 
 if __name__ == "__main__":
     test_defense()
-    test_robustify_global_model()
