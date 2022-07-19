99c83e12a068127938a6a3514f13cacc882d8968
beiyuouo <bj.yan.pa@qq.com>
2022-07-19 23:54:37 +0800

    build(healthcare): flamby fed-kits19

---
+++ python/app/healthcare/flamby/run_simulation.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+WORKER_NUM=$1
+PROCESS_NUM=`expr $WORKER_NUM + 1`
+echo $PROCESS_NUM
+hostname > mpi_host_file
+$(which mpirun) -np $PROCESS_NUM \
+python main_fedml_flamby.py --cf config/simulation/fedml_config.yaml
