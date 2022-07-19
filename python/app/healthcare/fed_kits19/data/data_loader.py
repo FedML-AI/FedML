2ecd025b8262cb7c6698dfc6292f751912ac9680
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-19 11:51:18 -0500

    code review for healthcare (bingjie <> chaoyang)

--- python/app/healthcare/fed_kits19/data/data_loader.py
+++ python/app/healthcare/fed_kits19/data/data_loader.py
@@ -1,27 +1,3 @@
-import argparse
-import copy
-import os
-import time
-
-import numpy as np
-import torch
-from torch.optim import lr_scheduler
-
-
-import flamby
-
-from flamby.datasets.fed_kits19 import (
-    BATCH_SIZE,
-    LR,
-    NUM_EPOCHS_POOLED,
-    Baseline,
-    BaselineLoss,
-    FedKits19,
-    evaluate_dice_on_tests,
-    metric,
-    softmax_helper,
-)
-from flamby.utils import check_dataset_from_config
 from .fed_kits19 import load_partition_fed_kits19
 
 
