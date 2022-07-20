aec8a6af0d9ab0d83af1a9c5b8731241f3298d88
wizard1203 <956234606@qq.com>
2022-07-20 21:50:52 +0800

    async fedavg test

--- python/fedml/simulation/mpi/async_fedavg/AsyncFedAVGAggregator.py
+++ python/fedml/simulation/mpi/async_fedavg/AsyncFedAVGAggregator.py
@@ -5,10 +5,17 @@ import time
 import numpy as np
 import torch
 import wandb
+import collections
+
 from .utils import transform_list_to_tensor
 from ....core.security.fedml_defender import FedMLDefender
 
-class FedAVGAggregator(object):
+from ....core.schedule.scheduler import scheduler
+from ....core.schedule.runtime_estimate import t_sample_fit
+
+
+
+class AsyncFedAVGAggregator(object):
     def __init__(
         self,
         train_global,
@@ -41,79 +48,75 @@ class FedAVGAggregator(object):
         self.flag_client_model_uploaded_dict = dict()
         for idx in range(self.worker_num):
             self.flag_client_model_uploaded_dict[idx] = False
+        self.runtime_history = {}
+        for i in range(self.worker_num):
+            self.runtime_history[i] = {}
+            for j in range(self.args.client_num_in_total):
+                self.runtime_history[i][j] = []
+        self.model_weights = self.trainer.get_model_params()
+        self.client_running_status = np.array([])
+
 
     def get_global_model_params(self):
-        return self.trainer.get_model_params()
+        # return self.trainer.get_model_params()
+        return self.model_weights
 
     def set_global_model_params(self, model_parameters):
         self.trainer.set_model_params(model_parameters)
 
-    def add_local_trained_result(self, index, model_params, sample_num):
+    def add_local_trained_result(self, index, model_params, local_sample_number,
+            current_round, client_round):
         logging.info("add_model. index = %d" % index)
+
+        self.client_running_status = np.setdiff1d(self.client_running_status,
+                    np.array([index]), assume_unique=False) 
+        client_staleness = current_round - client_round
+        weight = 1. / (1 + client_staleness)
+
         self.model_dict[index] = model_params
-        self.sample_num_dict[index] = sample_num
-        self.flag_client_model_uploaded_dict[index] = True
+        # self.sample_num_dict[index] = sample_num
+        for name, param in self.model_weights.items():
+            self.model_weights[name] += model_params[name] * weight
 
-    def check_whether_all_receive(self):
-        logging.debug("worker_num = {}".format(self.worker_num))
-        for idx in range(self.worker_num):
-            if not self.flag_client_model_uploaded_dict[idx]:
-                return False
-        for idx in range(self.worker_num):
-            self.flag_client_model_uploaded_dict[idx] = False
-        return True
 
-    def aggregate(self):
-        start_time = time.time()
-        model_list = []
+    def client_schedule(self, round_idx, client_indexes, mode="simulate"):
+        self.runtime_history = {}
+        for i in range(self.worker_num):
+            self.runtime_history[i] = {}
+            for j in range(self.args.client_num_in_total):
+                self.runtime_history[i][j] = []
+
+        fit_params, fit_funcs, fit_errors = t_sample_fit(
+            self.worker_num, self.args.client_num_in_total, self.runtime_history, 
+            self.train_data_local_num_dict, uniform_client=True, uniform_gpu=False)
+
+        client_schedule = np.array_split(client_indexes, self.worker_num)
+        return client_schedule
+
+
+    def get_average_weight(self, client_indexes):
+        average_weight_dict = {}
         training_num = 0
+        for client_index in client_indexes:
+            training_num += self.train_data_local_num_dict[client_index]
 
-        for idx in range(self.worker_num):
-            if self.args.is_mobile == 1:
-                self.model_dict[idx] = transform_list_to_tensor(self.model_dict[idx])
+        for client_index in client_indexes:
+            average_weight_dict[client_index] = self.train_data_local_num_dict[client_index] / training_num
+        return average_weight_dict
 
-            # added for attack & defense; enable multiple defenses
-            if FedMLDefender.get_instance().is_defense_enabled():
-                self.model_dict[idx] = FedMLDefender.get_instance().defend(
-                    self.model_dict[idx], self.get_global_model_params()
-                )
-
-            model_list.append((self.sample_num_dict[idx], self.model_dict[idx]))
-            training_num += self.sample_num_dict[idx]
-        logging.info("len of self.model_dict[idx] = " + str(len(self.model_dict)))
-
-        # logging.info("################aggregate: %d" % len(model_list))
-        (num0, averaged_params) = model_list[0]
-        for k in averaged_params.keys():
-            for i in range(0, len(model_list)):
-                local_sample_number, local_model_params = model_list[i]
-                w = local_sample_number / training_num
-                if i == 0:
-                    averaged_params[k] = local_model_params[k] * w
-                else:
-                    averaged_params[k] += local_model_params[k] * w
-
-        # update the global model which is cached at the server side
-        self.set_global_model_params(averaged_params)
-
-        end_time = time.time()
-        logging.info("aggregate time cost: %d" % (end_time - start_time))
-        return averaged_params
 
     def client_sampling(self, round_idx, client_num_in_total, client_num_per_round):
-        if client_num_in_total == client_num_per_round:
-            client_indexes = [
-                client_index for client_index in range(client_num_in_total)
-            ]
-        else:
-            num_clients = min(client_num_per_round, client_num_in_total)
-            np.random.seed(
-                round_idx
-            )  # make sure for each comparison, we are selecting the same clients each round
-            client_indexes = np.random.choice(
-                range(client_num_in_total), num_clients, replace=False
-            )
+        num_clients = min(client_num_per_round, client_num_in_total)
+        np.random.seed(
+            round_idx
+        )  # make sure for each comparison, we are selecting the same clients each round
+        sleep_clients = np.setdiff1d(np.array(range(client_num_in_total)),
+                    self.client_running_status, assume_unique=False) 
+        client_indexes = np.random.choice(
+            sleep_clients, 1, replace=False
+        )
         logging.info("client_indexes = %s" % str(client_indexes))
+        self.client_running_status = np.concatenate((self.client_running_status, client_indexes))
         return client_indexes
 
     def _generate_validation_set(self, num_samples=10000):
@@ -143,34 +146,35 @@ class FedAVGAggregator(object):
             round_idx % self.args.frequency_of_the_test == 0
             or round_idx == self.args.comm_round - 1
         ):
+            self.set_global_model_params(self.model_weights)
             logging.info(
                 "################test_on_server_for_all_clients : {}".format(round_idx)
             )
             train_num_samples = []
             train_tot_corrects = []
             train_losses = []
-            for client_idx in range(self.args.client_num_in_total):
-                # train data
-                metrics = self.trainer.test(
-                    self.train_data_local_dict[client_idx], self.device, self.args
-                )
-                train_tot_correct, train_num_sample, train_loss = (
-                    metrics["test_correct"],
-                    metrics["test_total"],
-                    metrics["test_loss"],
-                )
-                train_tot_corrects.append(copy.deepcopy(train_tot_correct))
-                train_num_samples.append(copy.deepcopy(train_num_sample))
-                train_losses.append(copy.deepcopy(train_loss))
+            # for client_idx in range(self.args.client_num_in_total):
+            #     # train data
+            #     metrics = self.trainer.test(
+            #         self.train_data_local_dict[client_idx], self.device, self.args
+            #     )
+            #     train_tot_correct, train_num_sample, train_loss = (
+            #         metrics["test_correct"],
+            #         metrics["test_total"],
+            #         metrics["test_loss"],
+            #     )
+            #     train_tot_corrects.append(copy.deepcopy(train_tot_correct))
+            #     train_num_samples.append(copy.deepcopy(train_num_sample))
+            #     train_losses.append(copy.deepcopy(train_loss))
 
             # test on training dataset
-            train_acc = sum(train_tot_corrects) / sum(train_num_samples)
-            train_loss = sum(train_losses) / sum(train_num_samples)
-            if self.args.enable_wandb:
-                wandb.log({"Train/Acc": train_acc, "round": round_idx})
-                wandb.log({"Train/Loss": train_loss, "round": round_idx})
-            stats = {"training_acc": train_acc, "training_loss": train_loss}
-            logging.info(stats)
+            # train_acc = sum(train_tot_corrects) / sum(train_num_samples)
+            # train_loss = sum(train_losses) / sum(train_num_samples)
+            # if self.args.enable_wandb:
+            #     wandb.log({"Train/Acc": train_acc, "round": round_idx})
+            #     wandb.log({"Train/Loss": train_loss, "round": round_idx})
+            # stats = {"training_acc": train_acc, "training_loss": train_loss}
+            # logging.info(stats)
 
             # test data
             test_num_samples = []
