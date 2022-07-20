3d79cc23271b7e93890b6a12e3a17fba6fdb08af
Emir Ceyani <emirceyani@gmail.com>
2022-07-19 22:29:15 -0700

    fix mlops script for ego networks link prediction

--- python/app/fedgraphnn/ego_networks_link_pred/build_mlops_pkg.sh
+++ python/app/fedgraphnn/ego_networks_link_pred/build_mlops_pkg.sh
@@ -1,12 +1,12 @@
 SOURCE=.
-ENTRY=fedml_ego_networks_node_clf.py
+ENTRY=fedml_ego_networks_link_pred.py
 CONFIG=config
 DEST=./mlops
 fedml build -t client -sf $SOURCE -ep $ENTRY -cf $CONFIG -df $DEST
 
 
 SOURCE=.
-ENTRY=fedml_ego_networks_node_clf.py
+ENTRY=fedml_ego_networks_link_pred.py
 CONFIG=config
 DEST=./mlops
 fedml build -t server -sf $SOURCE -ep $ENTRY -cf $CONFIG -df $DEST
