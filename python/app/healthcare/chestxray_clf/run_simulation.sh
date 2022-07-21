04b8ec8fbdd852c007c40c76c9429056f945407a
beiyuouo <bj.yan.pa@qq.com>
2022-07-21 10:15:34 +0800

    build(healthcare): chestxray

--- python/app/healthcare/chestxray_clf/run_simulation.sh
+++ python/app/healthcare/chestxray_clf/run_simulation.sh
@@ -9,4 +9,4 @@ hostname > mpi_host_file
 
 $(which mpirun) -np $PROCESS_NUM \
 -hostfile mpi_host_file \
-python torch_medical_chest_xray_image_clf.py --cf config/fedml_config.yaml
+python main_fedml_chestxray_clf.py --cf config/simulation/fedml_config.yaml
