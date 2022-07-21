69f34460d83b2d1de7cd89f65386eedfc7cabf8f
beiyuouo <bj.yan.pa@qq.com>
2022-07-21 11:09:57 +0800

    fix(healthcare): chestxray

--- python/app/healthcare/chestxray_clf/run_client.sh
+++ python/app/healthcare/chestxray_clf/run_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 RANK=$1
-python3 main_fedml_chestxray_clf.py --cf config/fedml_config.yaml --run_id healthcare_chestxray_clf --rank $RANK --role client
+python3 main_fedml_chestxray_clf.py --cf config/fedml_config.yaml --run_id healthcare_chestxray --rank $RANK --role client
