c5e032fbd98831aea0f89b07257e4e496e73621e
beiyuouo <bj.yan.pa@qq.com>
2022-07-21 11:09:57 +0800

    fix(healthcare): chestxray

--- python/app/healthcare/chestxray_clf/data/data_loader.py
+++ python/app/healthcare/chestxray_clf/data/data_loader.py
@@ -25,16 +25,6 @@ def combine_batches(batches):
 
 def load_synthetic_data(args):
     dataset_name = str(args.dataset).lower()
-    # check if the centralized training is enabled
-    centralized = True if (args.client_num_in_total == 1 and args.training_type != "cross_silo") else False
-
-    # check if the full-batch training is enabled
-    args_batch_size = args.batch_size
-    if args.batch_size <= 0:
-        full_batch = True
-        args.batch_size = 128  # temporary batch size
-    else:
-        full_batch = False
 
     if dataset_name == "chexpert":
         # load chexpert dataset
@@ -49,6 +39,7 @@ def load_synthetic_data(args):
             test_data_local_dict,
             class_num,
         ) = load_partition_data_chexpert(
+            args=args,
             data_dir=args.data_cache_dir,
             partition_method="random",
             partition_alpha=None,
@@ -68,6 +59,7 @@ def load_synthetic_data(args):
             test_data_local_dict,
             class_num,
         ) = load_partition_data_mimiccxr(
+            args=args,
             data_dir=args.data_cache_dir,
             partition_method="random",
             partition_alpha=None,
@@ -87,33 +79,13 @@ def load_synthetic_data(args):
             test_data_local_dict,
             class_num,
         ) = load_partition_data_nihchestxray(
+            args=args,
             data_dir=args.data_cache_dir,
             partition_method="random",
             partition_alpha=None,
             client_number=args.client_num_in_total,
             batch_size=args.batch_size,
         )
-
-    if centralized:
-        train_data_local_num_dict = {
-            0: sum(user_train_data_num for user_train_data_num in train_data_local_num_dict.values())
-        }
-        train_data_local_dict = {
-            0: [batch for cid in sorted(train_data_local_dict.keys()) for batch in train_data_local_dict[cid]]
-        }
-        test_data_local_dict = {
-            0: [batch for cid in sorted(test_data_local_dict.keys()) for batch in test_data_local_dict[cid]]
-        }
-        args.client_num_in_total = 1
-
-    if full_batch:
-        train_data_global = combine_batches(train_data_global)
-        test_data_global = combine_batches(test_data_global)
-        train_data_local_dict = {
-            cid: combine_batches(train_data_local_dict[cid]) for cid in train_data_local_dict.keys()
-        }
-        test_data_local_dict = {cid: combine_batches(test_data_local_dict[cid]) for cid in test_data_local_dict.keys()}
-        args.batch_size = args_batch_size
 
     dataset = [
         train_data_num,
--- python/app/healthcare/chestxray_clf/data/nih_chest_xray/data_loader.py
+++ python/app/healthcare/chestxray_clf/data/nih_chest_xray/data_loader.py
@@ -82,8 +82,12 @@ def get_dataloader(dataset, datadir, train_bs, test_bs, dataidxs=None):
 
 
 # for local devices
-def get_dataloader_test(dataset, datadir, train_bs, test_bs, dataidxs_train, dataidxs_test):
-    return get_dataloader_test_nihchestxray(datadir, train_bs, test_bs, dataidxs_train, dataidxs_test)
+def get_dataloader_test(
+    dataset, datadir, train_bs, test_bs, dataidxs_train, dataidxs_test
+):
+    return get_dataloader_test_nihchestxray(
+        datadir, train_bs, test_bs, dataidxs_train, dataidxs_test
+    )
 
 
 def get_dataloader_nihchestxray(datadir, train_bs, test_bs, dataidxs=None):
@@ -126,7 +130,9 @@ def get_dataloader_nihchestxray(datadir, train_bs, test_bs, dataidxs=None):
     return train_dl, test_dl
 
 
-def get_dataloader_test_nihchestxray(datadir, train_bs, test_bs, dataidxs_train=None, dataidxs_test=None):
+def get_dataloader_test_nihchestxray(
+    datadir, train_bs, test_bs, dataidxs_train=None, dataidxs_test=None
+):
     dl_obj = NIHChestXray
 
     transform_train, transform_test = _data_transforms_nihchestxray()
@@ -166,7 +172,9 @@ def get_dataloader_test_nihchestxray(datadir, train_bs, test_bs, dataidxs_train=
     return train_dl, test_dl
 
 
-def distributed_centralized_nihchestxray_loader(dataset, data_dir, world_size, rank, batch_size):
+def distributed_centralized_nihchestxray_loader(
+    dataset, data_dir, world_size, rank, batch_size
+):
     """
     Used for generating distributed dataloader for
     accelerating centralized training
@@ -176,8 +184,12 @@ def distributed_centralized_nihchestxray_loader(dataset, data_dir, world_size, r
     test_bs = batch_size
 
     transform_train, transform_test = _data_transforms_nihchestxray()
-    train_dataset = NIHChestXray(data_dir=data_dir, dataidxs=None, train=True, transform=transform_train)
-    test_dataset = NIHChestXray(data_dir=data_dir, dataidxs=None, train=False, transform=transform_test)
+    train_dataset = NIHChestXray(
+        data_dir=data_dir, dataidxs=None, train=True, transform=transform_train
+    )
+    test_dataset = NIHChestXray(
+        data_dir=data_dir, dataidxs=None, train=False, transform=transform_test
+    )
 
     train_sam = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
     test_sam = DistributedSampler(test_dataset, num_replicas=world_size, rank=rank)
@@ -206,16 +218,30 @@ def distributed_centralized_nihchestxray_loader(dataset, data_dir, world_size, r
 
 
 def load_partition_data_nihchestxray(
+    args,
     data_dir,
     partition_method="random",
     partition_alpha=None,
     client_number=100,
     batch_size=10,
 ):
+    train_data_num = 0
+    test_data_num = 0
+    train_data_global = None
+    test_data_global = None
+    data_local_num_dict = dict()
+    train_data_local_dict = dict()
+    test_data_local_dict = dict()
+    class_num = 14
+
     transform_train, transform_test = _data_transforms_nihchestxray()
 
-    train_dataset = NIHChestXray(data_dir=data_dir, dataidxs=None, train=True, transform=transform_train)
-    test_dataset = NIHChestXray(data_dir=data_dir, dataidxs=None, train=False, transform=transform_test)
+    train_dataset = NIHChestXray(
+        data_dir=data_dir, dataidxs=None, train=True, transform=transform_train
+    )
+    test_dataset = NIHChestXray(
+        data_dir=data_dir, dataidxs=None, train=False, transform=transform_test
+    )
 
     # get local dataset
     if partition_method == "random":
@@ -226,9 +252,15 @@ def load_partition_data_nihchestxray(
         all_test_idxs = list(range(len(test_dataset)))
         for client_idx in range(client_number):
             dict_client[client_idx] = {}
-            dict_client[client_idx]["train"] = set(np.random.choice(all_train_idxs, num_train_items, replace=False))
-            dict_client[client_idx]["test"] = set(np.random.choice(all_test_idxs, num_test_items, replace=False))
-            all_train_idxs = list(set(all_train_idxs) - dict_client[client_idx]["train"])
+            dict_client[client_idx]["train"] = set(
+                np.random.choice(all_train_idxs, num_train_items, replace=False)
+            )
+            dict_client[client_idx]["test"] = set(
+                np.random.choice(all_test_idxs, num_test_items, replace=False)
+            )
+            all_train_idxs = list(
+                set(all_train_idxs) - dict_client[client_idx]["train"]
+            )
             all_test_idxs = list(set(all_test_idxs) - dict_client[client_idx]["test"])
         if len(all_train_idxs) > 0:
             all_client_idxs = list(range(client_number))
@@ -246,9 +278,10 @@ def load_partition_data_nihchestxray(
         raise NotImplementedError
 
     # build dataloader
-    train_dl = []
-    test_dl = []
-    for client_idx in range(client_number):
+    if args.process_id == 0:
+        pass
+    else:
+        client_idx = args.process_id - 1
         train_data_idxs = list(dict_client[client_idx]["train"])
         test_data_idxs = list(dict_client[client_idx]["test"])
         train_dl_, test_dl_ = get_dataloader_test_nihchestxray(
@@ -258,24 +291,13 @@ def load_partition_data_nihchestxray(
             train_bs=batch_size,
             test_bs=batch_size,
         )
-        train_dl.append(train_dl_)
-        test_dl.append(test_dl_)
-
-        logging.info(f"Client {client_idx} train data num: {len(train_dl_)} test data num: {len(test_dl_)}")
+        data_local_num_dict[client_idx] = len(train_data_idxs) + len(test_data_idxs)
+        train_data_local_dict[client_idx] = train_dl_
+        test_data_local_dict[client_idx] = test_dl_
 
     logging.info("Partition data done")
     # logging.info("Partition data for each client: {}".format(dict_client))
 
-    train_data_num = len(train_dataset)
-    test_data_num = len(test_dataset)
-    train_data_global = train_dataset
-    test_data_global = test_dataset
-    data_local_num_dict = {
-        client_idx: len(dict_client[client_idx]["train"]) + len(dict_client[client_idx]["test"])
-        for client_idx in range(client_number)
-    }
-    train_data_local_dict = {client_idx: train_dl_ for client_idx, train_dl_ in enumerate(train_dl)}
-    test_data_local_dict = {client_idx: test_dl_ for client_idx, test_dl_ in enumerate(test_dl)}
     class_num = train_dataset.num_classes
 
     return (
@@ -313,6 +335,8 @@ if __name__ == "__main__":
         train_data_local_dict,
         test_data_local_dict,
         class_num,
-    ) = load_partition_data_nihchestxray(data_dir=data_path, client_number=10, batch_size=10)
+    ) = load_partition_data_nihchestxray(
+        data_dir=data_path, client_number=10, batch_size=10
+    )
 
     print(train_data_num, test_data_num, class_num)
