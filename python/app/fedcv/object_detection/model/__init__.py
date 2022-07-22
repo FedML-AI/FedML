cd8497be3404bb3ce95fa65c5c09389e46cdeb7a
beiyuouo <bj.yan.pa@qq.com>
2022-07-22 16:57:22 +0800

    build(fedcv): yololv7

--- python/app/fedcv/object_detection/model/__init__.py
+++ python/app/fedcv/object_detection/model/__init__.py
@@ -4,5 +4,5 @@ import sys
 # add module path
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "yolov5"))
-
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "yolov6"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "yolov7"))
