e0f79d13dc07638500b46d562767da487e767a19
ranyide <yideran@gmail.com>
2022-07-24 16:38:05 +0800

    reset $RANK

--- python/app/fedgraphnn/recsys_subgraph_link_pred/run_client.sh
+++ python/app/fedgraphnn/recsys_subgraph_link_pred/run_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 RANK=$1
-python3 fedml_subgraph_link_prediction.py --run_id fedgraphnn_cs_ch --cf config/fedml_config.yaml --rank 1 --role client
+python3 fedml_subgraph_link_prediction.py --run_id fedgraphnn_cs_ch --cf config/fedml_config.yaml --rank $RANK --role client
