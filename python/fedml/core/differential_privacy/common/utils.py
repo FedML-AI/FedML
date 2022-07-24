d6a721b2064df25d6ea865426fe50fe8f8fb2c44
Shanshan <sshan0731@hotmail.com>
2022-07-23 18:42:58 -0700

    attack & defense: dp - gaussian code refactoring

--- python/fedml/core/differential_privacy/common/utils.py
+++ python/fedml/core/differential_privacy/common/utils.py
@@ -1,4 +1,5 @@
-from numbers import Real
+import secrets
+from numbers import Real, Integral
 
 
 def check_bounds(lower, upper):
@@ -15,6 +16,12 @@ def check_numeric_value(value):
     return True
 
 
+def check_integer_value(value):
+    if not isinstance(value, Integral):
+        raise TypeError("Value to be randomised must be an integer")
+    return True
+
+
 def check_params(epsilon, delta, sensitivity):
     if not isinstance(epsilon, Real) or not isinstance(delta, Real):
         raise TypeError("Epsilon and delta must be numeric")
@@ -28,3 +35,30 @@ def check_params(epsilon, delta, sensitivity):
         raise TypeError("Sensitivity must be numeric")
     if sensitivity < 0:
         raise ValueError("Sensitivity must be non-negative")
+
+
+def bernoulli_neg_exp(gamma, rng=None):
+    """Sample from Bernoulli(exp(-gamma)).
+    "The Discrete Gaussian for Differential Privacy": https://arxiv.org/pdf/2004.00010v2.pdf
+
+    Parameters
+    ----------
+    gamma : float. Parameter to sample from Bernoulli(exp(-gamma)).  Must be non-negative.
+    rng : Random number generator, optional. Random number generator to use.
+            If not provided, uses SystemRandom from secrets by default.
+    Returns
+    -------
+    One sample from the Bernoulli(exp(-gamma)) distribution.
+    """
+    if gamma < 0:
+        raise ValueError(f"Gamma must be non-negative, got {gamma}.")
+    if rng is None:
+        rng = secrets.SystemRandom()
+    while gamma > 1:
+        gamma -= 1
+        if not bernoulli_neg_exp(1, rng):
+            return 0
+    counter = 1
+    while rng.random() <= gamma / counter:
+        counter += 1
+    return counter % 2
