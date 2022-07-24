cdd090fd2ada1bb277014843e3cab7978b951901
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-24 10:59:50 -0500

    0.7.220

--- python/setup.py
+++ python/setup.py
@@ -48,7 +48,7 @@ if platform.machine() == "x86_64":
 
 setup(
     name="fedml",
-    version="0.7.219",
+    version="0.7.220",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
