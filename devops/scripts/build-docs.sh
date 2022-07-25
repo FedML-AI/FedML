7a2a5c467d6d3f4f670016ec5ab86c678079a928
alexliang <alexliang.kh@gmail.com>
2022-07-25 22:32:35 +0800

    update build-docs.

--- devops/scripts/build-docs.sh
+++ devops/scripts/build-docs.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-/bin/bash
-source /etc/profile
-source ~/.bashrc
-source $WORKSPACE/miniconda/etc/profile.d/conda.sh
+#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+#bash miniconda.sh -b -p $WORKSPACE/miniconda
+#hash -r
+#conda config --set always_yes yes --set changeps1 no
+#conda update -q conda
+#
+## Useful for debugging any issues with conda
+#conda info -a
+#conda config --add channels defaults
+#conda config --add channels conda-forge
+#conda config --add channels bioconda
+#
+#/bin/bash
+#source /etc/profile
+#source ~/.bashrc
+#source $WORKSPACE/miniconda/etc/profile.d/conda.sh
+#
+#which conda
+#conda info
+#conda install sphinx
+#conda install -c conda-forge myst-parser
 
-which conda
-conda info
-conda install sphinx
-conda install -c conda-forge myst-parser
+pip install -U sphinx
+pip install myst-parser
+
 cd doc/en/
 make html
 make clean html
