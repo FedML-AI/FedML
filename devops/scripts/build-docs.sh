303ed1a715ee54f41ee47dc4471cf4381f2138e9
alexliang <alexliang.kh@gmail.com>
2022-07-25 22:50:20 +0800

    update build-docs.

--- devops/scripts/build-docs.sh
+++ devops/scripts/build-docs.sh
@@ -3,6 +3,7 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $WORKSPACE/miniconda
 hash -r
+source $WORKSPACE/miniconda/etc/profile.d/conda.sh
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 
