0a2d73c9d854216acb9c14ba02a7357a24825d5b
Alex <alex.gpt.llm@gmail.com>
2022-07-25 22:44:18 +0800

    update build-docs.

--- devops/scripts/build-docs.sh
+++ devops/scripts/build-docs.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-#bash miniconda.sh -b -p $WORKSPACE/miniconda
-#hash -r
-#conda config --set always_yes yes --set changeps1 no
-#conda update -q conda
-#
-## Useful for debugging any issues with conda
-#conda info -a
-#conda config --add channels defaults
-#conda config --add channels conda-forge
-#conda config --add channels bioconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $WORKSPACE/miniconda
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+
+# Useful for debugging any issues with conda
+conda info -a
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
 #
 #/bin/bash
 #source /etc/profile
@@ -18,13 +18,14 @@
 #source $WORKSPACE/miniconda/etc/profile.d/conda.sh
 #
 #which conda
-#conda info
-#conda install sphinx
-#conda install -c conda-forge myst-parser
+
+conda info
+conda install sphinx
+conda install -c conda-forge myst-parser
 
 #pip3 install --upgrade pip
-pip3 install -U sphinx
-pip3 install myst-parser
+#pip3 install -U sphinx
+#pip3 install myst-parser
 
 cd doc/en/
 make html
