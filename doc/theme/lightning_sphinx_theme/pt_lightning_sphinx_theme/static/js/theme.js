587d3aff99e7ead8874cda712c041e06fb539090
ranyide <yideran@gmail.com>
2022-07-26 18:00:02 +0800

    open the first toctree

--- doc/theme/lightning_sphinx_theme/pt_lightning_sphinx_theme/static/js/theme.js
+++ doc/theme/lightning_sphinx_theme/pt_lightning_sphinx_theme/static/js/theme.js
@@ -974,6 +974,9 @@ $(document).ready(function() {
       $(this.firstChild).after("<span class='hide-menu menu-item-decorator'>" + chevronDown + "</span>");
     }
   });
+  $("#pytorch-left-menu p.caption").next("ul").first().toggle();
+  $("#pytorch-left-menu p.caption").first().children(".expand-menu").toggle()
+  $("#pytorch-left-menu p.caption").first().children(".hide-menu").toggle()
 
   $(".expand-menu").on("click", function () {
     $(this).prev(".hide-menu").toggle();
