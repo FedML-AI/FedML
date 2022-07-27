941f7b1f0a8ad5e486bc696257b2b27db8ee22e1
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-27 12:04:42 -0700

    bug fix for create_model_trainer

--- python/fedml/simulation/mpi/fedprox/FedProxAPI.py
+++ python/fedml/simulation/mpi/fedprox/FedProxAPI.py
@@ -85,7 +85,7 @@ def init_server(
     preprocessed_sampling_lists=None,
 ):
     if model_trainer is None:
-        model_trainer = create_model_trainer(model, args)
+        model_trainer = create_model_trainer(args, model)
     model_trainer.set_id(-1)
 
     # aggregator
@@ -139,7 +139,7 @@ def init_client(
 ):
     client_index = process_id - 1
     if model_trainer is None:
-        model_trainer = create_model_trainer(model, args)
+        model_trainer = create_model_trainer(args, model)
     model_trainer.set_id(client_index)
     backend = args.backend
     trainer = FedProxTrainer(
