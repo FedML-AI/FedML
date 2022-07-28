e219d2a74b2d4416e86af232258f8585664a4a78
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-27 21:09:49 -0700

    polish flow

--- python/fedml/core/distributed/flow/test_fedml_flow.py
+++ python/fedml/core/distributed/flow/test_fedml_flow.py
@@ -12,12 +12,19 @@ class Client(FedMLExecutor):
         neighbor_id_list = [0]
         super().__init__(id, neighbor_id_list)
 
+        self.device = None
+        self.dataset = None
+        self.model = None
+
+    def init(self, device, dataset, model):
+        self.device = device
+        self.dataset = dataset
+        self.model = model
+
     def local_training(self):
         logging.info("local_training")
-
-        params = Params()
-        params.add("whatever_key_as_you_like_1", "any_value")
-        params.add("whatever_key_as_you_like_2", 2)
+        params = self.get_params()
+        model_params = params.get("model_params")
         return params
 
 
@@ -28,11 +35,24 @@ class Server(FedMLExecutor):
         neighbor_id_list = [1, 2]
         super().__init__(id, neighbor_id_list)
 
+        self.device = None
+        self.dataset = None
+        self.model = None
+
         self.round_idx = 0
+
+        self.client_count = 0
+        self.client_num = 2
+
+    def init(self, device, dataset, model):
+        self.device = device
+        self.dataset = dataset
+        self.model = model
 
     def init_global_model(self):
         logging.info("init_global_model")
         params = Params()
+        params.add("model_params", self.model.state_dict())
         return params
 
     def server_aggregate(self):
@@ -41,9 +61,13 @@ class Server(FedMLExecutor):
         value1 = params.get("whatever_key_as_you_like_1")
         logging.info("value1 = {}".format(value1))
         self.round_idx += 1
-        if self.round_idx == self.loop_times:
+        if self.client_count == self.client_num:
+            self.client_count = 0
             params = Params()
             return params
+        else:
+            self.client_count += 1
+            return None
 
     def final_eval(self):
         logging.info("final_eval")
@@ -53,21 +77,20 @@ if __name__ == "__main__":
     args = fedml.init()
 
     # init device
-    # device = fedml.device.get_device(args)
-    device = None
+    device = fedml.device.get_device(args)
 
     # load data
-    # dataset, output_dim = fedml.data.load(args)
-    dataset, output_dim = None, 10
+    dataset, output_dim = fedml.data.load(args)
 
     # load model
-    # model = fedml.model.create(args, output_dim)
-    model = None
+    model = fedml.model.create(args, output_dim)
 
     if args.rank == 0:
         executor = Server(args)
+        executor.init(device, dataset, model)
     else:
         executor = Client(args)
+        executor.init(device, dataset, model)
 
     fedml_alg_flow = FedMLAlgorithmFlow(args, executor, loop_times=2)
     fedml_alg_flow.add_flow("init_global_model", Server.init_global_model)
