e7d8a404dd5e9913724c13dbf19d322690801be2
emirceyani <emirceyani@gmail.com>
2022-07-29 23:49:03 +0000

    Update bootstrap for recsysSubgraph

--- python/app/fedgraphnn/recsys_subgraph_link_pred/config/bootstrap.sh
+++ python/app/fedgraphnn/recsys_subgraph_link_pred/config/bootstrap.sh
@@ -6,6 +6,10 @@ set -x
 ### please customize your script in this region ####
 pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.0+cpu.html
 
+DATA_PATH=~/fedgraphnn_data/ciao/
+mkdir $DATA_PATH
+
+cp -R ../data/ciao/ $DATA_PATH
 ### don't modify this part ###
 exit 0
 ##############################
