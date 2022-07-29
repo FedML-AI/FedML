ae5b2fc9ec367311a39f71458bf0508640d04a37
wizard1203 <956234606@qq.com>
2022-07-29 23:53:20 +0800

    fedavg seq to cross silo

--- python/fedml/ml/aggregator/agg_operator.py
+++ python/fedml/ml/aggregator/agg_operator.py
@@ -21,9 +21,35 @@ class FedMLAggOperator:
         return avg_params
 
     @staticmethod
+    def FedAvg_seq(raw_grad_list):
+        (num0, avg_params) = raw_grad_list[0]
+        for k in avg_params.keys():
+            for i in range(0, len(raw_grad_list)):
+                local_sample_number, local_model_params = raw_grad_list[i]
+                if i == 0:
+                    avg_params[k] = local_model_params[k]
+                else:
+                    avg_params[k] += local_model_params[k]
+        return avg_params
+
+
+    @staticmethod
+    def FedOpt(raw_grad_list):
+        pass
+
+
+    @staticmethod
     def agg(args, raw_grad_list: List[Tuple[float, Dict]]) -> Dict:
         if args.federated_optimizer == "FedAvg":
             agg_func = FedMLAggOperator.FedAVG
+        elif args.federated_optimizer == "FedAvg_seq":
+            agg_func = FedMLAggOperator.FedAVG_seq
+        elif args.federated_optimizer == "FedOpt":
+            agg_func = FedMLAggOperator.FedOpt
         else:
             raise Exception("will support many optimizers in a unified framework soon")
         return agg_func(raw_grad_list)
+
+
+
+
