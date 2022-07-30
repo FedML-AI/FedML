415b820c17a72fee626d5c8a7e1c6a3c2a57a5bf
Chaoyang He <chaoyanghe.com@gmail.com>
2022-07-30 07:58:04 -0700

    simplify flow

--- python/fedml/core/distributed/flow/test_fedml_flow.py
+++ python/fedml/core/distributed/flow/test_fedml_flow.py
@@ -24,9 +24,16 @@ class Client(FedMLExecutor):
     def local_training(self):
         logging.info("local_training start")
         params = self.get_params()
-        model_params = params.get("model_params")
+        model_params = params.get(Params.KEY_MODEL_PARAMS)
         return params
 
+    def handle_init_global_model(self):
+        received_params = self.get_params()
+        model_params = received_params.get(Params.KEY_MODEL_PARAMS)
+
+        params = Params()
+        params.add(Params.KEY_MODEL_PARAMS, model_params)
+        return params
 
 class Server(FedMLExecutor):
     def __init__(self, args):
@@ -52,19 +59,20 @@ class Server(FedMLExecutor):
     def init_global_model(self):
         logging.info("init_global_model")
         params = Params()
-        params.add("model_params", self.model.state_dict())
+        params.add(Params.KEY_MODEL_PARAMS, self.model.state_dict())
         return params
 
     def server_aggregate(self):
         logging.info("server_aggregate")
-        # params = self.get_params()
-        # value1 = params.get("whatever_key_as_you_like_1")
+        params = self.get_params()
+        model_params = params.get(Params.KEY_MODEL_PARAMS)
         # logging.info("value1 = {}".format(value1))
         self.round_idx += 1
         self.client_count += 1
         if self.client_count == self.client_num:
             self.client_count = 0
             params = Params()
+            params.add(Params.KEY_MODEL_PARAMS, model_params)
             return params
 
     def final_eval(self):
@@ -90,10 +98,12 @@ if __name__ == "__main__":
         executor = Client(args)
         executor.init(device, dataset, model)
 
-    fedml_alg_flow = FedMLAlgorithmFlow(args, executor, loop_times=2)
+    fedml_alg_flow = FedMLAlgorithmFlow(args, executor)
     fedml_alg_flow.add_flow("init_global_model", Server.init_global_model)
-    fedml_alg_flow.add_flow("local_training", Client.local_training, flow_tag=FedMLAlgorithmFlow.LOOP_START)
-    fedml_alg_flow.add_flow("server_aggregate", Server.server_aggregate, flow_tag=FedMLAlgorithmFlow.LOOP_END)
+    fedml_alg_flow.add_flow("handle_init", Client.handle_init_global_model)
+    for round_idx in range(args.comm_round):
+        fedml_alg_flow.add_flow("local_training", Client.local_training)
+        fedml_alg_flow.add_flow("server_aggregate", Server.server_aggregate)
     fedml_alg_flow.add_flow("final_eval", Server.final_eval)
     fedml_alg_flow.build()
 
