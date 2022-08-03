14381f2fe5d1d514e809903d5ff5b465a7562eb1
Shanshan <sshan0731@hotmail.com>
2022-08-03 16:33:32 -0700

    attack & defense

---
+++ python/fedml/core/differential_privacy/solutions/rappor.py
@@ -0,0 +1,11 @@
+
+
+
+"""
+Differential Privacy at Google
+paper: githttps://arxiv.org/pdf/1407.6981v2.pdf
+blog: https://ai.googleblog.com/2014/10/learning-statistics-with-privacy-aided.html
+code: https://github.com/google/rappor
+
+
+"""
