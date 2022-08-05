d471ff76849ce61835a3b84be04e61abefaf5ed1
Shanshan <sshan0731@hotmail.com>
2022-08-05 11:57:49 -0700

    fedml_ego_networks_node_clf bootstrap.sh

--- python/app/fedgraphnn/ego_networks_node_clf/config/bootstrap.sh
+++ python/app/fedgraphnn/ego_networks_node_clf/config/bootstrap.sh
@@ -6,10 +6,10 @@ set -x
 ### please customize your script in this region ####
 pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.0+cpu.html
 
-DATA_PATH=~/fedgraphnn_data/
+DATA_PATH=~/ego_networks_node_clf/
 mkdir $DATA_PATH
 
-python ../data/sampleEgonetworks.py --path $DATA_PATH --data citeseer --ego_number 1000 --hop_number 2
+python ./fedml_ego_networks_node_clf.py --path ego_networks_node_clf/ --data cora --ego_number 1000 --hop_number 2 --cf config/simulation/fedml_config.yaml
 
 
 ### don't modify this part ###
