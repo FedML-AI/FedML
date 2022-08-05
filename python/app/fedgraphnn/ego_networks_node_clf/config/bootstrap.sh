eb3d1db833d142f99830169babab115e3f75b295
Shanshan <sshan0731@hotmail.com>
2022-08-05 12:18:59 -0700

    fedml_ego_networks_node_clf update readme.md

--- python/app/fedgraphnn/ego_networks_node_clf/config/bootstrap.sh
+++ python/app/fedgraphnn/ego_networks_node_clf/config/bootstrap.sh
@@ -6,11 +6,10 @@ set -x
 ### please customize your script in this region ####
 pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.0+cpu.html
 
-DATA_PATH=~/ego_networks_node_clf/
+DATA_PATH=~/fedgraphnn_data/
 mkdir $DATA_PATH
 
-python ./fedml_ego_networks_node_clf.py --path ego_networks_node_clf/ --data cora --ego_number 1000 --hop_number 2 --cf config/simulation/fedml_config.yaml
-
+python ../data/sampleEgonetworks.py --path $DATA_PATH --data citeseer --ego_number 1000 --hop_number 2 --cf config/simulation/fedml_config.yaml
 
 ### don't modify this part ###
 echo "[FedML]Bootstrap Finished"
