1e89a161a77805944e311510be67017115ded5f6
alexliang <alexliang.kh@gmail.com>
2022-08-05 16:51:03 +0800

    add the finished tag into the bootstrap.sh.

--- python/scripts/docker/bootstrap.sh
+++ python/scripts/docker/bootstrap.sh
@@ -2,3 +2,7 @@
 echo "This is bootstrap script. You can use it to customize your additional installation and set some environment variables"
 
 pip install --upgrade fedml
+
+### don't modify this part ###
+echo "[FedML]Bootstrap Finished"
+##############################
