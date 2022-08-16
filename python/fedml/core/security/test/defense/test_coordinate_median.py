2b44ed59e24741ce57b7cdf19de39ccef16ee22b
Shanshan <sshan0731@hotmail.com>
2022-08-16 01:15:24 -0700

    attack & defense: CoordinateTrimmedMeanDefense

--- python/fedml/core/security/test/defense/test_coordinate_median.py
+++ python/fedml/core/security/test/defense/test_coordinate_median.py
@@ -1,34 +1,15 @@
-import argparse
-
-from fedml.core.security.defense.cclip_defense import CClipDefense
-from fedml.core.security.defense.coordinate_median_defense import CoordinateMedianDefense
-from fedml.core.security.test.aggregation.aggregation_functions import (
+from ....security.defense.coordinate_wise_median_defense import (
+    CoordinateWiseMedianDefense,
+)
+from ....security.test.aggregation.aggregation_functions import (
     AggregationFunction,
 )
-from fedml.core.security.test.utils import create_fake_model_list
-
-#
-# def add_args():
-#     parser = argparse.ArgumentParser(description="FedML")
-#     parser.add_argument(
-#         "--yaml_config_file",
-#         "--cf",
-#         help="yaml configuration file",
-#         type=str,
-#         default="",
-#     )
-#
-#     # default arguments
-#     parser.add_argument("--tau", type=int, default=10)
-#     parser.add_argument("--bucket_size", type=int, default=3)
-#     args, unknown = parser.parse_known_args()
-#     return args
+from ....security.test.utils import create_fake_model_list
 
 
 def test_defense():
-    # config = add_args()
     client_grad_list = create_fake_model_list(20)
-    defense = CoordinateMedianDefense(config=None)
+    defense = CoordinateWiseMedianDefense(config=None)
     result = defense.run(
         client_grad_list, base_aggregation_func=AggregationFunction.FedAVG
     )
