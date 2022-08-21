71d5a7bd3049077cef651a506225439171af398b
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-21 10:32:55 -0700

    remove default dependency of tf and jax

--- python/fedml/ml/engine/ml_engine_adapter.py
+++ python/fedml/ml/engine/ml_engine_adapter.py
@@ -3,32 +3,16 @@ import torch
 from .torch_process_group_manager import TorchProcessGroupManager
 
 
-class MLEngineBackend:
-    ml_engine_args_flag = "ml_engine"
-
-    ml_engine_backend_torch = "torch"
-    ml_engine_backend_tf = "tf"
-    ml_engine_backend_jax = "jax"
-    ml_engine_backend_mxnet = "mxnet"
-
-    ml_device_type_gpu = "gpu"
-    ml_device_type_cpu = "cpu"
-    ml_device_type_mps = "mps"
-
-
 def is_torch_device_available(args, device_type):
-    if device_type == MLEngineBackend.ml_device_type_gpu:
+    if device_type == "gpu":
         if torch.cuda.is_available():
             return True
         return False
-    elif device_type == MLEngineBackend.ml_device_type_mps:
+    elif device_type == "mps":
         # Macbook M1: https://pytorch.org/docs/master/notes/mps.html
         if not torch.backends.mps.is_available():
             if not torch.backends.mps.is_built():
-                print(
-                    "MPS not available because the current PyTorch install was not "
-                    "built with MPS enabled."
-                )
+                print("MPS not available because the current PyTorch install was not " "built with MPS enabled.")
             else:
                 print(
                     "MPS not available because the current MacOS version is not 12.3+ "
@@ -37,39 +21,22 @@ def is_torch_device_available(args, device_type):
             return False
         else:
             return True
-    elif device_type == MLEngineBackend.ml_device_type_gpu:
+    elif device_type == "cpu":
         return True
 
     return False
 
 
-def is_mxnet_device_available(args, device_type):
-    if device_type == MLEngineBackend.ml_device_type_cpu:
-        return True
-    elif device_type == MLEngineBackend.ml_device_type_gpu:
-        try:
-            import mxnet as mx
-
-            gpus = mx.device.num_gpus()
-        except Exception as ex:
-            return False
-
-        if gpus > 0:
-            return True
-
-    return False
-
-
-def is_device_available(args, device_type=MLEngineBackend.ml_device_type_gpu):
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+def is_device_available(args, device_type="gpu"):
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             import tensorflow as tf
 
             devices = tf.config.list_physical_devices(device_type.upper())
             if len(devices) > 0:
                 return True
             return False
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             try:
                 import jax
 
@@ -80,8 +47,6 @@ def is_device_available(args, device_type=MLEngineBackend.ml_device_type_gpu):
                 return False
 
             return False
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            return is_mxnet_device_available(args, device_type)
         else:
             return is_torch_device_available(args, device_type)
     else:
@@ -94,27 +59,27 @@ def get_torch_device(args, using_gpu, device_id, device_type):
         if device_id is not None:
             gpu_id = device_id
 
-        if torch.cuda.is_available() and device_type == MLEngineBackend.ml_device_type_gpu:
+        if torch.cuda.is_available() and device_type == "gpu":
             device = torch.device(f"cuda:{gpu_id}")
             torch.cuda.set_device(gpu_id)
-        elif device_type == MLEngineBackend.ml_device_type_mps:
+        elif device_type == "mps":
             # https://pytorch.org/docs/master/notes/mps.html
-            device = torch.device(MLEngineBackend.ml_device_type_mps)
+            device = torch.device("mps")
         else:
-            device = torch.device(MLEngineBackend.ml_device_type_cpu)
+            device = torch.device("cpu")
 
         return device
     else:
-        return torch.device(MLEngineBackend.ml_device_type_cpu)
+        return torch.device("cpu")
 
 
 def get_tf_device(args, using_gpu, device_id, device_type):
     import tensorflow as tf
 
     if using_gpu:
-        return tf.device('/device:gpu:{}'.format(device_id))
+        return tf.device("/device:gpu:{}".format(device_id))
     else:
-        return tf.device('/device:cpu:0')
+        return tf.device("/device:cpu:0")
 
 
 def get_jax_device(args, using_gpu, device_id, device_type):
@@ -130,25 +95,15 @@ def get_jax_device(args, using_gpu, device_id, device_type):
         return None
 
 
-def get_mxnet_device(args, using_gpu, device_id, device_type):
-    import mxnet as mx
-    if using_gpu:
-        return mx.gpu(device_id)
-    else:
-        return mx.cpu()
-
-
 def get_device(args, using_gpu=False, device_id=None, device_type="cpu"):
     if hasattr(args, "using_gpu") and args.using_gpu:
         using_gpu = True
 
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             return get_tf_device(args, using_gpu, device_id, device_type)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             return get_jax_device(args, using_gpu, device_id, device_type)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            return get_mxnet_device(args, using_gpu, device_id, device_type)
         else:
             return get_torch_device(args, using_gpu, device_id, device_type)
     else:
@@ -156,19 +111,15 @@ def get_device(args, using_gpu=False, device_id=None, device_type="cpu"):
 
 
 def dict_to_device(args, dict_obj, device):
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             with device:
                 dict_ret = dict_obj
                 return dict_ret
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             import jax
 
             return jax.device_put(dict_obj, device)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            with device:
-                dict_ret = dict_obj
-                return dict_ret
         else:
             return dict_obj.to(device)
     else:
@@ -176,18 +127,14 @@ def dict_to_device(args, dict_obj, device):
 
 
 def model_params_to_device(args, params_obj, device):
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             with device:
                 params_ret = params_obj
                 return params_ret
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             for key in params_obj.keys():
                 params_obj[key] = dict_to_device(args, params_obj[key], device)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            with device:
-                params_ret = params_obj
-                return params_ret
         else:
             for key in params_obj.keys():
                 params_obj[key] = dict_to_device(args, params_obj[key], device)
@@ -199,15 +146,13 @@ def model_params_to_device(args, params_obj, device):
 
 
 def model_to_device(args, model_obj, device):
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             with device:
                 model_ret = model_obj
                 return model_ret
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             return model_obj
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            model_obj.reset_device(device)
         else:
             model_obj.to(device)
     else:
@@ -219,11 +164,7 @@ def torch_model_ddp(args, model_obj, device):
 
     only_gpu = args.using_gpu
     process_group_manager = TorchProcessGroupManager(
-        args.proc_rank_in_silo,
-        args.n_proc_in_silo,
-        args.pg_master_address,
-        args.pg_master_port,
-        only_gpu,
+        args.proc_rank_in_silo, args.n_proc_in_silo, args.pg_master_address, args.pg_master_port, only_gpu,
     )
     model = DDP(model_obj, device_ids=[device] if only_gpu else None)
     return process_group_manager, model
@@ -239,20 +180,13 @@ def jax_model_ddp(args, model_obj, device):
     return process_group_manager, model
 
 
-def mxnet_model_ddp(args, model_obj, device):
-    process_group_manager, model = None, model_obj
-    return process_group_manager, model
-
-
 def model_ddp(args, model_obj, device):
     process_group_manager, model = None, model_obj
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             return tf_model_ddp(args, model_obj, device)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             return jax_model_ddp(args, model_obj, device)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            return mxnet_model_ddp(args, model_obj, device)
         else:
             process_group_manager, model = torch_model_ddp(args, model_obj, device)
     else:
@@ -322,47 +256,21 @@ def jax_aggregator(args, raw_grad_list, training_num):
                 local_sample_number, local_model_params = raw_grad_list[i]
                 w = local_sample_number / training_num
                 if i == 0:
-                    avg_params[k]['w'] = local_model_params[k]['w'] * w
-                    avg_params[k]['b'] = local_model_params[k]['b'] * w
+                    avg_params[k]["w"] = local_model_params[k]["w"] * w
+                    avg_params[k]["b"] = local_model_params[k]["b"] * w
                 else:
-                    avg_params[k]['w'] += local_model_params[k]['w'] * w
-                    avg_params[k]['b'] += local_model_params[k]['b'] * w
+                    avg_params[k]["w"] += local_model_params[k]["w"] * w
+                    avg_params[k]["b"] += local_model_params[k]["b"] * w
     elif args.federated_optimizer == "FedAvg_seq":
         for k in avg_params.keys():
             for i in range(0, len(raw_grad_list)):
                 local_sample_number, local_model_params = raw_grad_list[i]
                 if i == 0:
-                    avg_params[k]['b'] = local_model_params[k]['b']
-                    avg_params[k]['w'] = local_model_params[k]['w']
+                    avg_params[k]["b"] = local_model_params[k]["b"]
+                    avg_params[k]["w"] = local_model_params[k]["w"]
                 else:
-                    avg_params[k]['b'] += local_model_params[k]['b']
-                    avg_params[k]['w'] += local_model_params[k]['w']
-    elif args.federated_optimizer == "FedOpt":
-        pass
-
-    return avg_params
-
-
-def mxnet_aggregator(args, raw_grad_list, training_num):
-    (num0, avg_params) = raw_grad_list[0]
-
-    if args.federated_optimizer == "FedAvg":
-        for k in avg_params.keys():
-            for i in range(0, len(raw_grad_list)):
-                local_sample_number, local_model_params = raw_grad_list[i]
-                w = local_sample_number / training_num
-                if i == 0:
-                    avg_params[k] = local_model_params[k] * w
-                else:
-                    avg_params[k] += local_model_params[k] * w
-    elif args.federated_optimizer == "FedAvg_seq":
-        for k in avg_params.keys():
-            for i in range(0, len(raw_grad_list)):
-                local_sample_number, local_model_params = raw_grad_list[i]
-                if i == 0:
-                    avg_params[k] = local_model_params[k]
-                else:
-                    avg_params[k] += local_model_params[k]
+                    avg_params[k]["b"] += local_model_params[k]["b"]
+                    avg_params[k]["w"] += local_model_params[k]["w"]
     elif args.federated_optimizer == "FedOpt":
         pass
 
@@ -370,13 +278,11 @@ def mxnet_aggregator(args, raw_grad_list, training_num):
 
 
 def model_aggregator(args, raw_grad_list, training_num):
-    if hasattr(args, MLEngineBackend.ml_engine_args_flag):
-        if args.ml_engine == MLEngineBackend.ml_engine_backend_tf:
+    if hasattr(args, "ml_engine"):
+        if args.ml_engine == "tf":
             return tf_aggregator(args, raw_grad_list, training_num)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_jax:
+        elif args.ml_engine == "jax":
             return jax_aggregator(args, raw_grad_list, training_num)
-        elif args.ml_engine == MLEngineBackend.ml_engine_backend_mxnet:
-            return mxnet_aggregator(args, raw_grad_list, training_num)
         else:
             return torch_aggregator(args, raw_grad_list, training_num)
     else:
