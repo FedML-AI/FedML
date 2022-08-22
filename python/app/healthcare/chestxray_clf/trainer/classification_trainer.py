f034e2757a708c6a9a686df142819f79d16d97c2
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-22 14:59:58 -0700

    update README.md

--- python/app/healthcare/chestxray_clf/trainer/classification_trainer.py
+++ python/app/healthcare/chestxray_clf/trainer/classification_trainer.py
@@ -101,8 +101,3 @@ class ClassificationTrainer(ClientTrainer):
                     metrics["test_total"] += target.size(0) * target.size(1)
 
         return metrics
-
-    def test_on_the_server(
-        self, train_data_local_dict, test_data_local_dict, device, args=None
-    ) -> bool:
-        return False
