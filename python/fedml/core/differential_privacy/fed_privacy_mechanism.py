c9a2d72abbc2e879f56a9c79a4902d70136518a6
Shanshan <sshan0731@hotmail.com>
2022-08-23 00:39:29 -0700

    add cdp

--- python/fedml/core/differential_privacy/fed_privacy_mechanism.py
+++ python/fedml/core/differential_privacy/fed_privacy_mechanism.py
@@ -1,15 +1,40 @@
 from .mechanisms import Laplace, Gaussian
 
 
-class DP:
-    def __init__(
-        self, epsilon, delta=0, sensitivity=1.0, mechanism_type="laplace"
-    ):
-        mechanism_type = mechanism_type.lower()
+class DifferentialPrivacy:
+    _defender_instance = None
+
+    @staticmethod
+    def get_instance():
+        if DifferentialPrivacy._defender_instance is None:
+            DifferentialPrivacy._defender_instance = DifferentialPrivacy()
+        return DifferentialPrivacy._defender_instance
+
+    def __init__(self):
+        self.is_dp_enabled = False
+        self.dp_type = None
+        self.dp = None
+
+    def init(
+        self, args
+    ):  # epsilon, delta=0, sensitivity=1.0, mechanism_type="laplace"
+        mechanism_type = args.mechanism_type.lower()
+        self.is_dp_enabled = True
+        self.dp_type = args.dp_type.lower()
+        if self.dp_type not in ["cdp", "ldp"]:
+            raise ValueError("DP type can only be cdp (for central DP) and ldp (for local DP)! ")
         if mechanism_type == "laplace":
-            self.dp = Laplace(epsilon=epsilon, delta=delta, sensitivity=sensitivity)
+            self.dp = Laplace(epsilon=args.epsilon, delta=args.delta, sensitivity=args.sensitivity)
         elif mechanism_type == "gaussian":
-            self.dp = Gaussian(epsilon=epsilon, delta=delta, sensitivity=sensitivity)
+            self.dp = Gaussian(epsilon=args.epsilon, delta=args.delta, sensitivity=args.sensitivity)
+        else:
+            raise NotImplementedError("DP mechanism not implemented!")
+
+    def is_dp_enabled(self):
+        return self.is_dp_enabled
+
+    def get_dp_type(self):
+        return self.dp_type
 
     def randomise(self, value):
         return self.dp.randomise(value)
@@ -23,3 +48,13 @@ class DP:
         for k in grad.keys():
             new_grad[k] = self.compute_a_noise() + grad[k]
         return new_grad
+
+    def add_cdp_noise(self, avg_param):
+        if self.dp_type == "cdp":
+            avg_param = self.compute_randomized_gradient(avg_param)
+        return avg_param
+
+    def add_ldp_noise(self, grad):
+        if self.dp_type == "ldp":
+            grad = self.compute_randomized_gradient(grad)
+        return grad
