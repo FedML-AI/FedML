0164c2f454753e33c189d0c80f0d8f6fd60ac4a9
alexliang <alexliang.kh@gmail.com>
2022-08-25 15:58:48 +0800

    update smoke test scripts on windows.

---
+++ python/tests/smoke_test/cli/build.bat
@@ -0,0 +1,32 @@
+
+:: https://doc.fedml.ai/mlops/api.html
+
+:: fedml build -t client -sf $SOURCE -ep $ENTRY -cf $CONFIG -df $DEST
+::
+:: Usage: fedml build [OPTIONS]
+::
+:: Commands for open.fedml.ai MLOps platform
+::
+:: Options:
+:: -t, --type TEXT            client or server? (value: client; server)
+:: -sf, --source_folder TEXT  the source code folder path
+:: -ep, --entry_point TEXT    the entry point of the source code
+:: -cf, --config_folder TEXT  the config folder path
+:: -df, --dest_folder TEXT    the destination package folder path
+:: --help                     Show this message and exit.
+
+:: build client package
+cd ../../../examples/cross_silo/mqtt_s3_fedavg_mnist_lr_example/one_line
+
+set SOURCE=client
+set ENTRY=torch_client.py
+set CONFIG=config
+set DEST=./mlops
+fedml build -t client -sf %SOURCE% -ep %ENTRY% -cf %CONFIG% -df %DEST%
+
+:: build server package
+set SOURCE=server
+set ENTRY=torch_server.py
+set CONFIG=config
+set DEST=./mlops
+fedml build -t server -sf %SOURCE% -ep %ENTRY% -cf %CONFIG% -df %DEST%
