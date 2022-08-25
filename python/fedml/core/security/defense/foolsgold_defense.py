f666d36eb47d95e12d3a0e939aed4c5f28f09865
Shanshan <sshan0731@hotmail.com>
2022-08-25 00:39:03 -0700

    foolsgold add cross-silo example

--- python/fedml/core/security/defense/foolsgold_defense.py
+++ python/fedml/core/security/defense/foolsgold_defense.py
@@ -15,6 +15,7 @@ potential bugs when using memory: when only some of clients participate in compu
 
 class FoolsGoldDefense(BaseDefenseMethod):
     def __init__(self, config):
+        print(f"init fools gold===========")
         self.memory = None
         self.use_memory = config.use_memory
 
