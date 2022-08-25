5991c9e95d3c18168f9b94ebe96986a6290734af
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-24 21:25:21 -0700

    [pylint] optimize alg_frame

--- python/fedml/core/alg_frame/server_aggregator.py
+++ python/fedml/core/alg_frame/server_aggregator.py
@@ -1,10 +1,11 @@
+import logging
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Dict
-from fedml.core.security.fedml_defender import FedMLDefender
-from fedml.core.security.fedml_attacker import FedMLAttacker
-from fedml.core.dp.fed_privacy_mechanism import FedMLDifferentialPrivacy
-from fedml.ml.aggregator.agg_operator import FedMLAggOperator
-import logging
+
+from ..dp.fed_privacy_mechanism import FedMLDifferentialPrivacy
+from ..security.fedml_attacker import FedMLAttacker
+from ..security.fedml_defender import FedMLDefender
+from ...ml.aggregator.agg_operator import FedMLAggOperator
 
 
 class ServerAggregator(ABC):
@@ -31,23 +32,16 @@ class ServerAggregator(ABC):
     ) -> List[Tuple[float, Dict]]:
         if FedMLAttacker.get_instance().is_model_attack():
             raw_client_model_or_grad_list = FedMLAttacker.get_instance().attack_model(
-                local_w=raw_client_model_or_grad_list,
-                global_w=self.get_model_params(),
-                refs=None,
+                local_w=raw_client_model_or_grad_list, global_w=self.get_model_params(), refs=None,
             )
         if FedMLDefender.get_instance().is_defense_enabled():
-            raw_client_model_or_grad_list = (
-                FedMLDefender.get_instance().defend_before_aggregation(
-                    raw_client_grad_list=raw_client_model_or_grad_list,
-                    extra_auxiliary_info=self.get_model_params(),
-                )
+            raw_client_model_or_grad_list = FedMLDefender.get_instance().defend_before_aggregation(
+                raw_client_grad_list=raw_client_model_or_grad_list, extra_auxiliary_info=self.get_model_params(),
             )
 
         return raw_client_model_or_grad_list
 
-    def aggregate(
-        self, raw_client_model_or_grad_list: List[Tuple[float, Dict]]
-    ) -> Dict:
+    def aggregate(self, raw_client_model_or_grad_list: List[Tuple[float, Dict]]) -> Dict:
         if FedMLDefender.get_instance().is_defense_enabled():
             return FedMLDefender.get_instance().defend_on_aggregation(
                 raw_client_grad_list=raw_client_model_or_grad_list,
@@ -59,16 +53,12 @@ class ServerAggregator(ABC):
     def on_after_aggregation(self, aggregated_model_or_grad: Dict) -> Dict:
         if FedMLDifferentialPrivacy.get_instance().is_enabled():
             logging.info("-----add cdp noise ----")
-            aggregated_model_or_grad = FedMLDifferentialPrivacy.get_instance().add_cdp_noise(
-                aggregated_model_or_grad
-            )
+            aggregated_model_or_grad = FedMLDifferentialPrivacy.get_instance().add_cdp_noise(aggregated_model_or_grad)
         return aggregated_model_or_grad
 
     @abstractmethod
     def test(self, test_data, device, args):
         pass
 
-    def test_all(
-        self, train_data_local_dict, test_data_local_dict, device, args
-    ) -> bool:
+    def test_all(self, train_data_local_dict, test_data_local_dict, device, args) -> bool:
         pass
