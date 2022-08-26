b03cc77e46a4fc13efca5a4c50bc79c90624e6e3
Shanshan <sshan0731@hotmail.com>
2022-08-26 00:46:05 -0700

    move attack & defense test case

--- python/tests/security/defense/test_soteria.py
+++ python/tests/security/defense/test_soteria.py
@@ -4,25 +4,13 @@ arXiv preprint arXiv:2012.06043 (2020).
 added by Kai, 07/10/2022
 """
 
-# test using local directory
-# import os, sys
-# __file__ = '/Users/kai/Documents/FedML/python/fedml/core/'
-# sys.path.append(__file__)
-# from security.defense.soteria_defense import SoteriaDefense
-# from security.test.utils import create_fake_gradient_Cifar100, create_fake_model_Cifar100, create_fake_data_Cifar100
 
-import logging
-
+from fedml.core.security.defense.soteria_defense import SoteriaDefense
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-
-from ...defense.soteria_defense import SoteriaDefense
-from ...test.utils import (
-    create_fake_gradient_Cifar100,
-    create_fake_model_Cifar100,
-    create_fake_data_Cifar100,
-)
+import torch.nn as nn
+import logging
+from tests.security.utils import create_fake_gradient_Cifar100, create_fake_model_Cifar100, create_fake_data_Cifar100
 
 """
 TODO FIX: load model and the corresponding parameters from FedML system
