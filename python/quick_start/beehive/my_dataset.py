a6f5983d9288765391e0533351cefe4f7eb9c837
alexliang <alexliang.kh@gmail.com>
2022-08-26 19:57:34 +0800

    debug cross device example for quickstart.

--- python/quick_start/beehive/my_dataset.py
+++ python/quick_start/beehive/my_dataset.py
@@ -5,11 +5,11 @@ F = MNN.expr
 
 
 class MnistDataset(MNN.data.Dataset):
-    def __init__(self, training_dataset=True):
+    def __init__(self, training_dataset=True, root_path="./data"):
         super(MnistDataset, self).__init__()
         self.is_training_dataset = training_dataset
-        trainset = MNIST(root="./data", train=True, download=True)
-        testset = MNIST(root="./data", train=False, download=True)
+        trainset = MNIST(root=root_path, train=True, download=True)
+        testset = MNIST(root=root_path, train=False, download=True)
         if self.is_training_dataset:
             self.data = trainset.data / 255.0
             self.labels = trainset.targets
--- python/quick_start/beehive/torch_server.py
+++ python/quick_start/beehive/torch_server.py
@@ -1,3 +1,6 @@
+import os
+from os.path import expanduser
+
 import MNN
 
 import fedml
@@ -8,12 +11,25 @@ if __name__ == "__main__":
     # init FedML framework
     args = fedml.init()
 
+    # Handle argument compatibility for cross-platform
+    home_dir = expanduser("~")
+    args.data_cache_dir = str(args.data_cache_dir).replace('~', home_dir)
+    args.data_cache_dir = str(args.data_cache_dir).replace('\\', os.sep).replace('/', os.sep)
+    cur_dir = os.path.split(os.path.realpath(__file__))[0]
+    args.model_file_cache_folder = str(args.model_file_cache_folder).replace('\\', os.sep).replace('/', os.sep)
+    args.model_file_cache_folder = os.path.join(cur_dir, args.model_file_cache_folder)
+    args.global_model_file_path = str(args.global_model_file_path).replace('\\', os.sep).replace('/', os.sep)
+    args.global_model_file_path = os.path.join(cur_dir, args.global_model_file_path)
+    print("data_cache_dir {}".format(args.data_cache_dir))
+    print("model_file_cache_folder {}".format(args.model_file_cache_folder))
+    print("global_model_file_path {}".format(args.global_model_file_path))
+
     # init device
     device = fedml.device.get_device(args)
 
     # load data
-    train_dataset = MnistDataset(True)
-    test_dataset = MnistDataset(False)
+    train_dataset = MnistDataset(True, root_path=args.data_cache_dir)
+    test_dataset = MnistDataset(False, root_path=args.data_cache_dir)
     train_dataloader = MNN.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
     test_dataloader = MNN.data.DataLoader(
         test_dataset, batch_size=args.batch_size, shuffle=False
