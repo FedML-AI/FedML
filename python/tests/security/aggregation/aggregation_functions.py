4231108620367f9fdd1d9d4e3c7b583785e5a625
Shanshan <sshan0731@hotmail.com>
2022-08-26 16:17:10 -0700

    fix slsgd test case

--- python/tests/security/aggregation/aggregation_functions.py
+++ python/tests/security/aggregation/aggregation_functions.py
@@ -15,3 +15,18 @@ class AggregationFunction:
                 else:
                     avg_params[k] += local_model_params[k] * w
         return avg_params
+
+    @staticmethod
+    # FedAVG with 2 params; consistent with fedml aggregator; later will remove AggregationFunction.FedAVG
+    def FedAVG2(args, raw_grad_list):
+        (num0, avg_params) = raw_grad_list[0]
+        training_num = get_total_sample_num(raw_grad_list)
+        for k in avg_params.keys():
+            for i in range(0, len(raw_grad_list)):
+                local_sample_number, local_model_params = raw_grad_list[i]
+                w = local_sample_number / training_num
+                if i == 0:
+                    avg_params[k] = local_model_params[k] * w
+                else:
+                    avg_params[k] += local_model_params[k] * w
+        return avg_params
