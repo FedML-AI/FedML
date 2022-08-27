22b34d82e7892cf6c525c4ed6488a85287a3010f
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-27 15:18:47 -0700

    [bug]  distinguish its run_id to avoid incompatible message across ML frameworks.

--- python/examples/cross_silo/tf_mqtt_s3_fedavg_mnist_lr_example/run_client.sh
+++ python/examples/cross_silo/tf_mqtt_s3_fedavg_mnist_lr_example/run_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 RANK=$1
-python3 tf_client.py --cf config/fedml_config.yaml --rank $RANK --role client
+python3 tf_client.py --cf config/fedml_config.yaml --rank $RANK --role client --run_id tf_run_example
