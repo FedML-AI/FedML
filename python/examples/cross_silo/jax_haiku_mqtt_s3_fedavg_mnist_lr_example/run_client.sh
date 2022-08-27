c89e8c0374e40e7ee1148eac8ad29f1f6ed9dbc9
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-27 15:18:47 -0700

    [bug]  distinguish its run_id to avoid incompatible message across ML frameworks.

--- python/examples/cross_silo/jax_haiku_mqtt_s3_fedavg_mnist_lr_example/run_client.sh
+++ python/examples/cross_silo/jax_haiku_mqtt_s3_fedavg_mnist_lr_example/run_client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 RANK=$1
-python3 jax_haiku_client.py --cf config/fedml_config.yaml --rank $RANK --role client
+python3 jax_haiku_client.py --cf config/fedml_config.yaml --rank $RANK --role client --run_id jax_run_example
