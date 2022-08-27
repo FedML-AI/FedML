6c40b9d3fd1af227b1a70ca039ad4bb6243d1358
zhang-tuo-pdf <tuozhang@usc.edu>
2022-08-26 21:00:40 -0700

    secagg 3rd

--- python/fedml/cross_silo/fedml_server.py
+++ python/fedml/cross_silo/fedml_server.py
@@ -46,6 +46,21 @@ class FedMLCrossSiloServer:
                 model_trainer=None,
                 preprocessed_sampling_lists=None,
             )
+        
+        elif args.federated_optimizer == "SA":
+            from .secagg.sa_fedml_api import FedML_SA_Horizontal
+
+            FedML_SA_Horizontal(
+                args,
+                0,
+                args.worker_num,
+                args.comm,
+                device,
+                dataset,
+                model,
+                model_trainer=None,
+                preprocessed_sampling_lists=None,
+            )
         else:
             raise Exception("Exception")
 
