30a53d0529dff20c8674cfb3ae11f4784ea5843c
ranyide <yideran@gmail.com>
2022-08-28 22:28:29 +0800

    bugfix synthetic data url split data

--- python/examples/cross_silo/mqtt_s3_fedavg_cifar10_lr_example/torch_client.py
+++ python/examples/cross_silo/mqtt_s3_fedavg_cifar10_lr_example/torch_client.py
@@ -3,59 +3,60 @@ import torch
 import fedml
 from fedml import FedMLRunner
 from fedml.data.MNIST.data_loader import download_mnist, load_partition_data_mnist
-from .trainer.classification_aggregator import ClassificationAggregator
-from .trainer.classification_trainer import ClassificationTrainer
+from trainer.classification_aggregator import ClassificationAggregator
+from trainer.classification_trainer import ClassificationTrainer
+from fedml.data.data_loader import load
 
 
-def load_data(args):
-    download_mnist(args.data_cache_dir)
-    fedml.logging.info("load_data. dataset_name = %s" % args.dataset)
-
-    """
-    Please read through the data loader at to see how to customize the dataset for FedML framework.
-    """
-    (
-        client_num,
-        train_data_num,
-        test_data_num,
-        train_data_global,
-        test_data_global,
-        train_data_local_num_dict,
-        train_data_local_dict,
-        test_data_local_dict,
-        class_num,
-    ) = load_partition_data_mnist(
-        args,
-        args.batch_size,
-        train_path=args.data_cache_dir + "/MNIST/train",
-        test_path=args.data_cache_dir + "/MNIST/test",
-    )
-    """
-    For shallow NN or linear models, 
-    we uniformly sample a fraction of clients each round (as the original FedAvg paper)
-    """
-    args.client_num_in_total = client_num
-    dataset = [
-        train_data_num,
-        test_data_num,
-        train_data_global,
-        test_data_global,
-        train_data_local_num_dict,
-        train_data_local_dict,
-        test_data_local_dict,
-        class_num,
-    ]
-    return dataset, class_num
-
-
-class LogisticRegression(torch.nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(LogisticRegression, self).__init__()
-        self.linear = torch.nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        outputs = torch.sigmoid(self.linear(x))
-        return outputs
+# def load_data(args):
+#     download_mnist(args.data_cache_dir)
+#     fedml.logging.info("load_data. dataset_name = %s" % args.dataset)
+#
+#     """
+#     Please read through the data loader at to see how to customize the dataset for FedML framework.
+#     """
+#     (
+#         client_num,
+#         train_data_num,
+#         test_data_num,
+#         train_data_global,
+#         test_data_global,
+#         train_data_local_num_dict,
+#         train_data_local_dict,
+#         test_data_local_dict,
+#         class_num,
+#     ) = load_partition_data_mnist(
+#         args,
+#         args.batch_size,
+#         train_path=args.data_cache_dir + "/MNIST/train",
+#         test_path=args.data_cache_dir + "/MNIST/test",
+#     )
+#     """
+#     For shallow NN or linear models,
+#     we uniformly sample a fraction of clients each round (as the original FedAvg paper)
+#     """
+#     args.client_num_in_total = client_num
+#     dataset = [
+#         train_data_num,
+#         test_data_num,
+#         train_data_global,
+#         test_data_global,
+#         train_data_local_num_dict,
+#         train_data_local_dict,
+#         test_data_local_dict,
+#         class_num,
+#     ]
+#     return dataset, class_num
+#
+#
+# class LogisticRegression(torch.nn.Module):
+#     def __init__(self, input_dim, output_dim):
+#         super(LogisticRegression, self).__init__()
+#         self.linear = torch.nn.Linear(input_dim, output_dim)
+#
+#     def forward(self, x):
+#         outputs = torch.sigmoid(self.linear(x))
+#         return outputs
 
 
 if __name__ == "__main__":
@@ -66,13 +67,13 @@ if __name__ == "__main__":
     device = fedml.device.get_device(args)
 
     # load data
-    dataset, class_num = load_data(args)
+    dataset, class_num = load(args)
 
     # create model and trainer
     model = fedml.model.create(args, output_dim=class_num)
-    trainer = ClassificationTrainer(model=model, args=args)
-    aggregator = ClassificationAggregator(model=model, args=args)
+    # trainer = ClassificationTrainer(model=model, args=args)
+    # aggregator = ClassificationAggregator(model=model, args=args)
 
     # start training
-    fedml_runner = FedMLRunner(args, device, dataset, model, trainer, aggregator)
+    fedml_runner = FedMLRunner(args, device, dataset, model)
     fedml_runner.run()
