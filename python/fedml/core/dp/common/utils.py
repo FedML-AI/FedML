696869da17d812beab535eeed0665b18f380eacb
Shanshan <sshan0731@hotmail.com>
2022-08-27 23:04:19 -0700

    [dp] update laplace add noise; remove exponential mechanism

--- python/fedml/core/dp/common/utils.py
+++ python/fedml/core/dp/common/utils.py
@@ -1,8 +1,6 @@
 import secrets
 from numbers import Real, Integral
 
-import torch
-
 
 def check_bounds(lower, upper):
     if not isinstance(lower, Real) or not isinstance(upper, Real):
@@ -37,22 +35,6 @@ def check_params(epsilon, delta, sensitivity):
         raise TypeError("Sensitivity must be numeric")
     if sensitivity < 0:
         raise ValueError("Sensitivity must be non-negative")
-
-
-def vectorize_weight(state_dict):
-    weight_list = []
-    for (k, v) in state_dict.items():
-        if is_weight_param(k):
-            weight_list.append(v.flatten())
-    return torch.cat(weight_list)
-
-
-def is_weight_param(k):
-    return (
-            "running_mean" not in k
-            and "running_var" not in k
-            and "num_batches_tracked" not in k
-    )
 
 
 def bernoulli_neg_exp(gamma, rng=None):
