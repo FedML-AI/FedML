798cd4f8357d8580d8d8733d65b9c2ea086a93ba
Chaoyang He <chaoyanghe.com@gmail.com>
2022-08-27 20:55:25 -0700

    Update run_client.sh

--- python/examples/cross_silo/jax_haiku_mqtt_s3_fedavg_mnist_lr_example/run_client.sh
+++ python/examples/cross_silo/jax_haiku_mqtt_s3_fedavg_mnist_lr_example/run_client.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 RANK=$1
-python3 jax_haiku_client.py --cf config/fedml_config.yaml --rank $RANK --role client --run_id jax_run_example
+RUN_ID=$2
+python3 jax_haiku_client.py --cf config/fedml_config.yaml --rank $RANK --role client --run_id $RUN_ID
