9446a815927f96edff578de2af59508e4f2abfbf
Shanshan <sshan0731@hotmail.com>
2022-08-27 23:04:19 -0700

    [dp] update laplace add noise; remove exponential mechanism

--- python/fedml/core/dp/fed_privacy_mechanism.py
+++ python/fedml/core/dp/fed_privacy_mechanism.py
@@ -1,8 +1,4 @@
 import logging
-
-import numpy as np
-
-from .common.utils import is_weight_param, vectorize_weight
 from .mechanisms import Laplace, Gaussian
 
 
@@ -56,43 +52,43 @@ class FedMLDifferentialPrivacy:
         return self.dp_type
 
     def add_noise(self, grad):
-        noise_list_len = len(vectorize_weight(grad))
-        noise_list = np.zeros(noise_list_len)
-        vec_weight = vectorize_weight(grad)
-        for i in range(noise_list_len):
-            noise_list[i] = self.dp.compute_noise()
-        new_vec_grad = vec_weight + noise_list
-
-        new_grad = {}
-        index_bias = 0
-        print(f"noises in add_noise = {noise_list}")
-        for item_index, (k, v) in enumerate(grad.items()):
-            if is_weight_param(k):
-                new_grad[k] = new_vec_grad[index_bias : index_bias + v.numel()].view(
-                    v.size()
-                )
-                index_bias += v.numel()
-            else:
-                new_grad[k] = v
-        return new_grad
-
-    def add_a_noise_to_local_data(self, local_data):
-        new_data = []
-        for i in range(len(local_data)):
-            list = []
-            for x in local_data[i]:
-                y = self._compute_new_grad(x)
-                list.append(y)
-            new_data.append(tuple(list))
-        return new_data
-
-    def add_noise_with_data_distribution(self, grad):
         new_grad = dict()
         for k in grad.keys():
             new_grad[k] = self._compute_new_grad(grad[k])
         return new_grad
 
     def _compute_new_grad(self, grad):
-        noise = self.dp.compute_noise_with_shape(grad.shape)
+        noise = self.dp.compute_noise(grad.shape)
         print(f"noise computed with data distribution = {noise}")
         return noise + grad
+
+    # def add_noise(self, grad):
+    #     noise_list_len = len(vectorize_weight(grad))
+    #     noise_list = np.zeros(noise_list_len)
+    #     vec_weight = vectorize_weight(grad)
+    #     for i in range(noise_list_len):
+    #         noise_list[i] = self.dp.compute_noise()
+    #     new_vec_grad = vec_weight + noise_list
+    #
+    #     new_grad = {}
+    #     index_bias = 0
+    #     print(f"noises in add_noise = {noise_list}")
+    #     for item_index, (k, v) in enumerate(grad.items()):
+    #         if is_weight_param(k):
+    #             new_grad[k] = new_vec_grad[index_bias : index_bias + v.numel()].view(
+    #                 v.size()
+    #             )
+    #             index_bias += v.numel()
+    #         else:
+    #             new_grad[k] = v
+    #     return new_grad
+
+    # def add_a_noise_to_local_data(self, local_data):
+    #     new_data = []
+    #     for i in range(len(local_data)):
+    #         list = []
+    #         for x in local_data[i]:
+    #             y = self._compute_new_grad(x)
+    #             list.append(y)
+    #         new_data.append(tuple(list))
+    #     return new_data
--- python/fedml/core/dp/test/test_fed_privacy_mechanism.py
+++ python/fedml/core/dp/test/test_fed_privacy_mechanism.py
@@ -1,11 +1,15 @@
 import argparse
-
 import torch
-
 from ..fed_privacy_mechanism import FedMLDifferentialPrivacy
 
+a_local_w = dict()
+a_local_w["linear.weight"] = torch.FloatTensor(
+    [[0.1, 0.2, 0.2, 0.1], [0.15, 0.12, 0.02, 0.2], [0.3, 0.01, 0.21, 0.11]]
+)
+a_local_w["linear.bias"] = torch.FloatTensor([0.01, 0.19, 0.21])
 
-def add_args():
+
+def add_gaussian_args():
     parser = argparse.ArgumentParser(description="FedML")
     parser.add_argument(
         "--yaml_config_file",
@@ -14,7 +18,6 @@ def add_args():
         type=str,
         default="",
     )
-    # default arguments
     parser.add_argument("--enable_dp", type=bool, default=True)
     parser.add_argument("--mechanism_type", type=str, default="gaussian")
     parser.add_argument("--epsilon", type=float, default=0.1)
@@ -27,36 +30,39 @@ def add_args():
     return args
 
 
-def test_FedMLDifferentialPrivacy():
-    print("----------- test_FedMLDifferentialPrivacy -----------")
-    FedMLDifferentialPrivacy.get_instance().init(add_args())
-    a_local_w = dict()
-    a_local_w["linear.weight"] = torch.FloatTensor(
-        [[0.1, 0.2, 0.2, 0.1], [0.15, 0.12, 0.02, 0.2], [0.3, 0.01, 0.21, 0.11]]
+def add_laplace_args():
+    parser = argparse.ArgumentParser(description="FedML")
+    parser.add_argument(
+        "--yaml_config_file",
+        "--cf",
+        help="yaml configuration file",
+        type=str,
+        default="",
     )
-    a_local_w["linear.bias"] = torch.FloatTensor([0.01, 0.19, 0.21])
-    k = "linear.weight"
+    parser.add_argument("--enable_dp", type=bool, default=True)
+    parser.add_argument("--mechanism_type", type=str, default="laplace")
+    parser.add_argument("--epsilon", type=float, default=0.1)
+    parser.add_argument("--delta", type=float, default=0)
+    parser.add_argument("--sensitivity", type=float, default=1.0)
+    parser.add_argument("--dp_type", type=str, default="ldp")
+    args, unknown = parser.parse_known_args()
+    return args
+
+
+def test_FedMLDifferentialPrivacy_gaussian():
+    print("----------- test_FedMLDifferentialPrivacy - gaussian mechanism -----------")
+    FedMLDifferentialPrivacy.get_instance().init(add_gaussian_args())
     print(f"grad = {a_local_w}")
     print(FedMLDifferentialPrivacy.get_instance().add_noise(a_local_w))
 
 
-def test_add_noise_with_data_distribution():
-    print("----------- test_add_noise_with_data_distribution -----------")
-    FedMLDifferentialPrivacy.get_instance().init(add_args())
-    a_local_w = dict()
-    a_local_w["linear.weight"] = torch.FloatTensor(
-        [[0.1, 0.2, 0.2, 0.1], [0.15, 0.12, 0.02, 0.2], [0.3, 0.01, 0.21, 0.11]]
-    )
-    a_local_w["linear.bias"] = torch.FloatTensor([0.01, 0.19, 0.21])
-    k = "linear.weight"
+def test_FedMLDifferentialPrivacy_laplace():
+    print("----------- test_FedMLDifferentialPrivacy - laplace mechanism -----------")
+    FedMLDifferentialPrivacy.get_instance().init(add_laplace_args())
     print(f"grad = {a_local_w}")
-    print(
-        FedMLDifferentialPrivacy.get_instance().add_noise_with_data_distribution(
-            a_local_w
-        )
-    )
+    print(FedMLDifferentialPrivacy.get_instance().add_noise(a_local_w))
 
 
 if __name__ == "__main__":
-    test_FedMLDifferentialPrivacy()
-    test_add_noise_with_data_distribution()
+    test_FedMLDifferentialPrivacy_gaussian()
+    test_FedMLDifferentialPrivacy_laplace()
