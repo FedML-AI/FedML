e83266866068446f2e960df3c57b4e377ec58fbc
Shanshan <sshan0731@hotmail.com>
2022-08-31 01:34:33 -0700

    [dp] add budget account

--- python/fedml/core/dp/mechanisms/gaussian.py
+++ python/fedml/core/dp/mechanisms/gaussian.py
@@ -1,21 +1,15 @@
 import numpy as np
 import torch
 from .base_dp_mechanism import BaseDPMechanism
-from ..common.utils import check_params
 
 
 class Gaussian(BaseDPMechanism):
-    r"""The Gaussian mechanism in differential privacy.
-    Some codes refer to IBM DP Library: https://github.com/IBM/differential-privacy-library
-    """
-
     def __init__(self, *, epsilon, delta, sensitivity):
-        check_params(epsilon, delta, sensitivity)
         if epsilon == 0 or delta == 0:
             raise ValueError("Neither Epsilon nor Delta can be zero")
         if epsilon > 1.0:
             raise ValueError(
-                "Epsilon cannot be greater than 1. If required, use GaussianAnalytic instead."
+                "Epsilon cannot be greater than 1. "
             )
         self._scale = (
             np.sqrt(2 * np.log(1.25 / float(delta)))
@@ -23,8 +17,11 @@ class Gaussian(BaseDPMechanism):
             / float(epsilon)
         )
 
-    def variance(self, value):
-        return self._scale**2
-
     def compute_noise(self, size):
         return torch.normal(mean=0, std=self._scale, size=size)
+
+    # def clip_gradients(self, grad): # Gaussian: 2 norm
+    #     new_grad = dict()
+    #     for k in grad.keys():
+    #         new_grad[k] = max(1, grad[k].norm(2)) / self.clipping
+    #     return new_grad
