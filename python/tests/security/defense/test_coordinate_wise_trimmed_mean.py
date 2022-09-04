5ba014692af2015de057e9c4c9c4ba60d8fca2ca
Shanshan <sshan0731@hotmail.com>
2022-09-03 20:40:00 -0700

    add crfl test case && config file

--- python/tests/security/defense/test_coordinate_wise_trimmed_mean.py
+++ python/tests/security/defense/test_coordinate_wise_trimmed_mean.py
@@ -4,7 +4,7 @@ from fedml.core.security.defense.coordinate_wise_trimmed_mean_defense import (
     CoordinateWiseTrimmedMeanDefense,
 )
 from fedml.ml.aggregator.agg_operator import FedMLAggOperator
-from utils import create_fake_model_list
+from .utils import create_fake_model_list
 
 
 def add_args():
