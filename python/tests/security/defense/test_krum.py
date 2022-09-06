306437efe5b808eb29338ae4ce2bb951229b4875
Shanshan <sshan0731@hotmail.com>
2022-09-05 18:22:41 -0700

    [defense] krum: upd test case

--- python/tests/security/defense/test_krum.py
+++ python/tests/security/defense/test_krum.py
@@ -1,36 +1,24 @@
 import argparse
 from fedml.core.security.defense.krum_defense import KrumDefense
-from utils import create_fake_model_list
+from .utils import create_fake_model_list
 
 
-def add_args_krum():
+def add_args_krum(isMultiKrum):
     parser = argparse.ArgumentParser(description="FedML")
     parser.add_argument(
         "--yaml_config_file", "--cf", help="yaml configuration file", type=str, default="",
     )
-    parser.add_argument("--federated_optimizer", type=str, default="FedAvg")
-    parser.add_argument("--byzantine_client_num", type=int, default=2)
-    parser.add_argument("--multi", type=bool, default=False)
-    args, unknown = parser.parse_known_args()
-    return args
-
-
-def add_args_multi_krum():
-    parser = argparse.ArgumentParser(description="FedML")
-    parser.add_argument(
-        "--yaml_config_file", "--cf", help="yaml configuration file", type=str, default="",
-    )
-    parser.add_argument("--federated_optimizer", type=str, default="FedAvg")
-    parser.add_argument("--byzantine_client_num", type=int, default=2)
-    parser.add_argument("--multi", type=bool, default=True)
+    parser.add_argument("--byzantine_client_num", type=int, default=1)
+    if isMultiKrum:
+        parser.add_argument("--krum_param_m", type=bool, default=2)
     args, unknown = parser.parse_known_args()
     return args
 
 
 def test_defense_Krum():
-    model_list = create_fake_model_list(10)
+    model_list = create_fake_model_list(20)
     print(f"test krum")
-    krum = KrumDefense(add_args_krum())
+    krum = KrumDefense(add_args_krum(isMultiKrum=False))
     filtered_model_list = krum.defend_before_aggregation(model_list)
     print(f"filtered_model_list={filtered_model_list}")
 
@@ -38,7 +26,7 @@ def test_defense_Krum():
 def test_defense_multi_Krum():
     model_list = create_fake_model_list(20)
     print(f"test multi-krum")
-    krum = KrumDefense(add_args_multi_krum())
+    krum = KrumDefense(add_args_krum(isMultiKrum=True))
     filtered_model_list = krum.defend_before_aggregation(model_list)
     print(f"filtered_model_list={filtered_model_list}")
 
