0acf5e6dbb5619bbe2288678036d2b125e320fda
Chaoyang He <chaoyanghe.com@gmail.com>
2022-09-06 13:56:17 -0700

    [github actions] add flow test

--- python/fedml/core/distributed/flow/test_run_server.sh
+++ python/fedml/core/distributed/flow/test_run_server.sh
@@ -1,2 +1,2 @@
-
-python test_fedml_flow.py --cf fedml_config.yaml --rank 0 --role server --run_id ch_flow
+RUN_ID=$1
+python test_fedml_flow.py --cf fedml_config.yaml --rank 0 --role server --run_id $RUN_ID
