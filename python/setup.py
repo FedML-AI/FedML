2aef3b15b908a60af72b5b3bcf598a23d1f74e09
Alex <alex.gpt.llm@gmail.com>
2022-09-06 18:54:00 +0800

    fixed printing args issue with mpi.

--- python/setup.py
+++ python/setup.py
@@ -71,7 +71,7 @@ if platform.machine() == "x86_64":
 
 setup(
     name="fedml",
-    version="0.7.311",
+    version="0.7.312",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
