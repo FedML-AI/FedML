75809888f14e64645fb7dd40c083ed1616940edb
Shanshan <sshan0731@hotmail.com>
2022-09-05 17:13:16 -0700

    [defense] multikrum & krum: add to defender; add test case

--- python/fedml/core/security/defense/krum_defense.py
+++ python/fedml/core/security/defense/krum_defense.py
@@ -13,11 +13,12 @@ https://arxiv.org/pdf/1703.02757.pdf
 class KrumDefense(BaseDefenseMethod):
     def __init__(self, config):
         self.config = config
-        self.k = config.byzantine_client_num  # assume there are k byzantine clients
-        if config.multi:
-            self.multi = False
-        else:
-            self.multi = config.multi  # krum or multi-krum
+        self.byzantine_client_num = config.byzantine_client_num
+
+        # krum_param_m = 1: krum; krum_param_m > 1: multi-krum
+        self.krum_param_m = 1  # krum
+        if hasattr(config, "krum_param_m") and isinstance(config.krum_param_m, int):
+            self.krum_param_m = config.krum_param_m
 
     def run(
         self,
@@ -25,32 +26,37 @@ class KrumDefense(BaseDefenseMethod):
         base_aggregation_func: Callable = None,
         extra_auxiliary_info: Any = None,
     ) -> Dict:
+        pass
+
+    def defend_before_aggregation(
+            self,
+            raw_client_grad_list: List[Tuple[float, Dict]],
+            extra_auxiliary_info: Any = None,
+    ):
         num_client = len(raw_client_grad_list)
+        # in the Krum paper, it says 2 * byzantine_client_num + 2 < client #
+        if 2 * self.byzantine_client_num + 2 >= num_client - self.krum_param_m:
+            raise ValueError(
+                "byzantine_client_num conflicts with requirements in Krum: 2 * byzantine_client_num + 2 < client number - krum_param_m"
+            )
+
         vec_local_w = [
-            (raw_client_grad_list[i][0], utils.vectorize_weight(raw_client_grad_list[i][1]))
-            for i in range(0, num_client)
-        ]
-        print(vec_local_w)
-        krum_score = self._compute_krum_score(vec_local_w)
-        index = torch.argsort(torch.Tensor(krum_score)).tolist()
-        if self.multi is True:
-            index = index[0:num_client - self.k]
-        else:
-            index = index[0:1]
+            utils.vectorize_weight(raw_client_grad_list[i][1])
+            for i in range(0, num_client)]
+        krum_scores = self._compute_krum_score(vec_local_w)
+        score_index = torch.argsort(torch.Tensor(krum_scores)).tolist()  # indices; ascending
+        score_index = score_index[0:self.krum_param_m]
+        return [raw_client_grad_list[i] for i in score_index]
 
-        grad_list = [raw_client_grad_list[i] for i in index]
-        print(f"krum_scores = {krum_score}")
-        return base_aggregation_func(self.config, grad_list)  # avg_params
-
-    def _compute_krum_score(self, local_w):
-        krum_score = []
-        num_client = len(local_w)
+    def _compute_krum_score(self, vec_grad_list):
+        krum_scores = []
+        num_client = len(vec_grad_list)
         for i in range(0, num_client):
             dists = []
             for j in range(0, num_client):
                 if i != j:
-                    dists.append(utils.compute_euclidean_distance(local_w[i][1], local_w[j][1]).item())
-            dists.sort()
-            score = dists[0 : num_client - self.k]
-            krum_score.append(sum(score))
-        return krum_score
+                    dists.append(utils.compute_euclidean_distance(vec_grad_list[i], vec_grad_list[j]).item())
+            dists.sort()  # ascending
+            score = dists[0:num_client - self.byzantine_client_num - 2]
+            krum_scores.append(sum(score))
+        return krum_scores
