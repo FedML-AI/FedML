c0120986f4939243603a1cc1c976e29a34e15427
wizard1203 <956234606@qq.com>
2022-09-08 00:14:23 -0700

    aa

--- python/examples/simulation/mpi_torch_fedavg/main.py
+++ python/examples/simulation/mpi_torch_fedavg/main.py
@@ -27,6 +27,11 @@ from fedml.arguments import Arguments
 
 from fedml import FedMLRunner
 
+from fedml.data.reddit.data_loader import load_partition_data_reddit
+
+from reddit_trainer import RedditTrainer
+from reddit_aggregator import RedditAggregator
+
 
 
 def add_args():
@@ -125,7 +130,21 @@ if __name__ == "__main__":
         process_id: {args.process_id}, device: {device} ==============" )
 
     # load data
-    dataset, output_dim = fedml.data.load(args)
+    if args.dataset == "reddit":
+
+        dataset = load_partition_data_reddit(
+            args,
+            args.dataset,
+            args.data_cache_dir,
+            args.partition_method,
+            args.partition_alpha,
+            args.client_num_in_total,
+            args.batch_size,
+            n_proc_in_silo=0,
+        )
+        output_dim = 10000
+    else:
+        dataset, output_dim = fedml.data.load(args)
 
     if args.dataset == "femnist":
         in_channels = 1
@@ -143,14 +162,31 @@ if __name__ == "__main__":
     elif args.model == "resnet18_cifar":
         logging.info("ResNet18_GN")
         model = resnet18_cifar(group_norm=args.group_norm_channels, num_classes=output_dim)
+    elif args.model == "albert-base-v2":
+        from transformers import (AdamW, AlbertTokenizer, AutoConfig,
+                                AutoModelWithLMHead, AutoTokenizer,
+                                MobileBertForPreTraining)
+        config = AutoConfig.from_pretrained(
+            os.path.join(args.data_cache_dir, args.model+'-config.json'))
+        model = AutoModelWithLMHead.from_config(config)
     else:
         model = fedml.model.create(args, output_dim)
 
-    # start training
-    fedml_runner = FedMLRunner(args, device, dataset, model)
-    fedml_runner.run()
-    # simulator = SimulatorMPI(args, device, dataset, model)
-    # simulator.run()
+
+    if args.dataset == "reddit":
+        from reddit_trainer import RedditTrainer
+        from reddit_aggregator import RedditAggregator
+        client_trainer = RedditTrainer(model, args)
+        server_aggregator = RedditAggregator(model, args)
+        fedml_runner = FedMLRunner(args, device, dataset, model,
+            client_trainer=client_trainer, server_aggregator=server_aggregator)
+        fedml_runner.run()
+    else:
+        # start training
+        fedml_runner = FedMLRunner(args, device, dataset, model)
+        fedml_runner.run()
+        # simulator = SimulatorMPI(args, device, dataset, model)
+        # simulator.run()
 
 
 
