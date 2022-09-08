991d90eb3a90226077a07e091aca8103e8912c9d
Alex <alex.gpt.llm@gmail.com>
2022-09-08 23:24:59 +0800

    Update wheels and upload pypi.

--- python/setup.py
+++ python/setup.py
@@ -75,7 +75,7 @@ if platform.machine() == "x86_64":
 
 setup(
     name="fedml",
-    version="0.7.315",
+    version="0.7.316",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
