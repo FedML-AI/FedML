9f54be72ae1e00b62689080863bc2fff0c687eef
Alex <alex.gpt.llm@gmail.com>
2022-09-08 22:53:21 +0800

    debug wheels.

--- python/build_tools/github/test_source.sh
+++ python/build_tools/github/test_source.sh
@@ -8,5 +8,6 @@ cd ../../
 python -m venv test_env
 source test_env/bin/activate
 
+python -m pip install -U wheel setuptools
 python -m pip install FedML/python/dist/*.tar.gz
 python -c "import fedml; fedml.__version__"
