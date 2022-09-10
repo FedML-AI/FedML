8b94110b0e782ff5e621427f0313b2a53ccd24d4
Chaoyang He <chaoyanghe.com@gmail.com>
2022-09-10 12:47:47 -0700

    [dp] add local dp examples

--- python/fedml/core/alg_frame/client_trainer.py
+++ python/fedml/core/alg_frame/client_trainer.py
@@ -1,5 +1,8 @@
-from abc import ABC, abstractmethod
 import logging
+from abc import ABC, abstractmethod
+
+from ..security.fedml_attacker import FedMLAttacker
+from ..security.fedml_defender import FedMLDefender
 from ...core.dp.fedml_differential_privacy import FedMLDifferentialPrivacy
 
 
@@ -15,6 +18,7 @@ class ClientTrainer(ABC):
         self.model = model
         self.id = 0
         self.args = args
+        FedMLDifferentialPrivacy.get_instance().init(args)
 
     def set_id(self, trainer_id):
         self.id = trainer_id
@@ -35,11 +39,10 @@ class ClientTrainer(ABC):
         pass
 
     def on_after_local_training(self, train_data, device, args):
-        pass
-        # if FedMLDifferentialPrivacy.get_instance().is_local_dp_enabled():
-        #     logging.info("-----add local DP noise ----")
-        #     model_params_with_dp_noise = FedMLDifferentialPrivacy.get_instance().add_local_noise(self.get_model_params())
-        #     self.set_model_params(model_params_with_dp_noise)
-            
+        if FedMLDifferentialPrivacy.get_instance().is_local_dp_enabled():
+            logging.info("-----add local DP noise ----")
+            model_params_with_dp_noise = FedMLDifferentialPrivacy.get_instance().add_local_noise(self.get_model_params())
+            self.set_model_params(model_params_with_dp_noise)
+
     def test(self, test_data, device, args):
         pass
