3569f24d48b7258cb668a1206ca6670d5e9655f8
Shanshan <sshan0731@hotmail.com>
2022-09-11 01:28:15 -0700

    [dp]

--- python/fedml/core/dp/common/constants.py
+++ python/fedml/core/dp/common/constants.py
@@ -1,3 +1,3 @@
 NBAFL_DP = "nbafl"
-DP_NAIVE_LDP = "naive_ldp"
-DP_NAIVE_CDP = "naive_cdp"
+DP_LDP = "ldp"
+DP_CDP = "cdp"
--- python/fedml/core/dp/solutions/base_dp_solution.py
+++ python/fedml/core/dp/solutions/base_dp_solution.py
@@ -13,26 +13,8 @@ class BaseDPSolution(ABC):
     def set_ldp(self, dp_mechanism: DPMechanism):
         self.ldp = dp_mechanism
 
-    def is_local_dp(self):
-        return self.ldp is not None
-
-    def is_central_dp(self):
-        return self.cdp is not None
-
-    def before_add_local_noise(self, local_grad: dict):
-        pass
-
     def add_local_noise(self, local_grad: dict):
-        pass
-
-    def after_add_local_noise(self, local_grad: dict):
-        pass
-
-    def before_add_global_noise(self, global_model: dict):
-        pass
+        return self.ldp.add_noise(grad=local_grad)
 
     def add_global_noise(self, global_model: dict):
         return self.cdp.add_noise(grad=global_model)
-
-    def after_add_global_noise(self, global_model: dict):
-        pass
