93e98c0ad27d0b9db47c63c9843df57ad41d98dd
alexliang <alexliang.kh@gmail.com>
2022-09-23 00:06:29 +0800

    update bootstrap on windows.

--- python/setup.py
+++ python/setup.py
@@ -73,7 +73,7 @@ if platform.machine() == "x86_64":
 
 setup(
     name="fedml",
-    version="0.7.321",
+    version="0.7.323",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
