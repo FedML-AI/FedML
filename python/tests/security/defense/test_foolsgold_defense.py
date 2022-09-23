104a7dc8a9f7a7dd64627138d869c0cc341130f8
Chaoyang He <chaoyanghe.com@gmail.com>
2022-09-22 21:01:24 -0700

    debug foolsgold

--- python/tests/security/defense/test_foolsgold_defense.py
+++ python/tests/security/defense/test_foolsgold_defense.py
@@ -1,17 +1,19 @@
 import argparse
-import copy
+
+import torch
 
 from fedml.core.security.defense.foolsgold_defense import FoolsGoldDefense
 from fedml.ml.aggregator.agg_operator import FedMLAggOperator
-from utils import create_fake_model_list_MNIST, create_fake_model_list
 
-import torch
-import numpy as np
 
 def add_args(use_memory):
     parser = argparse.ArgumentParser(description="FedML")
     parser.add_argument(
-        "--yaml_config_file", "--cf", help="yaml configuration file", type=str, default="",
+        "--yaml_config_file",
+        "--cf",
+        help="yaml configuration file",
+        type=str,
+        default="",
     )
     parser.add_argument("--federated_optimizer", type=str, default="FedAvg")
     parser.add_argument("--use_memory", type=bool, default=use_memory)
@@ -19,14 +21,12 @@ def add_args(use_memory):
     return args
 
 
-
 def test_defense():
     config = add_args(use_memory=True)
-    # model_list = create_fake_model_list_MNIST(3)
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11', pretrained=True).state_dict()
-    model_list = [(100, model) for i in range(3)]
+    model = torch.hub.load("pytorch/vision:v0.10.0", "vgg11", pretrained=True).state_dict()
+    model_list = [(100, model) for i in range(6)]
 
-    print(f"modellist len = {len(model_list)}")
+    print(f"model_list len = {len(model_list)}")
     defense = FoolsGoldDefense(config)
     aggr_result = defense.run(model_list, base_aggregation_func=FedMLAggOperator.agg)
     # print(f"result = {aggr_result}")
