7a9a87eb07ec4ad218af6a18663cb93b061d735a
Chaoyang He <chaoyanghe.com@gmail.com>
2022-09-22 20:16:00 -0700

    debug foolsgold

--- python/tests/security/defense/test_foolsgold_defense.py
+++ python/tests/security/defense/test_foolsgold_defense.py
@@ -1,8 +1,12 @@
 import argparse
+import copy
+
 from fedml.core.security.defense.foolsgold_defense import FoolsGoldDefense
 from fedml.ml.aggregator.agg_operator import FedMLAggOperator
 from utils import create_fake_model_list_MNIST, create_fake_model_list
 
+import torch
+import numpy as np
 
 def add_args(use_memory):
     parser = argparse.ArgumentParser(description="FedML")
@@ -15,21 +19,18 @@ def add_args(use_memory):
     return args
 
 
-def test_fools_gold_score():
-    model_list = create_fake_model_list(5)
-    print(f"modellist len = {len(model_list)}")
-    grads = [grad for num, grad in model_list]
-    print(f"fools_gold_score={FoolsGoldDefense.fools_gold_score(grads)}")
-
 
 def test_defense():
     config = add_args(use_memory=True)
-    model_list = create_fake_model_list_MNIST(3)
+    # model_list = create_fake_model_list_MNIST(3)
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11', pretrained=True).state_dict()
+    model_list = [(100, model) for i in range(3)]
+
+    print(f"modellist len = {len(model_list)}")
     defense = FoolsGoldDefense(config)
     aggr_result = defense.run(model_list, base_aggregation_func=FedMLAggOperator.agg)
-    print(f"result = {aggr_result}")
+    # print(f"result = {aggr_result}")
 
 
 if __name__ == "__main__":
-    test_fools_gold_score()
     test_defense()
