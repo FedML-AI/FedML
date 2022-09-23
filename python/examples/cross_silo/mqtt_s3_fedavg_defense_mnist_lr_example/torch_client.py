4c015db7ebd2f2c186421815fbdf63095e3a4b03
Chaoyang He <chaoyanghe.com@gmail.com>
2022-09-23 12:26:12 -0700

    change the model for defense method to CIFAR-10

--- python/examples/cross_silo/mqtt_s3_fedavg_defense_mnist_lr_example/torch_client.py
+++ python/examples/cross_silo/mqtt_s3_fedavg_defense_mnist_lr_example/torch_client.py
@@ -1,6 +1,17 @@
 import fedml
 from fedml import FedMLRunner
 
+from fedml.model.cv.resnet56.resnet_pretrained import resnet56_pretrained
+
+
+def create_model():
+    # please download the pre-trained weight file from
+    # https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar10_resnet44-2a3cabcb.pt
+    pre_trained_model_path = "./resnet56_on_cifar10.pt"
+    model = resnet56_pretrained(10, pretrained=True, path=pre_trained_model_path)
+    return model
+
+
 if __name__ == "__main__":
     args = fedml.init()
 
@@ -11,7 +22,8 @@ if __name__ == "__main__":
     dataset, output_dim = fedml.data.load(args)
 
     # load model
-    model = fedml.model.create(args, output_dim)
+    model = create_model()
+
 
     # start training
     fedml_runner = FedMLRunner(args, device, dataset, model)
