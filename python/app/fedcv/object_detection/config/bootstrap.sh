151ae5bd3550b990cba643e8a7270629dada94a4
Alex <alex.gpt.llm@gmail.com>
2022-10-05 17:57:49 +0800

    update bootstrap.sh.

--- python/app/fedcv/object_detection/config/bootstrap.sh
+++ python/app/fedcv/object_detection/config/bootstrap.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-### don't modify this part ###
-# set -x
-##############################
-
 
 ### please customize your script in this region ####
 pip install opencv-python-headless pandas matplotlib seaborn addict
