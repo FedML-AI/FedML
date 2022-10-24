da02309cff62e8ecddb2526baa1fc4d0f2a302ff
Alex <alex.gpt.llm@gmail.com>
2022-10-24 23:00:49 +0800

    update fedml docker build files.

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -7,31 +7,48 @@ PYTHON_VERSION=$4
 PYTORCH_VERSION=$5
 NCCL_VERSION=$6
 CUDA_VERSION=$7
-
-
-if [[ "$ARCH" == "x86_64" ]]
-then
-  docker build . -f ./x86-64/Dockerfile \
-  --build-arg OS=$OS \
-  --build-arg DISTRO=$DISTRO \
-  --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-  --build-arg PYTORCH_VERSION=$PYTORCH_VERSION \
-  --build-arg NCCL_VERSION=$NCCL_VERSION \
-  --build-arg CUDA_VERSION=$CUDA_VERSION \
-  --network=host
-
-elif [[  "$ARCH" == "arm64v8_m1" ]]
-  docker build . -f ./arm64v8-apple-m1/Dockerfile \
-  --build-arg OS=$OS \
-  --build-arg DISTRO=$DISTRO \
-  --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-  --build-arg PYTORCH_VERSION=$PYTORCH_VERSION \
-  --build-arg NCCL_VERSION=$NCCL_VERSION \
-  --build-arg CUDA_VERSION=$CUDA_VERSION \
-  --network=host
-then
-   echo "TBD"
-else
-   echo "TBD"
+OUTPUT_IMAGE=$8
+NVIDIA_BASE_IMAGE=""
+if [ $# == 9 ]; then
+  NVIDIA_BASE_IMAGE=$9
 fi
+
+
+DOCKER_FILE_PATH=""
+if [[ "$ARCH" == "x86_64" ]]; then
+  DOCKER_FILE_PATH=./x86-64/Dockerfile
+elif [[  "$ARCH" == "arm64v8" ]]; then
+  DOCKER_FILE_PATH=./arm64v8/Dockerfile
+elif [[  "$ARCH" == "arm64v8_m1" ]]; then
+  DOCKER_FILE_PATH=./arm64v8-apple-m1/Dockerfile
+fi
+
+if [ $DOCKER_FILE_PATH == "" ]; then
+  echo "Please specify correct arch (choices: x86_64 / arm64v8 /arm64v8_m1)"
+  exit -1
+fi
+
+if [ $NVIDIA_BASE_IMAGE != "" ]; then
+    docker build -f ./x86-64/Dockerfile \
+    --build-arg OS=$OS \
+    --build-arg DISTRO=$DISTRO \
+    --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+    --build-arg PYTORCH_VERSION=$PYTORCH_VERSION \
+    --build-arg NCCL_VERSION=$NCCL_VERSION \
+    --build-arg CUDA_VERSION=$CUDA_VERSION \
+    --build-arg NVIDIA_BASE_IMAGE=$NVIDIA_BASE_IMAGE \
+    --network=host \
+    -t $OUTPUT_IMAGE .
+else
+    docker build -f ./x86-64/Dockerfile \
+    --build-arg OS=$OS \
+    --build-arg DISTRO=$DISTRO \
+    --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+    --build-arg PYTORCH_VERSION=$PYTORCH_VERSION \
+    --build-arg NCCL_VERSION=$NCCL_VERSION \
+    --build-arg CUDA_VERSION=$CUDA_VERSION \
+    --network=host \
+    -t $OUTPUT_IMAGE .
+fi
+
 
