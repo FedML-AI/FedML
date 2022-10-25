1d0d924c1aad7e9c13e5ea62b301dc929a285dde
Alex <alex.gpt.llm@gmail.com>
2022-10-25 23:52:58 +0800

    update fedml dockerfile and jenkinsfile.

--- devops/scripts/build-fedml-docker.sh
+++ devops/scripts/build-fedml-docker.sh
@@ -10,7 +10,7 @@ PYTHON_VERSION=3.7
 PYTORCH_VERSION=1.12.1
 NCCL_VERSION=2.9.9
 CUDA_VERSION=11.3
-OUTPUT_IMAGE=pytorch/pytorch:1.12.1-cuda11.3-cudnn8-devel
+OUTPUT_IMAGE=public.ecr.aws/x6k8q1x9/pytorch:1.12.1-cuda11.3-cudnn8-devel
 NVIDIA_BASE_IMAGE=nvidia/cuda:11.3.1-cudnn8-devel-ubuntu18.04
 PYTORCH_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cu113
 PYTORCH_GEOMETRIC_URL=https://data.pyg.org/whl/torch-1.12.0+cu113.html
@@ -19,3 +19,6 @@ bash build-docker.sh $ARCH $OS $DISTRO $PYTHON_VERSION $PYTORCH_VERSION $NCCL_VE
      $OUTPUT_IMAGE $NVIDIA_BASE_IMAGE $PYTORCH_EXTRA_INDEX_URL $PYTORCH_GEOMETRIC_URL
 
 cd $pwd
+
+docker login --username AWS --password $(aws ecr-public get-login-password --region us-east-1) public.ecr.aws
+docker push ${OUTPUT_IMAGE}
