173b30d384a56d59e7aa1905fb50707bf4028a2b
Alex <alex.gpt.llm@gmail.com>
2022-10-25 23:35:44 +0800

    update fedml dockerfile.

--- devops/scripts/build-fedml-docker.sh
+++ devops/scripts/build-fedml-docker.sh
@@ -12,6 +12,10 @@ NCCL_VERSION=2.9.9
 CUDA_VERSION=11.3
 OUTPUT_IMAGE=pytorch/pytorch:1.12.1-cuda11.3-cudnn8-devel
 NVIDIA_BASE_IMAGE=nvidia/cuda:11.3.1-cudnn8-devel-ubuntu18.04
-cd ../../docker
+PYTORCH_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cu113
+PYTORCH_GEOMETRIC_URL=https://data.pyg.org/whl/torch-1.12.0+cu113.html
+cd ./docker
 bash build-docker.sh $ARCH $OS $DISTRO $PYTHON_VERSION $PYTORCH_VERSION $NCCL_VERSION $CUDA_VERSION \
-     $OUTPUT_IMAGE $NVIDIA_BASE_IMAGE
+     $OUTPUT_IMAGE $NVIDIA_BASE_IMAGE $PYTORCH_EXTRA_INDEX_URL $PYTORCH_GEOMETRIC_URL
+
+cd $pwd
--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -12,6 +12,17 @@ NVIDIA_BASE_IMAGE=""
 if [ $# == 9 ]; then
   NVIDIA_BASE_IMAGE=$9
 fi
+if [ $# == 10 ]; then
+  PYTORCH_EXTRA_INDEX_URL=$10
+else
+  PYTORCH_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cu113
+fi
+
+if [ $# == 11 ]; then
+  PYTORCH_GEOMETRIC_URL=$11
+else
+  PYTORCH_GEOMETRIC_URL=https://data.pyg.org/whl/torch-1.12.0+cu113.html
+fi
 
 
 DOCKER_FILE_PATH=""
@@ -37,6 +48,8 @@ if [ $NVIDIA_BASE_IMAGE != "" ]; then
     --build-arg NCCL_VERSION=$NCCL_VERSION \
     --build-arg CUDA_VERSION=$CUDA_VERSION \
     --build-arg NVIDIA_BASE_IMAGE=$NVIDIA_BASE_IMAGE \
+    --build-arg PYTHON_EXTRA_INDEX_URL=$PYTHON_EXTRA_INDEX_URL \
+    --build-arg PYTORCH_GEOMETRIC_URL=$PYTORCH_GEOMETRIC_URL \
     --network=host \
     -t $OUTPUT_IMAGE .
 else
@@ -47,6 +60,8 @@ else
     --build-arg PYTORCH_VERSION=$PYTORCH_VERSION \
     --build-arg NCCL_VERSION=$NCCL_VERSION \
     --build-arg CUDA_VERSION=$CUDA_VERSION \
+    --build-arg PYTORCH_EXTRA_INDEX_URL=$PYTORCH_EXTRA_INDEX_URL \
+    --build-arg PYTORCH_GEOMETRIC_URL=$PYTORCH_GEOMETRIC_URL \
     --network=host \
     -t $OUTPUT_IMAGE .
 fi
