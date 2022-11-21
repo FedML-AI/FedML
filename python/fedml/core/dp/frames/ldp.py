9b84cf389231df899766be4ece3badc87c9a5f21
Chaoyang He <chaoyanghe.com@gmail.com>
2022-11-20 22:41:52 -0800

    [bug] fix the wrong type (dict) of cross-silo CDP/LDP aggregated model to (OrderedDict)

--- python/fedml/core/dp/frames/ldp.py
+++ python/fedml/core/dp/frames/ldp.py
@@ -1,13 +1,13 @@
-from fedml.core.dp.mechanisms.dp_mechanism import DPMechanism
+from collections import OrderedDict
+
 from fedml.core.dp.frames.base_dp_solution import BaseDPFrame
+from fedml.core.dp.mechanisms.dp_mechanism import DPMechanism
 
 
 class LocalDP(BaseDPFrame):
     def __init__(self, args):
         super().__init__(args)
-        self.set_ldp(
-            DPMechanism(args.mechanism_type, args.epsilon, args.delta, args.sensitivity)
-        )
+        self.set_ldp(DPMechanism(args.mechanism_type, args.epsilon, args.delta, args.sensitivity))
 
-    def add_local_noise(self, local_grad: dict):
+    def add_local_noise(self, local_grad: OrderedDict):
         return super().add_local_noise(local_grad=local_grad)
