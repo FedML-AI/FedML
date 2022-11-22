99b92fadac96e5d2c4e4703ee2c18c21880f375b
bbuyukates <62020871+bbuyukates@users.noreply.github.com>
2022-11-21 17:13:31 -0800

    Update naive_shapley_value.py

--- python/fedml/core/contribution/naive_shapley_value.py
+++ python/fedml/core/contribution/naive_shapley_value.py
@@ -4,7 +4,6 @@ from scipy.special import comb
 
 from .base_contribution_assessor import BaseContributionAssessor
 
-
 class NaiveShapleyValue(BaseContributionAssessor):
     def __init__(self, args):
         super().__init__()
@@ -15,34 +14,31 @@ class NaiveShapleyValue(BaseContributionAssessor):
     def run(
         self,
         num_client_for_this_round: int,
-        idxs: List,
-        fraction: Dict,  # this is the weights of the clients in FedAvg
+        client_index_for_this_round: List,
+        aggregation_func: Callable,
         local_weights_from_clients: List[Dict],
-        model_aggregated: Dict,
-        model_last_round: Dict,
+        acc_on_last_round: float,
         acc_on_aggregated_model: float,
         val_dataloader: Any,
         validation_func: Callable[[Dict, Any, Any], float],
         device,
-    ) -> List[float]:
+    ) -> {}:
 
-        N = num_client_for_this_round
-
-        powerset = list(BaseContributionAssessor.generate_power_set(idxs))
+        powerset = list(BaseContributionAssessor.generate_power_set(client_index_for_this_round))
 
         util = {}
 
         for S in powerset:
-            # TODO:  this is basically training from scratch with the users in set S and return the final test accuracy
+            # TODO: this is basically training from scratch with the users in set S and returning the final test accuracy
             util[S] = V_S_D(S=S)
 
-        self.SV = self.shapley_value(util, idxs)
+        self.SV = self.shapley_value(util, client_index_for_this_round)
 
         return self.SV
 
-    def shapley_value(self, utility, idxs):
-        N = len(idxs)
-        sv_dict = {id: 0 for id in idxs}
+    def shapley_value(self, utility, client_index_for_this_round):
+        N = len(client_index_for_this_round)
+        sv_dict = {id: 0 for id in client_index_for_this_round}
         for S in utility.keys():
             if S != ():
                 for id in S:
