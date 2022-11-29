7d94b8d1d430cb8533bd3e857ab95c0b9078e74d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-29 06:46:53 +0000

    Adding tarfile member sanitization to extractall()

--- python/app/fedcv/image_segmentation/data/pascal_voc_augmented/utils.py
+++ python/app/fedcv/image_segmentation/data/pascal_voc_augmented/utils.py
@@ -70,7 +70,26 @@ def _extract_file(file_path, extract_dir):
   """
     with tarfile.open(file_path) as tar:
         logging.info('Extracting {} to {}...'.format(file_path, extract_dir))
-        tar.extractall(path=extract_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=extract_dir)
         tar.close()
         os.remove(file_path)
         logging.info('Extracted {}'.format(file_path))
