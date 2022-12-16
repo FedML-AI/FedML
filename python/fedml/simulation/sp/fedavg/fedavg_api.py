2bb88fb0c19e42c14e92a8dd3660b1b8385a0c53
chaoyanghe <chaoyanghe.com@gmail.com>
2022-12-15 18:56:45 -0800

    find the removed code back (due to a wrong submission by YH)

--- python/fedml/simulation/sp/fedavg/fedavg_api.py
+++ python/fedml/simulation/sp/fedavg/fedavg_api.py
@@ -46,11 +46,8 @@ class FedAvgAPI(object):
             train_data_local_num_dict, train_data_local_dict, test_data_local_dict, self.model_trainer,
         )
 
-        # if FedMLDifferentialPrivacy.get_instance().is_local_dp_enabled():
-        #     self.client_epsilon = {}
-
     def _setup_clients(
-            self, train_data_local_num_dict, train_data_local_dict, test_data_local_dict, model_trainer,
+        self, train_data_local_num_dict, train_data_local_dict, test_data_local_dict, model_trainer,
     ):
         logging.info("############setup_clients (START)#############")
         for client_idx in range(self.args.client_num_per_round):
@@ -63,9 +60,7 @@ class FedAvgAPI(object):
                 self.device,
                 model_trainer,
             )
-
             self.client_list.append(c)
-
         logging.info("############setup_clients (END)#############")
 
     def train(self):
@@ -102,11 +97,9 @@ class FedAvgAPI(object):
                 # train on new dataset
                 mlops.event("train", event_started=True, event_value="{}_{}".format(str(round_idx), str(idx)))
                 w = client.train(copy.deepcopy(w_global))
-
                 mlops.event("train", event_started=False, event_value="{}_{}".format(str(round_idx), str(idx)))
                 # self.logging.info("local weights = " + str(w))
                 w_locals.append((client.get_sample_number(), copy.deepcopy(w)))
-
 
             # update global weights
             mlops.event("agg", event_started=True, event_value=str(round_idx))
