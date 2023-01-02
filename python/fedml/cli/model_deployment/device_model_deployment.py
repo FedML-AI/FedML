edc3518f5b0ea9bfc33b8474119d8c77906889ee
Alex <alex.gpt.llm@gmail.com>
2023-01-03 06:00:37 +0800

    update model deployment cli.

--- python/fedml/cli/model_deployment/device_model_deployment.py
+++ python/fedml/cli/model_deployment/device_model_deployment.py
@@ -83,13 +83,11 @@ def start_deployment(end_point_id, model_id,
         src_model_file = os.path.join(converted_model_path, model_file)
         dst_model_file = os.path.join(model_serving_dir, model_file)
         if os.path.isdir(src_model_file):
-            if os.path.exists(dst_model_file):
-                shutil.rmtree(dst_model_file, ignore_errors=True)
-            shutil.copytree(src_model_file, dst_model_file, copy_function=shutil.copy)
+            if not os.path.exists(dst_model_file):
+                shutil.copytree(src_model_file, dst_model_file, copy_function=shutil.copy)
         else:
-            if os.path.exists(dst_model_file):
-                os.remove(dst_model_file)
-            shutil.copyfile(src_model_file, dst_model_file)
+            if not os.path.exists(dst_model_file):
+                shutil.copyfile(src_model_file, dst_model_file)
 
     # Run triton server
     triton_server_is_running = True
