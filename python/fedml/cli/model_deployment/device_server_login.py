8a93111126dc5e196720b6ba5d17370d18ffc6bd
alexliang <alexliang.kh@gmail.com>
2023-01-02 23:05:53 +0800

    update redis install cmds.

--- python/fedml/cli/model_deployment/device_server_login.py
+++ python/fedml/cli/model_deployment/device_server_login.py
@@ -333,7 +333,7 @@ def login(args):
     # Install redis server
     sys_name = platform.system()
     if sys_name == "Linux":
-        os.system("curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg")
+        os.system("sudo rm -f /usr/share/keyrings/redis-archive-keyring.gpg;curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg")
         os.system("echo \"deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/redis.list")
         os.system("sudo apt-get update")
         os.system("sudo apt-get install redis")
