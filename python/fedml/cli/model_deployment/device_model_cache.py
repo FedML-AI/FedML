5000629ed7d48312afa169442c3e03afdb82f2ac
alexliang <alexliang.kh@gmail.com>
2023-01-08 21:08:19 +0800

    ignore the device for end point when checking idle devices.

--- python/fedml/cli/model_deployment/device_model_cache.py
+++ python/fedml/cli/model_deployment/device_model_cache.py
@@ -98,7 +98,8 @@ class FedMLModelCache(object):
         idle_device_list = list()
         for status_item in status_list:
             device_id, status_payload = self.get_status_item_info(status_item)
-            model_status = status_payload["model_status"]
+            if str(device_id) == FedMLModelCache.FEDML_ALL_DEVICE_ID_TAG:
+                continue
             model_id = status_payload["model_id"]
             if model_id == in_model_id and model_status == ServerConstants.MSG_MODELOPS_DEPLOYMENT_STATUS_DEPLOYED:
                 idle_device_list.append(device_id)
