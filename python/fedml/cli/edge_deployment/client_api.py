be9156ca8ff9ee6e818f7a20ba2aba5ece58788f
alexliang <alexliang.kh@gmail.com>
2023-02-07 04:53:55 +0800

    update the client local api.

--- python/fedml/cli/edge_deployment/client_api.py
+++ python/fedml/cli/edge_deployment/client_api.py
@@ -18,10 +18,10 @@ async def get_current_job_status(request: Request):
     current_job = FedMLClientDataInterface.get_instance().get_current_job()
     response = {"jobId": current_job.job_id,
                 "edgeId": current_job.edge_id,
-                "startedTime": int(float(current_job.started_time)),
-                "endedTime": int(float(current_job.ended_time)),
+                "startedTime": int(float(current_job.started_time)) if current_job.started_time != "" else 0,
+                "endedTime": int(float(current_job.ended_time)) if current_job.ended_time != "" else 0,
                 "progress": current_job.progress, "ETA": int(current_job.eta),
-                "failedTime": int(float(current_job.failed_time)),
+                "failedTime": int(float(current_job.failed_time))if current_job.ended_time != "" else 0,
                 "errorCode": current_job.error_code,
                 "msg": current_job.msg}
 
@@ -38,9 +38,9 @@ async def get_history_job_status(request: Request):
     for job_item in history_jobs.job_list:
         response = {"jobId": job_item.job_id,
                     "edgeId": job_item.edge_id,
-                    "startedTime": int(float(job_item.started_time)),
-                    "endedTime": int(float(job_item.ended_time)),
-                    "failedTime": int(float(job_item.failed_time)),
+                    "startedTime": int(float(job_item.started_time)) if job_item.started_time != "" else 0,
+                    "endedTime": int(float(job_item.ended_time)) if job_item.ended_time != "" else 0,
+                    "failedTime": int(float(job_item.failed_time))if job_item.ended_time != "" else 0,
                     "errorCode": job_item.error_code,
                     "msg": job_item.msg}
         responses.append(response)
