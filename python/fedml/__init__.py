544baef129c4b3905fcd2ce56c0f0ea3f0f72841
alexliang <alexliang.kh@gmail.com>
2023-02-14 00:22:30 +0800

    update docs for the starting job cli.

--- python/fedml/__init__.py
+++ python/fedml/__init__.py
@@ -23,7 +23,7 @@ from .core.common.ml_engine_backend import MLEngineBackend
 _global_training_type = None
 _global_comm_backend = None
 
-__version__ = "0.7.414"
+__version__ = "0.7.415"
 
 
 def init(args=None):
