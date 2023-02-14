a17b9b748ad7d9acc718b5c1fa7918a99968c395
Chaoyang He <chaoyanghe.com@gmail.com>
2023-02-13 21:50:43 -0800

    0.7.420

--- python/setup.py
+++ python/setup.py
@@ -86,7 +86,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.415",
+    version="0.7.420",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
