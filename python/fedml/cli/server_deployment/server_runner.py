6451047a036e0b7174bf2c0239c2e4846727acae
Alex <alex.gpt.llm@gmail.com>
2023-02-16 02:14:28 +0800

    pull and refresh packages when starting the job.

--- python/fedml/cli/server_deployment/server_runner.py
+++ python/fedml/cli/server_deployment/server_runner.py
@@ -144,8 +144,9 @@ class FedMLServerRunner:
         except Exception as e:
             pass
         local_package_file = os.path.join(local_package_path, os.path.basename(package_url))
-        if not os.path.exists(local_package_file):
-            urllib.request.urlretrieve(package_url, local_package_file)
+        if os.path.exists(local_package_file):
+            os.remove(local_package_file)
+        urllib.request.urlretrieve(package_url, local_package_file)
         unzip_package_path = ServerConstants.get_package_unzip_dir(self.run_id, package_url)
         self.fedml_packages_base_dir = unzip_package_path
         try:
