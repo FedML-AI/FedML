de8281b417b8352b453fea3f84130beca8b49d62
Chaoyang He <chaoyanghe.com@gmail.com>
2023-02-15 14:28:00 -0800

    force to use "python" as the default interpretator

--- python/setup.py
+++ python/setup.py
@@ -87,7 +87,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.425",
+    version="0.7.426",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
