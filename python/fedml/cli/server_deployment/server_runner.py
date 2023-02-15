a4353ae9f031ef0f8a57fd5513d084cc2f187a25
alexliang <alexliang.kh@gmail.com>
2023-02-16 05:01:56 +0800

    fixed the spelling error.

--- python/fedml/cli/server_deployment/server_runner.py
+++ python/fedml/cli/server_deployment/server_runner.py
@@ -410,8 +410,8 @@ class FedMLServerRunner:
         else:
             # If the run status is killed or finished, then return with the normal state.
             current_job = FedMLServerDataInterface.get_instance().get_job_by_id(run_id)
-            if current_job is not None and current_job.status  == ServerConstants.MSG_MLOPS_SERVER_STATUS_FINISHED or
-                current_job.status  == ServerConstants.MSG_MLOPS_SERVER_STATUS_KILLED:
+            if current_job is not None and (current_job.status  == ServerConstants.MSG_MLOPS_SERVER_STATUS_FINISHED or \
+                current_job.status  == ServerConstants.MSG_MLOPS_SERVER_STATUS_KILLED):
                 return
 
             if err is not None:
