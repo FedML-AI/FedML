d86e9a992eca42579fa537d836f1d649e21060fd
alex-liang-kh <98797487+alex-liang-kh@users.noreply.github.com>
2023-02-19 14:41:38 +0800

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -87,7 +87,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.428",
+    version="0.7.430",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
