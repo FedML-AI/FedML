d8be494983d53c07bc7fe738f67c188436451096
alexliang <alexliang.kh@gmail.com>
2023-02-19 19:17:53 +0800

    checking whether there is running jobs at the server and client side.

--- python/setup.py
+++ python/setup.py
@@ -87,7 +87,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.433",
+    version="0.7.435",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
