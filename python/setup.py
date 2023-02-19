ec95e3c9994a3bc2a5df4093f68e05c822456454
alexliang <alexliang.kh@gmail.com>
2023-02-19 17:19:09 +0800

    fixed the exception(NoSuchProcess) when checking the running process.

--- python/setup.py
+++ python/setup.py
@@ -87,7 +87,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.430",
+    version="0.7.431",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
