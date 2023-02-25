9d05722b2c93385f92b1dd599fb8e3cacaa8bbe5
Alex <alex.gpt.llm@gmail.com>
2023-02-25 18:10:19 +0800

    fixed the issue for python version and add the mnn installation in the bootstrap.

--- python/quick_start/beehive/config/bootstrap.bat
+++ python/quick_start/beehive/config/bootstrap.bat
@@ -6,6 +6,7 @@
 set DATA_PATH=%userprofile%\fedml_data
 if exist %DATA_PATH% (echo Exist %DATA_PATH%) else mkdir %DATA_PATH%
 
+pip install MNN==1.1.6
 
 :: ### don't modify this part ###
 echo [FedML]Bootstrap Finished
