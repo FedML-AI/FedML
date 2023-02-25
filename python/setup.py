e852f9385deb7647becf274a82db2fdc3b9d15c8
alexliang <alexliang.kh@gmail.com>
2023-02-25 15:17:17 +0800

    fixed the issue for attrdict lib and update version to 0.7.449.

--- python/setup.py
+++ python/setup.py
@@ -87,7 +87,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.448",
+    version="0.7.449",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
