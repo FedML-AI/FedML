cc9ffb36190fe09c51d5978f096a8502169ced37
alexliang <alexliang.kh@gmail.com>
2023-03-07 03:15:14 +0800

    ignore retained messages.

--- python/setup.py
+++ python/setup.py
@@ -87,7 +87,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.7.504",
+    version="0.7.505",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
