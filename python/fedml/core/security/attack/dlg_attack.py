0e1c17c36b2dbf50ff5b58a8209d5c0f7e6e280b
han-shanshan <sshan0731@hotmail.com>
2023-03-08 14:04:19 -0800

    dlg attack

--- python/fedml/core/security/attack/dlg_attack.py
+++ python/fedml/core/security/attack/dlg_attack.py
@@ -1,9 +1,11 @@
 from collections import OrderedDict
+import time
 import numpy as np
 import torch
 import torch.nn.functional as F
 from matplotlib import pyplot as plt
 from torchvision import transforms
+from typing import List, Tuple, Any
 import fedml
 from fedml.model.cv.resnet import resnet56, resnet20
 from .attack_base import BaseAttackMethod
@@ -23,14 +25,10 @@ Steps:
 
 class DLGAttack(BaseAttackMethod):
     def __init__(self, args):
-        if args.model == "LeNet":
-            self.model = LeNet()
-        elif args.model == "resnet56":
-            self.model = resnet56(10, pretrained=False, path=None)
-        elif args.model == "resnet20":
-            self.model = resnet20(10)
-        else:
-            raise Exception(f"do not support this model: {args.model}")
+        self.model = None
+        self.model_type = args.model
+
+
         if args.dataset in ["cifar10", "cifar100"]:
             self.original_data_size = torch.Size([1, 3, 32, 32])
             if args.dataset == "cifar10":
@@ -53,9 +51,33 @@ class DLGAttack(BaseAttackMethod):
         if hasattr(args, "protected_layers"):
             self.protected_layers = args.protected_layers  # it should be a list of integers
         else:
-            self.protected_layers = None  # no attack
+            self.protected_layers = None  # no protected layers
+        self.iteration_num = 0
+        if hasattr(args, "attack_iteration_idxs"):
+            self.attack_iteration_idxs = args.attack_iteration_idxs
+        else:
+            self.attack_iteration_idxs = [args.comm_round - 1]
+            # attack the last iteration, as it contains more information
 
-    def reconstruct_data(self, a_model: OrderedDict, extra_auxiliary_info: Any = None):
+    def get_model(self):
+        if self.model_type == "LeNet":
+            return LeNet()
+        elif self.model_type == "resnet56":
+            return resnet56(10, pretrained=False, path=None)
+        elif self.model_type == "resnet20":
+            return resnet20(10)
+        else:
+            raise Exception(f"do not support this model: {self.model_type}")
+
+    def reconstruct_data(self, raw_client_grad_list: List[Tuple[float, OrderedDict]], extra_auxiliary_info: Any = None):
+        if self.iteration_num in self.attack_iteration_idxs:
+            for (_, local_model) in raw_client_grad_list:
+                print(f"-----------attack---------------")
+                self.reconstruct_data_using_a_model(a_model=local_model, extra_auxiliary_info=extra_auxiliary_info)
+        self.iteration_num += 1
+
+    def reconstruct_data_using_a_model(self, a_model: OrderedDict, extra_auxiliary_info: Any = None):
+        self.model = self.get_model()
         global_model_of_last_round = extra_auxiliary_info
         gradient = []
 
@@ -66,7 +88,7 @@ class DLGAttack(BaseAttackMethod):
                     gradient.append(torch.from_numpy(np.zeros(global_model_of_last_round[k].size())).float())
                     # if the layer is protected, set to 0
                 else:
-                    gradient.append(a_model[k] - global_model_of_last_round[k].to(self.device))  # todo: to double check
+                    gradient.append(a_model[k] - global_model_of_last_round[k].to(self.device))  # !!!!!!!!!!!!!!!!!!todo: to double check
                 layer_counter += 1
         gradient = tuple(gradient)
         dummy_data = torch.randn(self.original_data_size)
@@ -81,12 +103,13 @@ class DLGAttack(BaseAttackMethod):
                 dummy_pred = self.model(dummy_data)
                 dummy_loss = self.criterion(dummy_pred, F.softmax(dummy_label, dim=-1))
                 dummy_grad = torch.autograd.grad(dummy_loss, self.model.parameters(), create_graph=True)
-                dummy_grad = tuple(g.to(self.device) for g in dummy_grad)  # extract tensor from tuple and move to device
+                dummy_grad = tuple(
+                    g.to(self.device) for g in dummy_grad)  # extract tensor from tuple and move to device
 
                 grad_diff = 0
-                print(f"len(dummy_grad) = {len(dummy_grad)}, len(gradient) = {len(gradient)}")
-                print(f"gradient={gradient}")
-                print(f"dummy_grad={dummy_grad}")
+                # print(f"len(dummy_grad) = {len(dummy_grad)}, len(gradient) = {len(gradient)}")
+                # print(f"gradient={gradient}")
+                # print(f"dummy_grad={dummy_grad}")
                 for gx, gy in zip(dummy_grad, gradient):
                     grad_diff += ((gx - gy) ** 2).sum()
                 grad_diff.backward()
@@ -96,7 +119,7 @@ class DLGAttack(BaseAttackMethod):
 
             # print(f"dummy_data = {dummy_data}")
             # print(f"dummy_label = {dummy_label}")
-            if iters % 10 == 0:
+            if iters % 50 == 0:
                 current_loss = closure()
                 print(iters, "%.4f" % current_loss.item())
                 history.append(tt(dummy_data[0].cpu()))
@@ -105,9 +128,10 @@ class DLGAttack(BaseAttackMethod):
         for i in range(len(history)):
             plt.subplot(3, 10, i + 1)
             plt.imshow(history[i])
-            plt.title("iter=%d" % (i * 10))
+            plt.title("iter=%d" % (i * 50))
             plt.axis('off')
-
-        plt.show()
+        # plt.show()
+        filename = str(self.iteration_num) + '_' + str(round(time.time()*1000)) + '.png'
+        plt.savefig(filename)
 
         return dummy_data, dummy_label
