437bf9b69d5cc7077071fa31899ec3d808f91b52
alexliang <alexliang.kh@gmail.com>
2023-03-13 00:28:11 +0800

    update model client and server, refactor mqtt topics.

--- python/fedml/core/distributed/communication/constants.py
+++ python/fedml/core/distributed/communication/constants.py
@@ -4,8 +4,8 @@ class CommunicationConstants:
     MSG_TYPE_CONNECTION_IS_READY = 0
     MSG_CLIENT_STATUS_OFFLINE = "OFFLINE"
     MSG_CLIENT_STATUS_IDLE = "IDLE"
-    CLIENT_TOP_LAST_WILL_MSG = "/flclient/last_will_msg"
-    CLIENT_TOP_ACTIVE_MSG = "/flclient/active"
-    SERVER_TOP_LAST_WILL_MSG = "/flserver/last_will_msg"
-    SERVER_TOP_ACTIVE_MSG = "/flserver/active"
+    CLIENT_TOP_LAST_WILL_MSG = "flclient/last_will_msg"
+    CLIENT_TOP_ACTIVE_MSG = "flclient/active"
+    SERVER_TOP_LAST_WILL_MSG = "flserver/last_will_msg"
+    SERVER_TOP_ACTIVE_MSG = "flserver/active"
     GRPC_BASE_PORT = 8890
