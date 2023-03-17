36bfea581a705144c3a3bbbbacb5508cf096aef9
alexliang <alexliang.kh@gmail.com>
2023-03-17 21:37:34 +0800

    debug to write models.

--- python/fedml/core/distributed/communication/s3/remote_storage.py
+++ python/fedml/core/distributed/communication/s3/remote_storage.py
@@ -52,7 +52,8 @@ class S3Storage:
             {"PickleDumpsTime": time.time() - pickle_dump_start_time}
         )
         # for python clients
-        model_pkl = pickle.dumps(model.cpu())
+        device = torch.device('cpu')
+        model_pkl = pickle.dumps(model.to(device))
         model_to_send = model_pkl  # bytes object
         s3_upload_start_time = time.time()
 
