f586c9f29e2dd516e3c3e774d6bedec9b1ec90e4
alexliang <alexliang.kh@gmail.com>
2023-03-24 19:11:36 +0800

    [Test] update to 0.8.2a

--- python/setup.py
+++ python/setup.py
@@ -88,7 +88,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.8.1",
+    version="0.8.2a",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
