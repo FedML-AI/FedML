e8d03f998efffbd66e056bf006ec87e219a0fc4a
zhouLion <zhouquan147369@gmail.com>
2023-03-30 00:25:34 +0800

    feat: Initialize javascript codebase and ready for open source

---
+++ javascript/rollup.config.js
@@ -0,0 +1,55 @@
+import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import alias from '@rollup/plugin-alias'
+
+const entries = [
+  'src/index.ts',
+]
+
+const plugins = [
+  alias({
+    entries: [
+      { find: /^node:(.+)$/, replacement: '$1' },
+    ],
+  }),
+  resolve({
+    preferBuiltins: true,
+  }),
+  json(),
+  commonjs(),
+  esbuild({
+    target: 'node14',
+  }),
+]
+
+export default [
+  ...entries.map(input => ({
+    input,
+    output: [
+      {
+        file: input.replace('src/', 'dist/').replace('.ts', '.mjs'),
+        format: 'esm',
+      },
+      {
+        file: input.replace('src/', 'dist/').replace('.ts', '.cjs'),
+        format: 'cjs',
+      },
+    ],
+    external: [],
+    plugins,
+  })),
+  ...entries.map(input => ({
+    input,
+    output: {
+      file: input.replace('src/', '').replace('.ts', '.d.ts'),
+      format: 'esm',
+    },
+    external: [],
+    plugins: [
+      dts({ respectExternal: true }),
+    ],
+  })),
+]
