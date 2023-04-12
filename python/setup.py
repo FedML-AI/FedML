7e18573a6b055c7fd5479e9022640820286ad2bc
Alex <alex.gpt.llm@gmail.com>
2023-04-13 00:59:10 +0800

    [DevOps] update to 0.8.3a7.

--- python/setup.py
+++ python/setup.py
@@ -89,7 +89,7 @@ requirements_extra_mxnet = [
 
 setup(
     name="fedml",
-    version="0.8.3a6",
+    version="0.8.3a7",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
