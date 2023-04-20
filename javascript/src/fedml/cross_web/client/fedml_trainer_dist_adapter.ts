4d58e9aca0b3c44237bbda9a679328c92a3d186d
ranyide <yideran@gmail.com>
2023-04-20 23:43:41 +0800

    perf: bugfix train model

--- javascript/src/fedml/cross_web/client/fedml_trainer_dist_adapter.ts
+++ javascript/src/fedml/cross_web/client/fedml_trainer_dist_adapter.ts
@@ -1,29 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { FedMLTrainer } from './fedml_trainer'
-import { ModelTrainerCLS } from './ModelTrainerCls'
+import { FedMLTrainer } from './fedml_trainer';
+import { ModelTrainerCLS } from './ModelTrainerCls';
 
 export class TrainerDistAdapter {
-  device: string
-  client_rank
-  model: any
-  trainData: any
-  trainDataLabel: any
-  testData: any
-  testDataLabel: any
-  model_trainer
-  client_index
-  trainer
-  args: Object
+  device: string;
+  client_rank;
+  model: any;
+  trainData: any;
+  trainDataLabel: any;
+  testData: any;
+  testDataLabel: any;
+  model_trainer;
+  client_index;
+  trainer;
+  args: Object;
 
   constructor(
-    args: Object,
-    device: string,
-    client_rank: any,
-    model: any,
-    trainData: any,
-    trainDataLabel: any,
-    testData: any,
-    testDataLabel: any,
+    args,
+    device,
+    client_rank,
+    model,
+    trainData,
+    trainDataLabel,
+    testData,
+    testDataLabel,
     model_trainer = null,
   ) {
     if (model_trainer == null) {
@@ -34,15 +33,15 @@ export class TrainerDistAdapter {
         testData,
         testDataLabel,
         args,
-      )
+      );
     }
-    this.trainData = trainData
-    this.trainDataLabel = trainDataLabel
-    this.testData = testData
-    this.testDataLabel = testDataLabel
-    const client_index = this.client_rank - 1
-    this.model_trainer!.set_id(client_index)
-    console.log('Initializing Trainer')
+    this.trainData = trainData;
+    this.trainDataLabel = trainDataLabel;
+    this.testData = testData;
+    this.testDataLabel = testDataLabel;
+    const client_index = this.client_rank - 1;
+    this.model_trainer.set_id(client_index);
+    console.log('Initializing Trainer');
     const trainer = this.get_trainer(
       client_index,
       trainData,
@@ -52,27 +51,27 @@ export class TrainerDistAdapter {
       device,
       args,
       this.model_trainer,
-    )
+    );
 
-    this.client_index = client_index
-    this.client_rank = client_rank
-    this.device = device
-    this.trainer = trainer
-    this.args = args
+    this.client_index = client_index;
+    this.client_rank = client_rank;
+    this.device = device;
+    this.trainer = trainer;
+    this.args = args;
   }
 
   /**
    * @description: get trainer
    */
   get_trainer(
-    client_index: number,
-    trainData: any,
-    trainDataLabel: any,
-    testData: any,
-    testDataLabel: any,
-    device: any,
-    args: any,
-    model_trainer: ModelTrainerCLS | undefined,
+    client_index,
+    trainData,
+    trainDataLabel,
+    testData,
+    testDataLabel,
+    device,
+    args,
+    model_trainer,
   ) {
     return new FedMLTrainer(
       client_index,
@@ -83,28 +82,25 @@ export class TrainerDistAdapter {
       device,
       args,
       model_trainer,
-    )
+    );
   }
 
-  async train(round_idx: number | null | undefined) {
-    if (!round_idx)
-      return
-
-    const weights = await this.trainer.train(round_idx)
+  async train(round_idx) {
+    const weights = await this.trainer.train(round_idx);
     return {
-      weights,
-    }
+      weights: weights,
+    };
   }
 
-  update_model(model_params: any) {
-    this.trainer.update_model(model_params)
+  update_model(model_params) {
+    this.trainer.update_model(model_params);
   }
 
-  update_dataset(client_index: number) {
-    const _client_index = client_index || this.client_index
+  update_dataset(client_index) {
+    const _client_index = client_index || this.client_index;
   }
 
-  create_model_trainer(model: any, trainData: any, trainDataLabel: any, testData: any, testDataLabel: any, args: any) {
+  create_model_trainer(model, trainData, trainDataLabel, testData, testDataLabel, args) {
     const model_trainer = new ModelTrainerCLS(
       model,
       trainData,
@@ -112,7 +108,7 @@ export class TrainerDistAdapter {
       testData,
       testDataLabel,
       args,
-    )
-    return model_trainer
+    );
+    return model_trainer;
   }
 }
