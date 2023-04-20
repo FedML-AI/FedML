5396445963d0e01eac767b2c95b1a6bbf1ac9705
ranyide <yideran@gmail.com>
2023-04-20 20:17:31 +0800

    perf:name

--- javascript/src/fedml/core/distributed/communication/observer.ts
+++
@@ -1,3 +0,0 @@
-export abstract class Observer {
-  abstract receive_message(msg_type: string | number, msg_params: unknown): any
-}
--- javascript/src/fedml/cross_web/client/client_launcher.ts
+++
@@ -1,8 +0,0 @@
-import { Arguments } from '../../arguments'
-import { constant } from './constants'
-
-export class CrossSiloLauncher {
-  static launch_dist_trainers(_torch_client_filename: string, inputs: any) {
-    const args = Arguments.load_arguments(constant.FEDML_TRAINING_PLATFORM_CROSS_SILO)
-  }
-}
