654ec373eb0cfca2c7275a9dabec0a1a63ead86d
ranyide <yideran@gmail.com>
2023-04-20 19:06:01 +0800

    perf: remove the comment

--- javascript/src/fedml/cross_web/client.ts
+++ javascript/src/fedml/cross_web/client.ts
@@ -1,12 +1,8 @@
-// import { toRaw } from 'vue'
 import type { ClientTrainer } from '../core/alg_frame/client_trainer'
 import { init_client } from './client/client_initializer'
 
 export class FedMLCrossSiloClient {
   constructor(args: { comm: any; rank: any; worker_num: any }, device: any, dataset: any, model: any, model_trainer: ClientTrainer) {
-    // console.log('dataset: ', toRaw(dataset))
-    // const { trainData, trainDataLabel, testData, testDataLabel } = toRaw(dataset)
-    // console.log('dataset: ', trainData)
     const { trainData, trainDataLabel, testData, testDataLabel } = dataset
     init_client(
       args,
