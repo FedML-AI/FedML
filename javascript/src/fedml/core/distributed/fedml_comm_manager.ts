0d66611ed53a3e0fa3135e856d6004349ed70011
ranyide <yideran@gmail.com>
2023-04-20 18:23:15 +0800

    perf: add fetchConfig

--- javascript/src/fedml/core/distributed/fedml_comm_manager.ts
+++ javascript/src/fedml/core/distributed/fedml_comm_manager.ts
@@ -1,7 +1,7 @@
 import { BaseCommunicationManager } from './communication/base_com_manager';
 // import { MqttS3MNNCommManager } from './distributed/communication/mqtt_s3_mnn/mqtt_s3_comm_manager';
 import { MqttS3MultiClientsCommManager } from './communication/mqtt_s3_multi_clients_comm_manager';
-import { fetchConfig } from '/@/api/config/index';
+import { fetchConfig } from '../../../api/index';
 import { Message } from './communication/message';
 import { MyMessage } from '../../cross_web/client/message_define';
 
