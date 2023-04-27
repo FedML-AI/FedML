f4f28be756c21cb690342797e34bc54798ebd74b
Zijian Hu <zjh@fedml.ai>
2023-04-27 12:25:44 -0700

    [FedLLM] update fedml scripts

--- python/app/fedllm/scripts/run_fedml_client.sh
+++ python/app/fedllm/scripts/run_fedml_client.sh
@@ -5,6 +5,8 @@ BASE_DIR="$(dirname "$0")"
 BASE_DIR="$(realpath "${BASE_DIR}/../")"
 cd "${BASE_DIR}"
 
+export WANDB_MODE=disabled # remove this line if you want to use wandb
+
 # FedML related
 RANK="$1"
 RUN_ID="$2"
@@ -13,15 +15,17 @@ RUN_ID="$2"
 NUM_NODES=1
 NUM_GPU=4
 TORCH_DISTRIBUTED_DEFAULT_PORT=29500
-MASTER_PORT=`expr $TORCH_DISTRIBUTED_DEFAULT_PORT + $RANK`
+MASTER_PORT="$((TORCH_DISTRIBUTED_DEFAULT_PORT + RANK))"
 
+# FedML config
+CONFIG_PATH="fedml_config/fedml_config.yaml"
 
 deepspeed \
   --num_nodes="${NUM_NODES}" \
   --num_gpus="${NUM_GPU}" \
   --master_port="${MASTER_PORT}" \
   main_federated_llm.py \
-  --cf fedml_config/fedml_config.yaml \
+  --cf "${CONFIG_PATH}" \
   --rank "${RANK}" \
   --role client \
   --run_id "${RUN_ID}"
