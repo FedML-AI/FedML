129ec98927616abe65a473fcc304db4dd53831e8
Zijian Hu <zjh@fedml.ai>
2023-04-30 03:05:16 -0700

    [FedLLM] update fedml scripts & configs

--- python/app/fedllm/scripts/run_fedml_server.sh
+++ python/app/fedllm/scripts/run_fedml_server.sh
@@ -6,27 +6,16 @@ BASE_DIR="$(realpath "${BASE_DIR}/../")"
 cd "${BASE_DIR}"
 
 export WANDB_MODE=disabled # remove this line if you want to use wandb
+export CUDA_VISIBLE_DEVICES="0"
 
 # FedML related
 RANK=0
 RUN_ID="$1"
 
-# GPU cluster related
-TORCH_DISTRIBUTED_DEFAULT_PORT=29500
-MASTER_ADDR="${2:-"localhost"}"
-MASTER_PORT="${3:-$((TORCH_DISTRIBUTED_DEFAULT_PORT + RANK))}"
-NUM_GPU="$(python -c "import torch; print(torch.cuda.device_count())")"
-NUM_NODES="${4:-1}"
-
 # FedML config
 CONFIG_PATH="fedml_config/fedml_config.yaml"
 
-deepspeed \
-  --num_nodes="${NUM_NODES}" \
-  --num_gpus="${NUM_GPU}" \
-  --master_addr="${MASTER_ADDR}" \
-  --master_port="${MASTER_PORT}" \
-  main_federated_llm.py \
+python main_federated_llm.py \
   --cf "${CONFIG_PATH}" \
   --rank "${RANK}" \
   --role server \
