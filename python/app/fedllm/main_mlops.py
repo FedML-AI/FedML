384b194593a1b1e123ad296cecc9bf9e511263af
Zijian Hu <zjh@fedml.ai>
2023-05-04 12:51:54 -0700

    [FedLLM] init & support MLOps

---
+++ python/app/fedllm/main_mlops.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from pathlib import Path
+import subprocess
+
+if __name__ == '__main__':
+    curr_dir = Path(__file__).parent
+    os.chdir(curr_dir)
+
+    result = subprocess.run(
+        " ".join([
+            "bash",
+            "scripts/run_fedml.sh",
+            "",  # master address
+            "",  # master port
+            "",  # number of nodes
+            *sys.argv[1:],
+        ]),
+        # stdout=subprocess.PIPE,
+        # stderr=subprocess.PIPE,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        shell=True,
+        # capture_output=True,
+        # text=True
+    )
+    print(result)
