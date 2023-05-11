9be0fab44277f5fde6e7e59c8a6a04112ce3fd17
Zijian Hu <zjh@fedml.ai>
2023-05-11 13:14:26 -0700

    [FedLLM] update fedml scripts

--- python/app/fedllm/scripts/run_fedml.sh
+++ python/app/fedllm/scripts/run_fedml.sh
@@ -5,6 +5,10 @@ BASE_DIR="$(dirname "$0")"
 BASE_DIR="$(realpath "${BASE_DIR}/../")"
 cd "${BASE_DIR}"
 
+if [[ -z "${WANDB_MODE}" ]]; then
+  export WANDB_MODE=disabled # remove this line if you want to use wandb
+fi
+
 NUM_GPU="$(python3 -c "import torch; print(torch.cuda.device_count())")"
 
 MASTER_ADDR="${1:-"localhost"}"
@@ -20,11 +24,15 @@ DS_ARGS=(
   --master_port="${MASTER_PORT}"
 )
 if [[ -z "${CUDA_VISIBLE_DEVICES}" ]]; then
-  echo "CUDA_VISIBLE_DEVICES=\"${CUDA_VISIBLE_DEVICES}\""
   DS_ARGS+=(
     --num_nodes="${NUM_NODES}"
     --num_gpus="${NUM_GPU}"
   )
+#else
+#  # see https://github.com/microsoft/DeepSpeed/issues/662
+#  DS_ARGS+=(
+#    --include "${localhost}:${CUDA_VISIBLE_DEVICES}"
+#  )
 fi
 
 deepspeed "${DS_ARGS[@]}" "${@:4}"
