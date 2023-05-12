f574e0bdb96722ee311c2bdaf5bce19866baee29
Alex <alex.gpt.llm@gmail.com>
2023-05-12 02:22:32 -0700

    [DevOps] use the fedml gpu image as the base image of the github action runner, add the readme for deployment the github action in self hosts.

--- devops/dockerfile/github-action-runner/start.sh
+++ devops/dockerfile/github-action-runner/start.sh
@@ -5,18 +5,17 @@ ACCESS_TOKEN=$2
 
 echo $ORGANIZATION
 echo $ACCESS_TOKEN
-#REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+cd /home/fedml/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${ACCESS_TOKEN}
+RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/${ORGANIZATION} --token ${ACCESS_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${ACCESS_TOKEN}
+    RUNNER_ALLOW_RUNASROOT="1" ./config.sh remove --unattended --token ${ACCESS_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+RUNNER_ALLOW_RUNASROOT="1" ./run.sh & wait $!
