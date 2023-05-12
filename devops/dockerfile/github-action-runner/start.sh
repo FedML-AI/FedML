3069aba3d0c149561895e8a473ea3aaac04925f7
Alex <alex.gpt.llm@gmail.com>
2023-05-11 23:03:50 -0700

    [DevOps] add scripts for the github action runner.

--- devops/dockerfile/github-action-runner/start.sh
+++ devops/dockerfile/github-action-runner/start.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
+ORGANIZATION=$1
+ACCESS_TOKEN=$2
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+echo $ORGANIZATION
+echo $ACCESS_TOKEN
+#REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION} --token ${ACCESS_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token ${ACCESS_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
 ./run.sh & wait $!
-
