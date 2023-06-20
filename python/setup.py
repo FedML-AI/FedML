62fcfa8531205727ebef2a33d7e5f7b6c69e0340
Alex <alex.gpt.llm@gmail.com>
2023-06-20 19:31:40 +0800

    [DevOps] change the default inference backend to triton and  upgrade to 0.8.4a38.

--- python/setup.py
+++ python/setup.py
@@ -90,7 +90,7 @@ requirements_extra_crypto = [
 
 setup(
     name="fedml",
-    version="0.8.4a37",
+    version="0.8.4a38",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
