6cfd9633f23fd38cfd981babf96317ebdccbb498
Zijian Hu <zjh@fedml.ai>
2023-07-12 02:59:46 -0700

    [FedLLM] HFTrainer: update save_checkpoint

--- python/app/fedllm/src/hf_trainer.py
+++ python/app/fedllm/src/hf_trainer.py
@@ -9,7 +9,12 @@ from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
 from transformers.utils import WEIGHTS_NAME as HF_WEIGHTS_NAME
 
 from .typing import PathType
-from .utils import is_deepspeed_module
+from .utils import (
+    barrier,
+    move_directory_content,
+    is_deepspeed_module,
+    is_directory,
+)
 
 
 class HFTrainer(Trainer):
@@ -17,10 +22,15 @@ class HFTrainer(Trainer):
         output_dir = Path(output_dir)
         checkpoint_dir = Path(self.args.output_dir) / f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
 
+        # should not save a temporary checkpoint if the checkpoint folder already exist
+        should_save_temp_ckpt = not is_directory(checkpoint_dir)
+        barrier()
+
         model = self.model
         model_wrapped = self.model_wrapped if self.model_wrapped is not None else self.model
 
-        self._save_checkpoint(model_wrapped, trial=None)
+        if should_save_temp_ckpt:
+            self._save_checkpoint(model_wrapped, trial=None)
 
         with self.args.main_process_first(local=self.args.save_on_each_node):
             if self.args.should_save:
@@ -37,5 +47,9 @@ class HFTrainer(Trainer):
                     del checkpoint
 
                 output_dir.mkdir(parents=True, exist_ok=True)
-                # TODO: dirs_exist_ok is for python 3.8+, support older python
-                shutil.copytree(str(checkpoint_dir), str(output_dir), dirs_exist_ok=True)
+                if should_save_temp_ckpt:
+                    # if saved a temporary checkpoint, should move all its content into target directory
+                    move_directory_content(checkpoint_dir, output_dir)
+                else:
+                    # TODO: dirs_exist_ok is for python 3.8+, support older python
+                    shutil.copytree(str(checkpoint_dir), str(output_dir), dirs_exist_ok=True)
