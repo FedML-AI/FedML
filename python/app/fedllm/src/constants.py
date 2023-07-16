9667907fddbaf50776250e3d8fc84c1211149f05
Zijian Hu <zjh@fedml.ai>
2023-07-16 01:20:41 -0700

    [FedLLM] support more models

--- python/app/fedllm/src/constants.py
+++ python/app/fedllm/src/constants.py
@@ -16,7 +16,9 @@ FINETUNE_TASKS = [
 MODEL_NAMES = [
     "EleutherAI/pythia-70m",
     "EleutherAI/pythia-160m",
+    "EleutherAI/pythia-410m",
     "EleutherAI/pythia-1b",
+    "EleutherAI/pythia-1.4b",
     "EleutherAI/pythia-2.8b",
     "EleutherAI/pythia-6.9b",
     "EleutherAI/pythia-12b",
