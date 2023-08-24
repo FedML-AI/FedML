b2a575380a5fd24f88d6b01309c9f4f07f2fbdbb
Alex <alex.gpt.llm@gmail.com>
2023-08-24 21:46:11 +0800

    [CoreEngine] add the gpu util lib in the setup.py.

--- python/setup.py
+++ python/setup.py
@@ -60,6 +60,7 @@ requirements = [
     "onnx",
     "docker==6.1.3",
     "prettytable",
+    "GPUtil"
 ]
 
 requirements_extra_mpi = [
@@ -93,7 +94,7 @@ requirements_extra_crypto = [
 
 setup(
     name="fedml",
-    version="0.8.8a42",
+    version="0.8.8a43",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
