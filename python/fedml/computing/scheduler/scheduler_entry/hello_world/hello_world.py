808ffd2fdaf4a2b4d1911aa4f9fb41d9a71c0d39
Alex <alex.gpt.llm@gmail.com>
2023-09-03 12:31:57 +0800

    [CoreEngine] fixed the error spelling for artifact types.

--- python/fedml/computing/scheduler/scheduler_entry/hello_world/hello_world.py
+++ python/fedml/computing/scheduler/scheduler_entry/hello_world/hello_world.py
@@ -8,3 +8,18 @@ if __name__ == "__main__":
     fedml.mlops.log_artifact(artifact)
 
     fedml.mlops.log_model("cv-model", "./requirements.txt")
+
+    artifact = fedml.mlops.Artifact(name="log-file", type=fedml.mlops.ARTIFACT_TYPE_NAME_LOG)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
+
+    artifact = fedml.mlops.Artifact(name="source-file", type=fedml.mlops.ARTIFACT_TYPE_NAME_SOURCE)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
+
+    artifact = fedml.mlops.Artifact(name="dataset-file", type=fedml.mlops.ARTIFACT_TYPE_NAME_DATASET)
+    artifact.add_file("./requirements.txt")
+    artifact.add_dir("./config")
+    fedml.mlops.log_artifact(artifact)
