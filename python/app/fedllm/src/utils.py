1d2ef7717bc52ff405afca16cf8892a1445a7843
Zijian Hu <zjh@fedml.ai>
2023-09-11 01:20:43 -0700

    [FedLLM] utils: remove unused functions, cleanup

--- python/app/fedllm/src/utils.py
+++ python/app/fedllm/src/utils.py
@@ -12,7 +12,6 @@ from typing import (
     Union,
 )
 
-from collections import OrderedDict
 import inspect
 import logging
 import os
@@ -94,35 +93,6 @@ def to_device(data: T, device: Union[torch.device, str], non_blocking: bool = Tr
         data = data.to(device, non_blocking=non_blocking)
 
     return data
-
-
-def get_device(inputs: Union[Tensor, Module]) -> torch.device:
-    if hasattr(inputs, "device"):
-        return inputs.device
-    else:
-        return next(inputs.parameters()).device
-
-
-def process_state_dict(state_dict: dict, reference_state_dict: dict) -> OrderedDict:
-    output_state_dict = OrderedDict(state_dict)
-
-    for k in state_dict.keys():
-        if k not in reference_state_dict:
-            output_state_dict.pop(k, None)
-            continue
-
-        v = state_dict[k]
-        rv = reference_state_dict[k]
-
-        if isinstance(v, Tensor) != isinstance(rv, Tensor):
-            # if v and rv have different type
-            output_state_dict.pop(k, None)
-
-        if isinstance(v, Tensor) and isinstance(rv, Tensor) and v.shape != rv.shape:
-            # v and rv have different shape
-            output_state_dict.pop(k, None)
-
-    return output_state_dict
 
 
 def save_config(model: ModelType, output_dir: PathType) -> None:
