a816b00eaccf197b88bb5eaa4315923c31e25cad
Raphael Jin <kimheavy@yahoo.com>
2023-09-20 17:59:03 -0700

    [CoreEngine] Support User Overriding Test Method In Their Customized Trainer

--- python/fedml/cross_silo/client/fedml_client_master_manager.py
+++ python/fedml/cross_silo/client/fedml_client_master_manager.py
@@ -117,6 +117,7 @@ class ClientMasterManager(FedMLCommManager):
         self.round_idx = 0
 
         self.__train()
+        self.__test(is_before_aggregation=True)
         self.round_idx += 1
 
     def handle_message_receive_model_from_server(self, msg_params):
@@ -134,7 +135,9 @@ class ClientMasterManager(FedMLCommManager):
         logging.info("current round index {}, total rounds {}".format(self.round_idx, self.num_rounds))
         self.trainer_dist_adapter.update_model(model_params)
         if self.round_idx < self.num_rounds:
+            self.__test(is_before_aggregation=False)    # After aggregation
             self.__train()
+            self.__test(is_before_aggregation=True)
             self.round_idx += 1
         else:
             mlops.stop_sys_perf()
@@ -235,6 +238,19 @@ class ClientMasterManager(FedMLCommManager):
             weights = convert_model_params_from_ddp(weights)
 
         self.send_model_to_server(0, weights, local_sample_num)
+    
+    def __test(self, is_before_aggregation=False):
+        if not hasattr(self.args, "test_on_clients") or self.args.test_on_clients == "no":
+            return
+        
+        if self.args.test_on_clients not in ["before_aggregation", "after_aggregation", "both"]:
+            raise Exception("test_on_clients should be set to 'no' | 'before_aggregation' | 'after_aggregation, | 'both',\
+                            get {} instead".format(self.args.test_on_clients))
+        
+        if self.args.test_on_clients == "both" or \
+            (self.args.test_on_clients == "before_aggregation" and is_before_aggregation == True) or \
+            (self.args.test_on_clients == "after_aggregation" and is_before_aggregation == False):
+            self.trainer_dist_adapter.test(self.round_idx)
 
     def run(self):
         super().run()
