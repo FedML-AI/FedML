424ea143665e111ec9c3a2f5e3620f0e796a9c1a
Alay Shah <alay11shah@gmail.com>
2023-10-08 22:50:58 -0700

    Fix test

--- python/fedml/api/api_test.py
+++ python/fedml/api/api_test.py
@@ -16,15 +16,8 @@ fedml_dir = os.path.dirname(cur_dir)
 python_dir = os.path.dirname(fedml_dir)
 yaml_file = os.path.join(python_dir, "examples", "launch", "hello_job.yaml")
 
-# Match resources
-resource_id,  project_id, error_code, error_msg = fedml.api.match_resources(yaml_file)
-if error_code != 0:
-    print(f"Failed to match resources. Reason: {error_msg}")
-    exit(1)
-
 # Launch job
-job_id, project_id, error_code, error_msg = fedml.api.launch_job(
-    yaml_file, resource_id=resource_id, prompt=False)
+job_id, project_id, error_code, error_msg = fedml.api.launch_job(yaml_file)
 if error_code != 0:
     print(f"Failed to launch job. Reason: {error_msg}")
     exit(1)
