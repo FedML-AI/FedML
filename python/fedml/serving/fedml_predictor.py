67477e1b6154c1acb723f2f0b4e69237bf6a22e9
Zijian Hu <zjh@fedml.ai>
2023-12-06 03:15:54 -0800

    [Deploy] bugfix `FedMLPredictor`

--- python/fedml/serving/fedml_predictor.py
+++ python/fedml/serving/fedml_predictor.py
@@ -1,12 +1,19 @@
 from abc import ABC
 
+
 class FedMLPredictor(ABC):
     def __init__(self):
-        if not (self.predict != FedMLPredictor.predict or self.async_predict != FedMLPredictor.async_predict):
+        if (
+                type(self) is FedMLPredictor or
+                (
+                        type(self).predict == FedMLPredictor.predict and
+                        type(self).async_predict == FedMLPredictor.async_predict
+                )
+        ):
             raise NotImplementedError("At least one of the predict methods must be implemented.")
 
     def predict(self, *args, **kwargs):
         raise NotImplementedError
-    
+
     async def async_predict(self, *args, **kwargs):
         raise NotImplementedError
