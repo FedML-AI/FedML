dd14034dd1ec62348b94060e1cb43cf9cd065154
Zijian Hu <zjh@fedml.ai>
2023-12-05 20:32:45 -0800

    [Deploy] improve streaming API

--- python/examples/deploy/streaming_response/src/main_entry.py
+++ python/examples/deploy/streaming_response/src/main_entry.py
@@ -1,16 +1,21 @@
-from fedml.serving import FedMLPredictor
-from fedml.serving import FedMLInferenceRunner
-import time
-class Chatbot(FedMLPredictor):                # Inherit FedMLClientPredictor
+import asyncio
+from typing import AsyncGenerator
+
+from fedml.serving import FedMLInferenceRunner, FedMLPredictor
+
+
+class Chatbot(FedMLPredictor):  # Inherit FedMLClientPredictor
     def __init__(self):
         super().__init__()
-    
-    async def async_predict(self, *args):
+
+    async def async_predict(self, *args) -> AsyncGenerator[str, None]:
+        # This function can also return fastapi.responses.StreamingResponse directly
         input_json = args[0]
         question = input_json.get("text", "[Empty question]")
         for i in range(5):
-            yield f"Answer for {question} is: {i+1}\n\n"
-            time.sleep(1)
+            yield f"Answer for {question} is: {i + 1}\n\n"
+            await asyncio.sleep(1.0)
+
 
 if __name__ == "__main__":
     chatbot = Chatbot()
--- python/fedml/serving/fedml_inference_runner.py
+++ python/fedml/serving/fedml_inference_runner.py
@@ -1,12 +1,14 @@
 from abc import ABC
-from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse
-from fastapi.responses import StreamingResponse
+import asyncio
+
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import FileResponse, StreamingResponse
+
 
 class FedMLInferenceRunner(ABC):
     def __init__(self, client_predictor):
         self.client_predictor = client_predictor
-    
+
     def run(self):
         api = FastAPI()
 
@@ -16,18 +18,25 @@ class FedMLInferenceRunner(ABC):
             header = request.headers.get("Accept", "application/json")
             if header == "application/json" or header == "*/*":
                 if input_json.get("stream", False):
-                    return StreamingResponse(self.client_predictor.async_predict(input_json))
+                    resp = self.client_predictor.async_predict(input_json)
+                    if asyncio.iscoroutine(resp):
+                        resp = await resp
+
+                    if isinstance(resp, Response):
+                        return resp
+                    else:
+                        return StreamingResponse(resp)
                 else:
                     response_obj = self.client_predictor.predict(input_json)
                     return {"generated_text": str(response_obj)}
             else:
                 response_obj = self.client_predictor.predict(input_json, request.headers.get("Accept"))
                 return FileResponse(response_obj)
-        
+
         @api.get("/ready")
         async def ready():
             return {"status": "Success"}
-        
+
         import uvicorn
         port = 2345
         uvicorn.run(api, host="0.0.0.0", port=port)
