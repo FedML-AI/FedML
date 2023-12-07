2ec5fd366d8e3269e2d4ff3491a81e6c4e10ef3d
Zijian Hu <zjh@fedml.ai>
2023-12-06 17:51:24 -0800

    [Deploy] update streaming example

--- python/examples/deploy/streaming_response/src/main_entry.py
+++ python/examples/deploy/streaming_response/src/main_entry.py
@@ -1,6 +1,7 @@
 import asyncio
 from typing import AsyncGenerator
 
+from fastapi.responses import StreamingResponse
 from fedml.serving import FedMLInferenceRunner, FedMLPredictor
 
 
@@ -8,7 +9,13 @@ class Chatbot(FedMLPredictor):  # Inherit FedMLClientPredictor
     def __init__(self):
         super().__init__()
 
-    async def async_predict(self, *args) -> AsyncGenerator[str, None]:
+    def predict(self, *args, **kwargs):
+        return {"my_output": "example output"}
+
+    async def async_predict(self, *args):
+        return StreamingResponse(self._async_predict(*args))
+
+    async def _async_predict(self, *args) -> AsyncGenerator[str, None]:
         # This function can also return fastapi.responses.StreamingResponse directly
         input_json = args[0]
         question = input_json.get("text", "[Empty question]")
