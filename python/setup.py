27681d825418566de626a74f8286576b35f8c6b7
Alex <alex.gpt.llm@gmail.com>
2024-01-08 20:46:08 +0800

    [CoreEngine] check the inference ready with response codes.

--- python/setup.py
+++ python/setup.py
@@ -112,7 +112,7 @@ requirements_extra_deepspeed = [
 
 setup(
     name="fedml",
-    version="0.8.14.dev21",
+    version="0.8.14b2",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
