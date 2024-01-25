74ad4bcbe3f8c253064ccaa127f0280daaf36f9f
Dimitris Stripelis <dimitris@fedml.ai>
2024-01-25 15:09:33 -0500

    Adding MPI and SP simulation packages.

---
+++ python/examples/launch/federated_mnist_mpi/config/bootstrap.sh
@@ -0,0 +1,8 @@
+# pip install fedml==0.7.15
+#pip install --upgrade fedml
+mkdir -p ~/.cache/fedml_data/MNIST
+rm -rf ~/.cache/fedml_data/MNIST
+
+### don't modify this part ###
+echo "[FedML]Bootstrap Finished"
+##############################
