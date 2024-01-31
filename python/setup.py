601b5344734a3b938b5c1cba44cc42c7b2b0c38a
Alex <98797487+fedml-alex@users.noreply.github.com>
2024-02-01 00:10:38 +0800

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -113,7 +113,7 @@ requirements_extra_deepspeed = [
 
 setup(
     name="fedml",
-    version="0.8.18a8",
+    version="0.8.18b8",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
