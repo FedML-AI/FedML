49405c5117465a676d10248a93a7bd90eeffd17f
Alex <alex.gpt.llm@gmail.com>
2024-02-06 04:45:13 +0800

    [CoreEngine] add the model deploy job, model inference job to the workflow.

---
+++ python/fedml/workflow/customized_jobs/model_inference_job.py
@@ -0,0 +1,92 @@
+
+from fedml.workflow.customized_jobs.customized_base_job import CustomizedBaseJob
+from fedml.computing.scheduler.model_scheduler.device_model_cards import FedMLModelCards
+from fedml.computing.scheduler.model_scheduler.device_model_object import FedMLEndpointDetail
+from fedml.computing.scheduler.model_scheduler.device_server_constants import ServerConstants
+from fedml.workflow.jobs import JobStatus
+import requests
+
+
+class ModelInferenceJob(CustomizedBaseJob):
+    def __init__(self, name, endpoint_id=None, job_api_key=None):
+        super().__init__(name, job_api_key=job_api_key)
+        self.endpoint_id = endpoint_id
+        self.endpoint_detail: FedMLEndpointDetail = None
+        self.inference_url = None
+        self.infer_request_body = None
+        self.key_token = job_api_key
+        self.out_response_json = None
+
+        try:
+            self.endpoint_detail = FedMLModelCards.get_instance().query_endpoint_detail_api(self.endpoint_id, self.job_api_key)
+        except Exception as e:
+            self.endpoint_detail = None
+
+        self.run_status = JobStatus.PROVISIONING
+
+    def run(self):
+        if self.endpoint_detail is None:
+            try:
+                self.endpoint_detail = FedMLModelCards.get_instance().query_endpoint_detail_api(self.endpoint_id, self.job_api_key)
+            except Exception as e:
+                self.endpoint_detail = None
+
+        if self.endpoint_detail is None:
+            self.run_status = JobStatus.FAILED
+
+            self.output_data_list.append(
+                {"error": True, "message": f"Can't get the detail info of endpoint {self.endpoint_id}."})
+
+            return
+
+        if self.endpoint_detail.status == ServerConstants.MSG_MODELOPS_DEPLOYMENT_STATUS_DEPLOYED:
+            self.run_status = JobStatus.RUNNING
+
+            self._build_in_params()
+
+            self.out_response_json = self._inference()
+
+            self.run_status = JobStatus.FAILED if self.out_response_json.get("error", False) else JobStatus.FINISHED
+
+            self.output_data_list.append(self.out_response_json)
+        else:
+            self.run_status = JobStatus.FAILED
+
+            self.output_data_list.append(
+                {"error": True, "message": f"The endpoint {self.endpoint_id} is in the status {self.endpoint_detail.status}."})
+
+    def status(self):
+        return self.run_status
+
+    def kill(self):
+        pass
+
+    def _build_in_params(self):
+        self.inference_url = self.endpoint_detail.inference_url
+
+        if self.input_data_list is not None and isinstance(self.input_data_list, list):
+            if len(self.input_data_list) == 1:
+                self.infer_request_body = self.input_data_list[0]
+            elif len(self.input_data_list) == 0:
+                self.infer_request_body = self.endpoint_detail.input_json
+            else:
+                self.infer_request_body = self.input_data_list
+        else:
+            self.infer_request_body = self.endpoint_detail.input_json
+
+    def _inference(self):
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.key_token}',
+        }
+
+        response = requests.post(self.inference_url, headers=headers, json=self.infer_request_body)
+        if response.status_code != 200:
+            print(f"Inference response with status_code = {response.status_code}, "
+                  f"response.content: {response.content}")
+            return {"error": True, "message": response.content()}
+        else:
+            return response.json()
+
+
--- python/fedml/workflow/driver_example/customized_job_example/customized_workflow.py
+++ python/fedml/workflow/driver_example/customized_job_example/customized_workflow.py
@@ -1,21 +1,19 @@
 import os
 
 from fedml.workflow.workflow import JobStatus, Workflow
-from fedml.workflow.customized_jobs.deploy_job import DeployJob
+from fedml.workflow.customized_jobs.model_deploy_job import ModelDeployJob
+from fedml.workflow.customized_jobs.model_inference_job import ModelInferenceJob
 from fedml.workflow.customized_jobs.train_job import TrainJob
+from typing import List
+import argparse
 
-CURRENT_CONFIG_VERSION = "release"
-CURRENT_ON_PREM_LOCAL_HOST = "localhost"
-CURRENT_ON_PREM_LOCAL_PORT = 18080
 MY_API_KEY = "" # Here you need to set your API key from nexus.fedml.ai
 
 
-class DeployImageJob(DeployJob):
-    def __init__(self, name, job_yaml_absolute_path=None):
-        super().__init__(name, job_yaml_absolute_path=job_yaml_absolute_path,
-                         job_api_key=MY_API_KEY, config_version=CURRENT_CONFIG_VERSION,
-                         local_on_prem_host=CURRENT_ON_PREM_LOCAL_HOST,
-                         local_on_prem_port=CURRENT_ON_PREM_LOCAL_PORT)
+class DeployImageJob(ModelDeployJob):
+    def __init__(self, name, endpoint_id=None, job_yaml_absolute_path=None, job_api_key=None):
+        super().__init__(name, endpoint_id=endpoint_id, job_yaml_absolute_path=job_yaml_absolute_path,
+                         job_api_key=job_api_key)
 
     def run(self):
         super().run()
@@ -37,12 +35,9 @@ class DeployImageJob(DeployJob):
         super().kill()
 
 
-class Deploy3DJob(DeployJob):
-    def __init__(self, name, job_yaml_absolute_path=None):
-        super().__init__(name, job_yaml_absolute_path=job_yaml_absolute_path,
-                         job_api_key=MY_API_KEY, config_version=CURRENT_CONFIG_VERSION,
-                         local_on_prem_host=CURRENT_ON_PREM_LOCAL_HOST,
-                         local_on_prem_port=CURRENT_ON_PREM_LOCAL_PORT)
+class InferenceImageJob(ModelInferenceJob):
+    def __init__(self, name, endpoint_id=None, job_api_key=None):
+        super().__init__(name, endpoint_id=endpoint_id, job_api_key=job_api_key)
         self.run_id = None
 
     def run(self):
@@ -66,11 +61,9 @@ class Deploy3DJob(DeployJob):
 
 
 class TrainJob(TrainJob):
-    def __init__(self, name, job_yaml_absolute_path=None):
+    def __init__(self, name, job_yaml_absolute_path=None, job_api_key=None):
         super().__init__(name, job_yaml_absolute_path=job_yaml_absolute_path,
-                         job_api_key=MY_API_KEY, config_version=CURRENT_CONFIG_VERSION,
-                         local_on_prem_host=CURRENT_ON_PREM_LOCAL_HOST,
-                         local_on_prem_port=CURRENT_ON_PREM_LOCAL_PORT)
+                         job_api_key=job_api_key)
         self.run_id = None
 
     def run(self):
@@ -93,39 +86,117 @@ class TrainJob(TrainJob):
         super().kill()
 
 
-if __name__ == "__main__":
+def show_workflow_metadata(workflow):
+    # After the workflow finished, print the graph, nodes and topological order
+    print("graph", workflow.metadata.graph)
+    print("nodes", workflow.metadata.nodes)
+    print("topological_order", workflow.metadata.topological_order)
+    print("loop", workflow.loop)
+
+
+def create_deploy_workflow(job_api_key=None):
     # Define the job yaml
     working_directory = os.path.dirname(os.path.abspath(__file__))
     deploy_image_job_yaml = os.path.join(working_directory, "deploy_image_job.yaml")
-    deploy_3d_job_yaml = os.path.join(working_directory, "deploy_3d_job.yaml")
     train_job_yaml = os.path.join(working_directory, "train_job.yaml")
 
     # Load the job yaml and change some config items.
     deploy_image_job_yaml_obj = DeployImageJob.load_yaml_config(deploy_image_job_yaml)
-    deploy_3d_job_yaml_obj = DeployImageJob.load_yaml_config(deploy_3d_job_yaml)
     train_job_yaml_obj = DeployImageJob.load_yaml_config(train_job_yaml)
     # deploy_image_job_yaml_obj["computing"]["resource_type"] = "A100-80GB-SXM"
     # deploy_image_job_yaml_obj["computing"]["device_type"] = "GPU"
     # DeployImageJob.generate_yaml_doc(deploy_image_job_yaml_obj, deploy_image_job_yaml)
 
     # Generate the job object
-    deploy_image_job = DeployImageJob(name="deploy_image_job", job_yaml_absolute_path=deploy_image_job_yaml)
-    deploy_3d_job = Deploy3DJob(name="deploy_3d_job", job_yaml_absolute_path=deploy_3d_job_yaml)
-    train_job = TrainJob(name="train_job", job_yaml_absolute_path=train_job_yaml)
+    endpoint_id = 100 # Here you need to set your own endpoint id
+    deploy_image_job = DeployImageJob(
+        name="deploy_image_job", endpoint_id=endpoint_id,
+        job_yaml_absolute_path=deploy_image_job_yaml, job_api_key=job_api_key)
 
     # Define the workflow
-    workflow = Workflow(name="workflow_with_multi_jobs", loop=False)
+    workflow = Workflow(name="deploy_workflow", loop=False)
 
-    # Add the job object to workflow and set the dependency (DAG based).
+    # Add the job object to workflow and set the dependencies (DAG based).
     workflow.add_job(deploy_image_job)
-    #workflow.add_job(deploy_3d_job, dependencies=[deploy_image_job])
-    workflow.add_job(train_job, dependencies=[deploy_image_job])
 
     # Run workflow
     workflow.run()
 
-    # After the workflow finished, print the graph, nodes and topological order
-    print("graph", workflow.metadata.graph)
-    print("nodes", workflow.metadata.nodes)
-    print("topological_order", workflow.metadata.topological_order)
-    print("loop", workflow.loop)
+    # Get the status and result of workflow
+    workflow_status = workflow.get_workflow_status()
+    outputs = workflow.get_outputs()
+    print(f"Final status of the workflow is as follows. {workflow_status}")
+    print(f"Output of the workflow is as follows. {outputs}")
+
+    return workflow_status, outputs
+
+
+def create_inference_train_workflow(
+        job_api_key=None, endpoint_id_list:List[int]=None, input_json=None):
+    # Define the job yaml
+    working_directory = os.path.dirname(os.path.abspath(__file__))
+    deploy_image_job_yaml = os.path.join(working_directory, "deploy_image_job.yaml")
+    train_job_yaml = os.path.join(working_directory, "train_job.yaml")
+
+    # Load the job yaml and change some config items.
+    deploy_image_job_yaml_obj = DeployImageJob.load_yaml_config(deploy_image_job_yaml)
+    train_job_yaml_obj = DeployImageJob.load_yaml_config(train_job_yaml)
+    # deploy_image_job_yaml_obj["computing"]["resource_type"] = "A100-80GB-SXM"
+    # deploy_image_job_yaml_obj["computing"]["device_type"] = "GPU"
+    # DeployImageJob.generate_yaml_doc(deploy_image_job_yaml_obj, deploy_image_job_yaml)
+
+    # Generate the job object
+    inference_jobs = list()
+    for index, endpoint_id in enumerate(endpoint_id_list):
+        inference_job = InferenceImageJob(
+            name=f"inference_job_{index}", endpoint_id=endpoint_id, job_api_key=job_api_key)
+        inference_jobs.append(inference_job)
+    train_job = TrainJob(name="train_job", job_yaml_absolute_path=train_job_yaml, job_api_key=job_api_key)
+
+    # Define the workflow
+    workflow = Workflow(name="inference_workflow", loop=False)
+
+    # Add the job object to workflow and set the dependency (DAG based).
+    for index, inference_job in enumerate(inference_jobs):
+        if index == 0:
+            workflow.add_job(inference_job)
+        else:
+            workflow.add_job(inference_job, dependencies=[inference_jobs[index-1]])
+    workflow.add_job(train_job, dependencies=[inference_jobs[-1]])
+
+    # Set the input to the workflow
+    input_json = {"text": "What is a good cure for hiccups?"} if input_json is None else input_json
+    workflow.set_input(input_json)
+
+    # Run workflow
+    workflow.run()
+
+    # Get the status and result of workflow
+    workflow_status = workflow.get_workflow_status()
+    outputs = workflow.get_outputs()
+    print(f"Final status of the workflow is as follows. {workflow_status}")
+    print(f"Output of the workflow is as follows. {outputs}")
+
+    return workflow_status, outputs
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--deploy", "-d", type=bool, default=False, help="Create a deploy workflow")
+    parser.add_argument("--inference", "-i", type=bool, default=True, help='Create a inference workflow')
+    parser.add_argument("--api_key", "-k", type=str, default=MY_API_KEY, help='API Key from the Nexus AI Platform')
+    parser.add_argument("--infer_json", "-ij", type=str, default=None, help='Input json data for inference')
+
+    args = parser.parse_args()
+
+    workflow_status, outputs = None, None
+    deployed_endpoint_id = 2998
+    if args.deploy:
+        workflow_status, outputs = create_deploy_workflow(job_api_key=args.api_key)
+        deployed_endpoint_id = outputs[0].get("endpoint_id", None)
+
+    if args.inference and deployed_endpoint_id is not None:
+        create_inference_train_workflow(
+            job_api_key=args.api_key, endpoint_id_list=[deployed_endpoint_id, deployed_endpoint_id], input_json=args.infer_json)
+        exit(0)
+
