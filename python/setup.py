e4eef94e1aa35a9f65e38cd5323fe017a7850058
Alay Shah <alay11shah@gmail.com>
2024-02-22 14:07:53 -0800

    Bump test version

--- python/setup.py
+++ python/setup.py
@@ -114,7 +114,7 @@ requirements_extra_deepspeed = [
 
 setup(
     name="fedml",
-    version="0.8.27.dev1",
+    version="0.8.27.a1",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
