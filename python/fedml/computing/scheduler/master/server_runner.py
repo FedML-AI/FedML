72a85e36e24abfd1c457e7f16e6109a424ca2479
Alex <alex.gpt.llm@gmail.com>
2024-02-27 20:58:11 +0800

    [CoreEngine] reset the debug options.

--- python/fedml/computing/scheduler/master/server_runner.py
+++ python/fedml/computing/scheduler/master/server_runner.py
@@ -152,7 +152,7 @@ class FedMLServerRunner:
         self.run_status = None
         self.ntp_offset = MLOpsUtils.get_ntp_offset()
         self.runner_list = dict()
-        self.enable_simulation_cloud_agent = True
+        self.enable_simulation_cloud_agent = False
         self.use_local_process_as_cloud_server = False
 
         self.model_device_server = None
