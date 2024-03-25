3085c5fecf0e485fd4fc21b0c2ad548f76aa9bac
Alay Shah <alay11shah@gmail.com>
2024-03-25 14:05:40 -0700

    Update Launch Driver Example

--- python/fedml/workflow/driver_example/main.py
+++ python/fedml/workflow/driver_example/main.py
@@ -1,18 +1,62 @@
-import argparse
-
-from fedml.workflow.customized_jobs.customized_base_job import CustomizedBaseJob
-from fedml.workflow.workflow import Workflow
 import os
+import argparse
+import traceback
 
+import fedml
+import logging
 
-class HelloWorldJob(CustomizedBaseJob):
-    def __init__(self, name, yaml_path, api_key, version):
-        super().__init__(name=name, job_yaml_absolute_path=yaml_path, job_api_key=api_key, version=version)
+from fedml.workflow.jobs import Job, JobStatus
+from fedml.workflow.workflow import Workflow
 
 
 def get_full_path(file_name):
     working_directory = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(working_directory, file_name)
+
+
+class LaunchJob(Job):
+    def __init__(self, name, yaml_path, api_key, version):
+        super().__init__(name)
+        fedml.set_env_version(version)
+        self.name = name
+        self.yaml_path = yaml_path
+        self.api_key = api_key
+        self.run_id = None
+        self.launch_result = None
+        self.launch_result_code = 0
+        self.launch_result_message = None
+
+    def run(self):
+        try:
+            self.launch_result = fedml.api.launch_job(yaml_file=self.yaml_path, api_key=self.api_key)
+            if self.launch_result.run_id and int(self.launch_result.run_id) > 0:
+                self.run_id = self.launch_result.run_id
+            self.launch_result_code = self.launch_result.result_code
+            self.launch_result_message = self.launch_result.result_message
+        except Exception as e:
+            self.launch_result_code = -1
+            self.launch_result_message = f"Exception {e}; Traceback: {traceback.format_exc()}"
+
+    def status(self):
+        if self.launch_result_code != 0:
+            self.run_status = JobStatus.FAILED
+            return JobStatus.FAILED
+
+        if self.run_id:
+            try:
+                _, run_status = fedml.api.run_status(run_id=self.run_id, api_key=self.api_key)
+                # print(f"run_id: {self.run_id}, run_status: {run_status}")
+                return JobStatus.get_job_status_from_run_status(run_status)
+            except Exception as e:
+                logging.error(f"Error while getting status of run {self.run_id}: {e}")
+            return JobStatus.UNDETERMINED
+
+    def kill(self):
+        if self.run_id:
+            try:
+                return fedml.api.run_stop(run_id=self.run_id, api_key=self.api_key)
+            except Exception as e:
+                logging.error(f"Error while stopping run {self.run_id}: {e}")
 
 
 if __name__ == "__main__":
@@ -22,11 +66,11 @@ if __name__ == "__main__":
                         help='Specify the version of the Nexus AI Platform. It should be test or release. Defaults to release')
     args = parser.parse_args()
 
-    job_1 = HelloWorldJob(name="hello_world", yaml_path=get_full_path("hello_world_job.yaml"), api_key=args.api_key,
+    job_1 = LaunchJob(name="launch_job", yaml_path=get_full_path("hello_world_job.yaml"), api_key=args.api_key,
                           version=args.version)
-    job_2 = HelloWorldJob(name="hello_world_dependent_on_job_1", yaml_path=get_full_path("hello_world_job.yaml"),
+    job_2 = LaunchJob(name="dependent_launch_job", yaml_path=get_full_path("hello_world_job.yaml"),
                           api_key=args.api_key, version=args.version)
-    workflow = Workflow(name="hello_world_workflow", loop=False)
+    workflow = Workflow(name="launch_workflow", loop=False)
     workflow.add_job(job_1)
     workflow.add_job(job_2, dependencies=[job_1])
     workflow.run()
