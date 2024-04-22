8e2e03791e0b71b74d020921936356059cc54759
Alex <alex.gpt.llm@gmail.com>
2024-04-22 11:15:36 +0800

    [CoreEngine] update the workflow example.

--- python/setup.py
+++ python/setup.py
@@ -118,7 +118,7 @@ requirements_extra_deepspeed = [
 
 setup(
     name="fedml",
-    version="0.8.55",
+    version="0.8.56",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
