9296884402f058f9fb64beca79aad6e5dea91596
Raphael Jin <kimheavy@yahoo.com>
2024-06-03 22:53:22 +0000

    [Deploy] Fix config reading from redis.

--- python/fedml/computing/scheduler/model_scheduler/device_model_cache.py
+++ python/fedml/computing/scheduler/model_scheduler/device_model_cache.py
@@ -900,11 +900,14 @@ class FedMLModelCache(Singleton):
             key_pattern = "{}*{}*".format(
                 self.FEDML_MODEL_ENDPOINT_REPLICA_USER_SETTING_TAG,
                 endpoint_id)
+
             endpoint_settings_keys = \
                 self.redis_connection.keys(pattern=key_pattern)
-            if endpoint_settings_keys:
+
+            if len(endpoint_settings_keys) > 0:
                 endpoint_settings = \
-                    json.load(endpoint_settings_keys[0])
+                    self.redis_connection.get(endpoint_settings_keys[0])
+
                 if not isinstance(endpoint_settings, dict):
                     endpoint_settings = json.loads(endpoint_settings)
             else:
