3a034717f7ebc43ff035e73cc49c13ea1c2e7d79
Raphael Jin <kimheavy@yahoo.com>
2024-06-12 12:04:36 -0700

    [Deploy] Nit.

--- python/fedml/computing/scheduler/model_scheduler/master_job_runner.py
+++ python/fedml/computing/scheduler/model_scheduler/master_job_runner.py
@@ -460,7 +460,7 @@ class FedMLDeployMasterJobRunner(FedMLBaseMasterJobRunner, FedMLDeployJobRunnerM
     def start_device_inference_gateway(inference_port=ServerConstants.MODEL_INFERENCE_DEFAULT_PORT):
         # start unified inference server
         python_program = get_python_program()
-        master_port_frm_env = os.getenv(ServerConstants.MODEL_INFERENCE_DEFAULT_PORT, None)
+        master_port_frm_env = os.getenv(ServerConstants.ENV_MASTER_INFERENCE_PORT_KEY, None)
         if master_port_frm_env is not None:
             inference_port = int(master_port_frm_env)
         if not ServerConstants.is_running_on_k8s():
