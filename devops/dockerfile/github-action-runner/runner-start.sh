257c0a7d269c91ac07590be25bf3ed2b0dbb5981
Xiang Wang <xiang@Xiangs-MacBook-Pro.local>
2024-06-12 18:06:53 +0800

    stash

--- devops/dockerfile/github-action-runner/runner-start.sh
+++ devops/dockerfile/github-action-runner/runner-start.sh
@@ -2,13 +2,11 @@ REPO=$1
 TAG=$2
 NUM=$3
 ACCESS_TOKEN=$4
-LOCAL_DEV_SOURCE_DIR=$5
-LOCAL_RELEASE_SOURCE_DIR=$6
-LOCAL_DATA_DIR=$7
+LOCAL_DATA_DIR=$5
 
-if [ $# != 7 ]; then
+if [ $# != 5 ]; then
   echo "Please provide five arguments."
-  echo "./runner-start.sh [YourGitRepo] [YourRunnerPrefix] [YourRunnerNum] [YourGitHubRunnerToken] [LocalDevSourceDir] [LocalReleaseSourceDir] [LocalDataDir]"
+  echo "./runner-start.sh [YourGitRepo] [YourRunnerPrefix] [YourRunnerNum] [YourGitHubRunnerToken][LocalDataDir]"
   exit -1
 fi
 
@@ -19,5 +17,5 @@ for((i=1;i<=$NUM;i++));
 do
 ACT_NAME=$TAG-$i
 sudo docker rm $ACT_NAME
-sudo docker run --name $ACT_NAME --env REPO=$REPO --env ACCESS_TOKEN=$ACCESS_TOKEN -v $LOCAL_DEV_SOURCE_DIR:/home/actions-runner/fedml-dev -v $LOCAL_RELEASE_SOURCE_DIR:/home/actions-runner/fedml-master -v $LOCAL_DATA_DIR:/home/fedml/fedml_data -v $LOCAL_DATA_DIR:/home/actions-runner/fedml_data -d fedml/github-action-runner:latest
+sudo docker run --name $ACT_NAME --env REPO=$REPO --env ACCESS_TOKEN=$ACCESS_TOKEN -v $LOCAL_DATA_DIR:/home/fedml/fedml_data -v $LOCAL_DATA_DIR:/home/actions-runner/fedml_data -d fedml/github-action-runner:latest
 done
