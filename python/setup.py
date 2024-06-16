b1d10de3e8b574a003066d139eb4c18bbbbd7200
Alex <alex.gpt.llm@gmail.com>
2024-06-16 17:21:04 +0800

    [CoreEngine] update the version and change the monitor.

--- python/setup.py
+++ python/setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 requirements = [
     'GPUtil',
-    'PyYAML',
+    'PyYAML==5.3.1',
     'aiohttp>=3.8.1',
     'attrdict',
     'attrs',
@@ -126,7 +126,7 @@ requirements_extra_nlp = [
 
 setup(
     name="fedml",
-    version="0.9.0",
+    version="0.8.50",
     author="FedML Team",
     author_email="ch@fedml.ai",
     description="A research and production integrated edge-cloud library for "
