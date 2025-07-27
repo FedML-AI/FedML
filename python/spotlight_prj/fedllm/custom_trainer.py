daef41604ac4113bc1f758610444628b5758db46
Marcos Villagra <mdvillagra@gmail.com>
2025-07-27 14:15:51 +0000

    Refactor aggregate method documentation in FullModelLLMAggregator and update model name in grpo_gsm8k_test_config.yaml to Qwen3-1.7B

--- python/spotlight_prj/fedllm/custom_trainer.py
+++ python/spotlight_prj/fedllm/custom_trainer.py
@@ -645,9 +645,9 @@ class FullModelLLMAggregator(LLMAggregator):
 
         self.log("finished")
 
-
+    """
     def aggregate(self, raw_client_model_list):
-        """
+        
         Aggregate client models with Nesterov momentum.
 
         Steps
@@ -659,7 +659,7 @@ class FullModelLLMAggregator(LLMAggregator):
         3. Perform an SGD update with momentum on the server side.  If
            ``self._nesterov`` is ``True``, use the Nesterov variant.
         4. Save the updated parameters via ``set_model_params`` and return them.
-        """
+        
         self.log("aggregate: start")
 
         # Step-1: FedAvg aggregation (reuse FedMLAggOperator)
@@ -700,7 +700,7 @@ class FullModelLLMAggregator(LLMAggregator):
         self.set_model_params(updated_params)
         self.log("aggregate: finished")
         return updated_params
-
+    """
 
 
 class TrainingMetricsLogger:
