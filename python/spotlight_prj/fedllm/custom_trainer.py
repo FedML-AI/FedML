675dae59d5fd246862c9cf216a201c8227344838
Marcos Villagra <mdvillagra@gmail.com>
2025-07-29 11:37:51 +0000

    Comment out the 'optim' parameter in FullModelLLMTrainer's GRPO configuration for potential optimization adjustments.

--- python/spotlight_prj/fedllm/custom_trainer.py
+++ python/spotlight_prj/fedllm/custom_trainer.py
@@ -330,7 +330,7 @@ class FullModelLLMTrainer(LLMTrainer):
             repetition_penalty=1.1,
             epsilon=0.2,
             beta=0.1,
-            optim="adamw_bnb_8bit",
+            #optim="adamw_bnb_8bit",
         )
         
         self.log(f"GRPO Config - bf16: {use_bf16}, fp16: {not use_bf16}, batch_size: {grpo_batch_size}")
