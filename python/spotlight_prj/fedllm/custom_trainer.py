28d7513d2bda92ebb2a3a28195b9bf1aa6c7c85b
Marcos Villagra <mdvillagra@gmail.com>
2025-08-06 16:25:19 +0000

    Update generation count in FullModelLLMTrainer from 4 to 2 for optimized testing configuration.

--- python/spotlight_prj/fedllm/custom_trainer.py
+++ python/spotlight_prj/fedllm/custom_trainer.py
@@ -291,7 +291,7 @@ class FullModelLLMTrainer(LLMTrainer):
         else:
             num_generations = 2
         
-        num_generations = 4
+        num_generations = 2
         
         # For testing, we can use a very small number of steps
         if grpo_max_steps > 0:
